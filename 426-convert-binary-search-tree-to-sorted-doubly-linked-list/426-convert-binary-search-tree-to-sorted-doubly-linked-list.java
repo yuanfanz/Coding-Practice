d85cffd65e1dbafd5dda7e400b44af180bba5819
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-07 15:36:48 -0800

    Time: 1 ms (19.12%), Space: 39.4 MB (17.30%) - LeetHub

--- 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.java
@@ -1,46 +1,26 @@
+
 class Solution {
+    Node prev;
     public Node treeToDoublyList(Node root) {
         if (root == null) {
-            return null;
+            return root;
         }
-        Node left = treeToDoublyList(root.left);
-        Node right = treeToDoublyList(root.right);
-        root.left = root;
-        root.right = root;
-        return connect(connect(left, root), right);
+        Node dummy = new Node(0);
+        dummy.right = root;
+        prev = dummy;
+        dfs(root);
+        dummy.right.left = prev;
+        prev.right = dummy.right;
+        return dummy.right;
     }
-    private Node connect(Node left, Node right) {
-        if (left == null) return right;
-        if (right == null) return left;
-        Node tail1 = left.left;
-        Node tail2 = right.left;
-        
-        tail1.right = right;
-        right.left = tail1;
-        tail2.right = left;
-        left.left = tail2;
-        
-        return left;
+    private void dfs(Node root) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left);
+        prev.right = root;
+        root.left = prev;
+        prev = root;
+        dfs(root.right);
     }
 }
-
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val,Node _left,Node _right) {
-        val = _val;
-        left = _left;
-        right = _right;
-    }
-};
-*/
