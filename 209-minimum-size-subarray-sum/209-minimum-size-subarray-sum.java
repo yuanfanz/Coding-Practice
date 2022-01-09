d55d1eb0d9d7f5d8521d073d50b91032350e1b78
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-08 16:58:24 -0800

    Time: 50 ms (10.23%), Space: 39.2 MB (39.43%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        
+        int j = 0;
+        int sum = 0;
+        int min = nums.length;
+        boolean flag = false;
+        for (int i = 0; i < nums.length; ++i) {
+            while (j < nums.length) {
+                if (sum >= target) {
+                    break;
+                }
+                sum += nums[j];
+                j++;
+            }
+            System.out.println(sum);
+            if (sum >= target) {
+                flag = true;
+                if (min > j - i) {
+                    min = j - i;
+                    
+                }
+            }
+            sum -= nums[i];
+        }
+        return flag ? min : 0;
+    }
+}
