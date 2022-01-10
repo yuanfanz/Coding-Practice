dc33f8ca3479d4ec87b6c58945e9b1bb97448050
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-09 21:46:38 -0800

    Time: 45 ms (36.33%), Space: 109.8 MB (20.27%) - LeetHub

---
+++ 1891-cutting-ribbons/1891-cutting-ribbons.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxLength(int[] ribbons, int k) {
+        int i = 1;
+        int j = 100001;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (!isValid(ribbons, mid, k)) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return i - 1;
+    }
+    private boolean isValid(int[] ribbons, int length, int k) {
+        int count = 0;
+        
+        for (int i = 0; i < ribbons.length; ++i) {
+            count += ribbons[i] / length;
+        }
+        return count >= k;
+    }
+}
