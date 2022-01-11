79302a407bdad27dc1ff009e50a5c08a55abc0cd
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-10 21:09:32 -0800

    Time: 1578 ms (10.90%), Space: 65.9 MB (17.52%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        sort(nums, i, j);
+        return nums;
+    }
+    private void sort(int[] nums, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        int pivot = partition(nums, i, j);
+        sort(nums, i, pivot);
+        sort(nums, pivot + 1, j);
+    }
+    private int partition(int[] nums, int i, int j) {
+        int pivot = nums[i];
+        while (i < j) {
+            while (i < j && nums[j] >= pivot) {
+                j--;
+            }
+            nums[i] = nums[j];
+            while (i < j && nums[i] <= pivot) {
+                i++;
+            }
+            nums[j] = nums[i];
+        }
+        nums[i] = pivot;
+        return i;
+    }
+}
