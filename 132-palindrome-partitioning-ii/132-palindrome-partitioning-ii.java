9f2ee146a2d9396dfb246e3b742555093b655bf6
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-10 20:23:11 -0800

    Time: 64 ms (38.79%), Space: 44.7 MB (45.53%) - LeetHub

---
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+        
+        int[] dp = new int[n];
+        boolean[][] isPalin = new boolean[n][n];
+        
+        dp[0] = 0;
+        for (int i = 1; i < n; ++i) {
+            dp[i] = i;
+            for (int j = 0; j <= i; ++j) {
+                if (s.charAt(i) == s.charAt(j) && (j + 1 > i - 1 || isPalin[j + 1][i - 1])) {
+                    isPalin[i][j] = isPalin[j][i] = true;
+                    if (j == 0) {
+                        dp[i] = 0;
+                    } else {
+                        dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                    }
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+}
