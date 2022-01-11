a580ab0a3a01e01208b04db6d49307136208c77c
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-10 20:45:26 -0800

    Time: 0 ms (100.00%), Space: 39 MB (30.24%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -2,15 +2,15 @@ class Solution {
     public void sortColors(int[] nums) {
         int zeroPtr = 0;
         int twoPtr = nums.length - 1;
+        
         for (int i = 0; i <= twoPtr; ++i) {
-            while (nums[i] == 2 && i < twoPtr) {
+            while (i < twoPtr && nums[i] == 2) {
                 swap(nums, i, twoPtr--);
             }
-            while (nums[i] == 0 && i > zeroPtr) {
+            while (zeroPtr < i && nums[i] == 0) {
                 swap(nums, i, zeroPtr++);
             }
         }
-        
     }
     private void swap(int[] nums, int i, int j) {
         int tmp = nums[i];
