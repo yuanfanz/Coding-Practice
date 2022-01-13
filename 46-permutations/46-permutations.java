620447ec876c4d455cd7874129e7eb4297cc582b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-13 11:17:25 -0800

    Time: 3 ms (34.33%), Space: 41.3 MB (32.21%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -4,11 +4,11 @@ class Solution {
         if (nums == null || nums.length == 0) {
             return result;
         }
-        dfs(result, new ArrayList<>(), nums, 0);
+        dfs(result, new ArrayList<>(), nums);
         return result;
     }
     private void dfs(List<List<Integer>> result, List<Integer> list,
-                     int[] nums, int index) {
+                     int[] nums) {
         if (list.size() == nums.length) {
             result.add(new ArrayList<>(list));
             return;
@@ -16,7 +16,7 @@ class Solution {
         for (int i = 0; i < nums.length; ++i) {
             if (list.contains(nums[i])) continue;
             list.add(nums[i]);
-            dfs(result, list, nums, i + 1);
+            dfs(result, list, nums);
             list.remove(list.size() - 1);
         }
     }
