509d25f4076a0089523dd3a09051619f06a8da89
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-16 13:31:31 -0800

    Time: 0 ms (100.00%), Space: 39.3 MB (64.43%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+        dfs(result, new ArrayList<>(), nums, 0);
+        return result;
+    }
+    private void dfs(List<List<Integer>> result, List<Integer> list,
+                    int[] nums, int index) {
+        result.add(new ArrayList<>(list));
+        for (int i = index; i < nums.length; ++i) {
+            list.add(nums[i]);
+            dfs(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
