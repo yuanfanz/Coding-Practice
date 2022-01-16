2342af5ffa8caa1b4747aa1c6535f5e64b72f8c8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-15 17:15:06 -0800

    Time: 2 ms (11.75%), Space: 40.9 MB (13.64%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean search(int[] nums, int target) {
         int i = 0;
         int j = nums.length - 1;
-        
         while (i <= j) {
             int mid = i + (j - i) / 2;
             if (nums[mid] == target) {
@@ -15,7 +14,7 @@ class Solution {
                     j = mid - 1;
                 }
             } else if (nums[mid] > nums[i] || nums[mid] > nums[j]) {
-                if (target >= nums[i] && target <= nums[mid]) {
+                if (target <= nums[mid] && target >= nums[i]) {
                     j = mid - 1;
                 } else {
                     i = mid + 1;
