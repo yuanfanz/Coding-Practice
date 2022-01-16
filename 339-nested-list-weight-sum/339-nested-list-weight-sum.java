d579810bf5930188f973e18fefe75f879461e109
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-16 12:16:59 -0800

    Time: 1 ms (49.17%), Space: 38.6 MB (19.47%) - LeetHub

--- 339-nested-list-weight-sum/339-nested-list-weight-sum.java
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.java
@@ -1,48 +1,17 @@
 
 class Solution {
     public int depthSum(List<NestedInteger> nestedList) {
-        return getSum(nestedList, 1);
+        return dfs(nestedList, 1);
     }
-    private int getSum(List<NestedInteger> nestedList, int level) {
-        if (nestedList.size() == 0) {
-            return 0;
-        }
-        int total = 0;
+    private int dfs(List<NestedInteger> nestedList, int level) {
+        int result = 0;
         for (NestedInteger ni : nestedList) {
             if (ni.isInteger()) {
-                total += ni.getInteger() * level;
+                result += ni.getInteger() * level;
             } else {
-                total += getSum(ni.getList(), level + 1);
+                result += dfs(ni.getList(), level + 1);
             }
         }
-        return total;
+        return result;
     }
 }
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * public interface NestedInteger {
- *     // Constructor initializes an empty nested list.
- *     public NestedInteger();
- *
- *     // Constructor initializes a single integer.
- *     public NestedInteger(int value);
- *
- *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
- *     public boolean isInteger();
- *
- *     // @return the single integer that this NestedInteger holds, if it holds a single integer
- *     // Return null if this NestedInteger holds a nested list
- *     public Integer getInteger();
- *
- *     // Set this NestedInteger to hold a single integer.
- *     public void setInteger(int value);
- *
- *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
- *     public void add(NestedInteger ni);
- *
- *     // @return the nested list that this NestedInteger holds, if it holds a nested list
- *     // Return empty list if this NestedInteger holds a single integer
- *     public List<NestedInteger> getList();
- * }
- */
