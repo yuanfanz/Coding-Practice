0db4bfde12582c180e00db74d9f98ccc01fdd71a
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-15 16:46:09 -0800

    Time: 19 ms (25.83%), Space: 42.1 MB (19.43%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        char sign = '+';
+        int num = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            if (Character.isDigit(ch)) {
+                num = num * 10 + ch - '0';
+            }
+            if (!Character.isDigit(ch) && ch != ' ' || i == s.length() - 1) {
+                if (sign == '+') {
+                    stack.push(num);
+                }
+                if (sign == '-') {
+                    stack.push(-num);
+                }
+                if (sign == '*') {
+                    stack.push(stack.pop() * num);
+                }
+                if (sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+                sign = ch;
+                num = 0;
+            }
+        }
+        int res = 0;
+        while (!stack.isEmpty()) {
+            res += stack.pop();
+        }
+        return res;
+    }
+}
