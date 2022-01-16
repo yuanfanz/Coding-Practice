54abeb0daed2a8d4c43f9ce8526c5ea81fca4802
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-15 16:27:14 -0800

    Time: 12 ms (7.78%), Space: 41.5 MB (30.05%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,13 +1,10 @@
 
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0 || inorder.length == 0) {
-            return null;
-        }
         return dfs(preorder, inorder, 0, 0, inorder.length - 1);
     }
     private TreeNode dfs(int[] preorder, int[] inorder, int preStart, int inStart, int inEnd) {
-        if (preStart > preorder.length - 1 || inStart > inEnd) {
+        if (inStart > inEnd || preStart > preorder.length) {
             return null;
         }
         int val = preorder[preStart];
@@ -16,12 +13,11 @@ class Solution {
         for (int i = inStart; i <= inEnd; ++i) {
             if (inorder[i] == val) {
                 index = i;
-                break;
             }
         }
-        int leftSize = index - inStart;
+        int size = index - inStart;
         root.left = dfs(preorder, inorder, preStart + 1, inStart, index - 1);
-        root.right = dfs(preorder, inorder, preStart + leftSize + 1, index + 1, inEnd);
+        root.right = dfs(preorder, inorder, preStart + size + 1, index + 1, inEnd);
         return root;
     }
 }
