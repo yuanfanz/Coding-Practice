28bd38530323ddf3f109161bd49aad33254f206b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-17 10:01:39 -0800

    Time: 15 ms (14.55%), Space: 40.8 MB (43.24%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -5,21 +5,8 @@ class Solution {
         for (int i = 0; i < tokens.length; ++i) {
             String cur = tokens[i];
             char ch = cur.charAt(cur.length() - 1);
-            int num = 0;
-            boolean negFlag = false;
             if (Character.isDigit(ch)) {
-                for (int j = 0; j < cur.length(); ++j) {
-                    ch = cur.charAt(j);
-                    if (ch == '-') {
-                        negFlag = true;
-                    }
-                    if (ch == '-' || ch == '+') continue;
-                    num = num * 10 + ch - '0';
-                }
-                if (negFlag) {
-                    num = -num;
-                }
-                stack.push(num);
+                stack.push(Integer.valueOf(cur));
             } else {
                 if (ch == '+') {
                     stack.push(stack.pop() + stack.pop());
