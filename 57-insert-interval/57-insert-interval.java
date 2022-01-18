de9273c3056247af014bcde64641087bd4e3a3dd
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-18 10:39:44 -0800

    Time: 3 ms (35.27%), Space: 44.8 MB (16.33%) - LeetHub

--- 57-insert-interval/57-insert-interval.java
+++ 57-insert-interval/57-insert-interval.java
@@ -1,29 +1,23 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         int n = intervals.length;
-        int[][] newArr = new int[n + 1][2];
-        int index = 0;
-        for (int[] cur : intervals) {
-            newArr[index++] = cur;
-        }
-        newArr[newArr.length - 1] = newInterval;
-        Arrays.sort(newArr, (a, b) -> a[0] - b[0]);
         
+        int i = 0;
         List<int[]> list = new ArrayList<>();
-        int[] prev = newArr[0];
-        list.add(prev);
-        for (int i = 1; i < newArr.length; ++i) {
-            int[] cur = newArr[i];
-            if (prev[1] >= cur[0]){
-                int end = Math.max(cur[1], prev[1]);
-                prev[1] = end;
-            } else {
-                list.add(cur);
-                prev = cur;
-            }
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            list.add(intervals[i++]);
+        }
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+            newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
+            i++;
+        }
+        list.add(newInterval);
+        while (i < n) {
+            list.add(intervals[i++]);
         }
         int[][] result = new int[list.size()][2];
-        index = 0;
+        int index = 0;
         for (int[] cur : list) {
             result[index++] = cur;
         }
