4d3601c9c29ef12340a48960f425ac3599f34ec4
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-17 19:05:10 -0800

    Time: 19 ms (22.51%), Space: 45.9 MB (19.34%) - LeetHub

--- 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.java
+++ 449-serialize-and-deserialize-bst/449-serialize-and-deserialize-bst.java
@@ -1,5 +1,6 @@
 
 public class Codec {
+
     // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
         if (root == null) {
@@ -51,3 +52,36 @@ public class Codec {
 // String tree = ser.serialize(root);
 // TreeNode ans = deser.deserialize(tree);
 // return ans;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ *//**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ *//**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ *//**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
