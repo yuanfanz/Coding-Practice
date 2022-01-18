05cee54a5a13969e388a13a25e474feb84691c64
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-18 13:42:09 -0800

    Time: 4 ms (17.63%), Space: 50.1 MB (13.39%) - LeetHub

---
+++ 143-reorder-list/143-reorder-list.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+        ListNode newHead = reverse(slow);
+        merge(head, newHead);
+    }
+    private void merge(ListNode l1, ListNode l2) {
+        while (l1 != null) {
+            ListNode n1 = l1.next;
+            ListNode n2 = l2.next;
+            l1.next = l2;
+            if (n1 == null) {
+                break;
+            }
+            l2.next = n1;
+            l1 = n1;
+            l2 = n2;
+        }
+    }
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null;
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = prev;
+            prev = head;
+            head = tmp;
+        }
+        return prev;
+    }
+}
