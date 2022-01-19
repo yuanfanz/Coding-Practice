060bd6afc5e8751171da5779bd818ae2664512b2
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-18 19:47:05 -0800

    Time: 30 ms (51.96%), Space: 57.5 MB (40.18%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -1,38 +1,32 @@
 class MyHashMap {
-    Node[] array;
+    Node[] arr;
     int SIZE = 10001;
     public MyHashMap() {
-        array = new Node[SIZE];
+        arr = new Node[SIZE];
     }
     
     public void put(int key, int value) {
         int hash = getHash(key);
-        if (array[hash] != null) {
-            Node head = array[hash];
-            Node cur = head;
+        if (arr[hash] != null) {
+            Node cur = arr[hash];
             while (cur != null) {
                 if (cur.key == key) {
-                    cur.value = value;
+                    cur.val = value;
                     return;
                 }
                 cur = cur.next;
             }
-            Node newNode = new Node(key, value, head);
-            array[hash] = newNode;
-            return;
         }
-        Node newNode = new Node(key, value, null);
-        array[hash] = newNode;
+        arr[hash] = new Node(key, value, arr[hash]);
     }
     
     public int get(int key) {
         int hash = getHash(key);
-        if (array[hash] != null) {
-            Node head = array[hash];
-            Node cur = head;
+        if (arr[hash] != null) {
+            Node cur = arr[hash];
             while (cur != null) {
-                if (key == cur.key) {
-                    return cur.value;
+                if (cur.key == key) {
+                    return cur.val;
                 }
                 cur = cur.next;
             }
@@ -42,17 +36,15 @@ class MyHashMap {
     
     public void remove(int key) {
         int hash = getHash(key);
-        if (array[hash] != null) {
-            Node head = array[hash];
-            Node cur = head;
+        if (arr[hash] != null) {
+            Node cur = arr[hash];
             Node prev = null;
-            
             while (cur != null) {
                 if (cur.key == key) {
                     if (prev != null) {
                         prev.next = cur.next;
                     } else {
-                        array[hash] = cur.next;
+                        arr[hash] = cur.next;
                     }
                     return;
                 }
@@ -68,11 +60,11 @@ class MyHashMap {
     
     class Node{
         int key;
-        int value;
+        int val;
         Node next;
-        public Node(int key, int value, Node next) {
+        public Node(int key, int val, Node next) {
             this.key = key;
-            this.value = value;
+            this.val = val;
             this.next = next;
         }
     }
