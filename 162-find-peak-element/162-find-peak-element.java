f82d74c0d083320f83d977af3844db6fb32e05dc
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-19 18:41:17 -0800

    Time: 0 ms (100.00%), Space: 40.3 MB (9.13%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return i;
+    }
+}
