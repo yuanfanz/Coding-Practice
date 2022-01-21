cf507b6331d4bffaab7651a8b99da5699222ee10
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-20 21:21:06 -0800

    Time: 2 ms (41.89%), Space: 41.9 MB (7.73%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,19 +1,16 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        if (nums.length == 0) {
-            return 0;
-        }
         int min = nums[0];
         int max = nums[0];
         int result = nums[0];
         for (int i = 1; i < nums.length; ++i) {
             if (nums[i] < 0) {
-                int tmp = max;
-                max = min;
-                min = tmp;
+                int tmp = min;
+                min = max;
+                max = tmp;
             }
-            max = Math.max(nums[i] * max, nums[i]);
-            min = Math.min(nums[i] * min, nums[i]);
+            min = Math.min(nums[i], min * nums[i]);
+            max = Math.max(nums[i], max * nums[i]);
             result = Math.max(max, result);
         }
         return result;
