5835e8b03611f4f46610744a42faa3980faedb25
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-23 16:01:56 -0800

    Time: 4 ms (72.39%), Space: 38.9 MB (49.89%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        
+        for (int i = 0; i < s.length(); ++i) {
+            count += extend(s, i, i); // even
+            count += extend(s, i, i + 1); // odd
+        }
+        return count;
+    }
+    private int extend(String s, int i, int j) {
+        int count = 0;
+        while (i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)) {
+            count++;
+            i--;
+            j++;
+        }
+        return count;
+    }
+}
