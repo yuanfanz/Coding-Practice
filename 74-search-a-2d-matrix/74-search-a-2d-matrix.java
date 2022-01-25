d76efc34828826f404ac0be341b4e4ae38f19a75
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-25 11:20:53 -0800

    Time: 1 ms (26.47%), Space: 39.3 MB (44.65%) - LeetHub

---
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int i = 0;
+        int j = m - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (matrix[mid][0] <= target && matrix[mid][n - 1] >= target) {
+                return helper(matrix[mid], target);
+            } else if (matrix[mid][0] > target) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return false;
+    }
+    private boolean helper(int[] nums, int target) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                return true;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return false;
+    }
+}
