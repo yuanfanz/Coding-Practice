8784f788bd942d7be6ab0fc9a81fd86f3f82a8f8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-24 22:27:03 -0800

    Time: 3 ms (51.68%), Space: 44.9 MB (45.84%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,10 +1,10 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] dp = new int[n + 1];
         
+        int[] result = new int[n + 1];
         for (int i = 1; i <= n; ++i) {
-            dp[i] = dp[i >>> 1] + (i & 1);
+            result[i] = result[i >>> 1] + (i & 1);
         }
-        return dp;
+        return result;
     }
 }
