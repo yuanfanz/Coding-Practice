5ff3c28a979b1a7755a6e5cad2868670325c9155
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-25 13:07:03 -0800

    Time: 1 ms (24.97%), Space: 43.9 MB (39.68%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,25 +1,40 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int[] result = new int[2];
-        int first = findFirstPosition(nums, target);
-        if (first == nums.length || nums[first] != target) {
-            return new int[]{-1, -1};
-        }
-        result[0] = first;
-        result[1] = findFirstPosition(nums, target + 1) - 1;
-        return result;
-    }
-    private int findFirstPosition(int[] nums, int target) {
         int i = 0;
-        int j = nums.length;
-        while (i < j) {
+        int j = nums.length - 1;
+        
+        int[] result = new int[2];
+        // find first
+        while (i <= j) {
             int mid = i + (j - i) / 2;
-            if (nums[mid] < target) {
-                i = mid + 1;
+            if (nums[mid] == target) {
+                j = mid - 1;
+            } else if (nums[mid] > target) {
+                j = mid - 1;
             } else {
-                j = mid;
+                i = mid + 1;
             }
         }
-        return i;
+        result[0] = i;
+        if (i >= nums.length || nums[i] != target) {
+            result[0] = -1;
+        }
+        i = 0;
+        j = nums.length - 1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == target) {
+                i = mid + 1;
+            } else if (nums[mid] < target) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        result[1] = j;
+        if (j < 0 || nums[j] != target) {
+            result[1] = -1;
+        }
+        return result;
     }
 }
