a0c00a860a2ecd1d578528f9bfa696d0950e59f3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-24 22:05:57 -0800

    Time: 6 ms (99.97%), Space: 38.5 MB (69.39%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0) {
+            return false;
+        }
         int y = x;
         int rev = 0;
         while (y != 0) {
             rev = rev * 10 + y % 10;
             y /= 10;
         }
-        return x == rev;
+        return rev == x;
     }
 }
