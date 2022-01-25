503b94c89e36f7881d695c7df144dd96e45c87a2
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-24 22:16:47 -0800

    Time: 111 ms (22.28%), Space: 113.5 MB (10.94%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -2,15 +2,15 @@ class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
         
-        int end = intervals[0][1];
-        int count = 1;
+        int[] prev = intervals[0];
+        int count = 0;
         for (int i = 1; i < intervals.length; ++i) {
-            int[] cur = intervals[i];
-            if (end <= cur[0]) {
-                end = cur[1];
+            if (prev[1] > intervals[i][0]) {
                 count++;
+            } else {
+                prev = intervals[i];
             }
         }
-        return intervals.length - count;
+        return count;
     }
 }
