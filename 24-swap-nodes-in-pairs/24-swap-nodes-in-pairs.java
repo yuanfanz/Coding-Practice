75b28bddc9bdfc694a63eba2dda2cafa297e1448
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-25 20:39:03 -0800

    Time: 0 ms (100.00%), Space: 38.4 MB (38.75%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -0,0 +1,12 @@
+
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode tmp = head.next;
+        head.next = swapPairs(head.next.next);
+        tmp.next = head;
+        return tmp;
+    }
+}
