7577e9ed9c8b9a92b02a56f582c120e1fdb89e1c
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-28 10:44:53 -0800

    Time: 42 ms (47.47%), Space: 43.5 MB (47.09%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -8,16 +8,22 @@ class Solution {
             return false;
         }
         int target = total / 2;
-        boolean[] dp = new boolean[target + 1];
+        boolean[][] dp = new boolean[nums.length + 1][target + 1];
         
-        dp[0] = true;
-        for (int j = 0; j < nums.length; ++j) {
-            for (int i = target; i >= 1; --i) {
-                if (i >= nums[j]) {
-                    dp[i] = dp[i] || dp[i - nums[j]];
+        for (int i = 0; i <= nums.length; ++i) {
+            dp[i][0] = true;
+        }
+        for (int i = 0; i <= target; ++i) {
+            dp[0][i] = false;
+        }
+        for (int i = 1; i <= nums.length; ++i) {
+            for (int j = 1; j <= target; ++j) {
+                dp[i][j] = dp[i - 1][j];
+                if (j >= nums[i - 1]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j - nums[i - 1]];
                 }
             }
         }
-        return dp[target];
+        return dp[nums.length][target];
     }
 }
