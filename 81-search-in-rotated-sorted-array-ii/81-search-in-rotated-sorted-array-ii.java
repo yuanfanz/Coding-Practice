06fb78fe40de46b497969c93ff7ed33bcb8c78b7
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-27 20:28:24 -0800

    Time: 0 ms (100.00%), Space: 41.9 MB (9.46%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -2,22 +2,23 @@ class Solution {
     public boolean search(int[] nums, int target) {
         int i = 0;
         int j = nums.length - 1;
+        
         while (i <= j) {
             int mid = i + (j - i) / 2;
             if (nums[mid] == target) {
                 return true;
             }
-            if (nums[mid] < nums[j] || nums[mid] < nums[i]) {
-                if (target <= nums[j] && target >= nums[mid]) {
-                    i = mid + 1;
-                } else {
-                    j = mid - 1;
-                }
-            } else if (nums[mid] > nums[i] || nums[mid] > nums[j]) {
+            if (nums[mid] > nums[i] || nums[mid] > nums[j]) {
                 if (target <= nums[mid] && target >= nums[i]) {
                     j = mid - 1;
                 } else {
                     i = mid + 1;
+                }
+            } else if (nums[mid] < nums[j] || nums[mid] < nums[i]) {
+                if (target >= nums[mid] && target <= nums[j]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
                 }
             } else {
                 i++;
