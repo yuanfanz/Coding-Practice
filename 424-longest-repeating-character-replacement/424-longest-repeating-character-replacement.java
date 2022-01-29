42ad969b8cd757a800071bd2916ecee641abcd38
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-28 18:22:44 -0800

    Time: 16 ms (40.61%), Space: 43.2 MB (7.53%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.java
@@ -1,11 +1,12 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        int i = 0;
         int[] hash = new int[26];
-        int max = 0;
+        
+        int max = 1;
+        int i = 0;
         for (int j = 0; j < s.length(); ++j) {
             hash[s.charAt(j) - 'A']++;
-            while (j - i + 1 - getMax(hash) > k) {
+            while (i < s.length() && j - i + 1 - getMax(hash) > k) {
                 hash[s.charAt(i++) - 'A']--;
             }
             if (j - i + 1 - getMax(hash) <= k) {
@@ -17,11 +18,10 @@ class Solution {
         return max;
     }
     private int getMax(int[] hash) {
-        int count = 0;
-        
+        int max = 0;
         for (int i = 0; i < hash.length; ++i) {
-            count = Math.max(count, hash[i]);
+            max = Math.max(max, hash[i]);
         }
-        return count;
+        return max;
     }
 }
