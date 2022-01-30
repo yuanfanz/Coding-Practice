91e5d08e1d794581597b90c9da941e9f875facd4
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-30 15:59:04 -0800

    Time: 97 ms (35.02%), Space: 41.1 MB (12.54%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.java
+++ 526-beautiful-arrangement/526-beautiful-arrangement.java
@@ -2,7 +2,8 @@ class Solution {
     int count = 0;
     public int countArrangement(int n) {
         if (n == 0) return 0;
-        dfs(n, 1, new boolean[n + 1]);
+        boolean[] visited = new boolean[n + 1];
+        dfs(n, 1, visited);
         return count;
     }
     private void dfs(int n, int index, boolean[] visited) {
@@ -12,14 +13,14 @@ class Solution {
         }
         for (int i = 1; i <= n; ++i) {
             if (visited[i]) continue;
-            if (valid(i, index)) {
+            if (isValid(i, index)) {
                 visited[i] = true;
                 dfs(n, index + 1, visited);
                 visited[i] = false;
             }
         }
     }
-    private boolean valid(int a, int b) {
+    private boolean isValid(int a, int b) {
         return a % b == 0 || b % a == 0;
     }
 }
