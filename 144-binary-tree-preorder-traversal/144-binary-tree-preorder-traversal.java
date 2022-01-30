249f1c82d4256c62f0c03816978086c1d78ec09b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-30 10:30:30 -0800

    Time: 1 ms (31.03%), Space: 41.9 MB (7.08%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -1,22 +1,16 @@
 
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
-        
         List<Integer> result = new ArrayList<>();
-        if (root == null) {
-            return result;
-        }
+        
         Stack<TreeNode> stack = new Stack<>();
         stack.push(root);
         while (!stack.isEmpty()) {
             TreeNode cur = stack.pop();
+            if (cur == null) continue;
             result.add(cur.val);
-            if (cur.right != null) {
-                stack.push(cur.right);
-            }
-            if (cur.left != null) {
-                stack.push(cur.left);
-            }
+            stack.push(cur.right);
+            stack.push(cur.left);
         }
         return result;
     }
