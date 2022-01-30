77ea74045f7d94c556990f2b15a13b8897d157e6
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-30 09:50:47 -0800

    Time: 2 ms (51.54%), Space: 48.1 MB (6.91%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.java
@@ -1,27 +1,23 @@
-
 class Solution {
     public Node connect(Node root) {
         if (root == null) {
-            return root;
+            return null;
         }
-        LinkedList<Node> queue = new LinkedList<>();
+        Queue<Node> queue = new LinkedList<>();
         queue.offer(root);
         while (queue.size() != 0) {
+            Node rightNode = null;
             int size = queue.size();
             for (int i = 0; i < size; ++i) {
-                Node cur = queue.get(i);
-                if (i + 1 < size) {
-                    cur.next = queue.get(i + 1);
+                Node cur = queue.poll();
+                cur.next = rightNode;
+                rightNode = cur;
+                if (cur.right != null) {
+                    queue.offer(cur.right);
                 }
                 if (cur.left != null) {
                     queue.offer(cur.left);
                 }
-                if (cur.right != null) {
-                    queue.offer(cur.right);
-                }
-            }
-            for (int i = 0; i < size; ++i) {
-                queue.poll();
             }
         }
         return root;
