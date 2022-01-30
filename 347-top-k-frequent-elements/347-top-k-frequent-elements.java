8a231208dfae880689e7d81ced44376d45a5ecf1
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-29 16:24:12 -0800

    Time: 15 ms (49.29%), Space: 51.3 MB (5.26%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,11 +1,11 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
+        int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < nums.length; ++i) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        ArrayList[] bucket = new ArrayList[nums.length + 1];
+        ArrayList[] bucket = new ArrayList[n + 1];
         for (int key : map.keySet()) {
             int val = map.get(key);
             if (bucket[val] == null) {
@@ -13,12 +13,13 @@ class Solution {
             }
             bucket[val].add(key);
         }
-        List<Integer> list = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
         for (int i = bucket.length - 1; i >= 0 && k > 0; --i) {
             if (bucket[i] == null) continue;
-            list.addAll(bucket[i]);
-            k -= bucket[i].size();
+            List<Integer> list = bucket[i];
+            result.addAll(list);
+            k -= list.size();
         }
-        return list.stream().mapToInt(i -> i).toArray();
+        return result.stream().mapToInt(i -> i).toArray();
     }
 }
