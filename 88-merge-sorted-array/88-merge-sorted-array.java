87964cfc1205c9af5f820d7dc4a3b5c2304bf81d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-29 16:15:18 -0800

    Time: 0 ms (100.00%), Space: 42.5 MB (5.34%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -4,13 +4,13 @@ class Solution {
         int i = m - 1;
         int j = n - 1;
         while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[index--] = nums1[i--];
-            } else {
+            if (nums1[i] < nums2[j]) {
                 nums1[index--] = nums2[j--];
+            } else {
+                nums1[index--] = nums1[i--];
             }
         }
-        while (j >= 0) {
+        while (j >= 0){
             nums1[index--] = nums2[j--];
         }
     }
