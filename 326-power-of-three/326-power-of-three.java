81c1205d8de721d3f60e4a65941133fa0a967a07
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-31 13:50:44 -0800

    Time: 22 ms (39.49%), Space: 47.9 MB (5.31%) - LeetHub

--- 326-power-of-three/326-power-of-three.java
+++ 326-power-of-three/326-power-of-three.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        return (n > 0 && 1162261467 % n == 0);
+        if (n < 1) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
     }
 }
