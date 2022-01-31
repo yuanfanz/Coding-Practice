bcc4b67f507c74a1de9ca2b006eb1877bea770de
Yuanfan Zhang <yuanfanz@uci.edu>
2022-01-30 16:12:04 -0800

    Time: 13 ms (68.10%), Space: 47.9 MB (15.18%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,26 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
+        
+        int[] hash = new int[26];
         int[] target = new int[26];
-        for (int i = 0; i < p.length(); ++i) {
+        int len = p.length();
+        for (int i = 0; i < len; ++i) {
             target[p.charAt(i) - 'a']++;
         }
-        int[] hash = new int[26];
         int i = 0;
         for (int j = 0; j < s.length(); ++j) {
             hash[s.charAt(j) - 'a']++;
-            while (i < s.length() && j - i + 1 > p.length()) {
+            while (i < s.length() && j - i + 1 > len) {
                 hash[s.charAt(i++) - 'a']--;
             }
-            if (valid(hash, target)) {
+            if (isValid(hash, target)) {
                 result.add(i);
             }
         }
         return result;
     }
-    private boolean valid(int[] hash, int[] target) {
-        for (int i = 0; i < hash.length; ++i) {
-            if (hash[i] != target[i]) {
+    private boolean isValid(int[] hash1, int[] hash2) {
+        for (int i = 0; i < hash1.length; ++i) {
+            if (hash1[i] != hash2[i]) {
                 return false;
             }
         }
