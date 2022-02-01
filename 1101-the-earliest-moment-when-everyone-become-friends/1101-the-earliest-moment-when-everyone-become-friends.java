8483a19a7db983d2be0a34a1640a1b32336984b3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-01 11:57:46 -0800

    Time: 8 ms (55.83%), Space: 47.4 MB (10.65%) - LeetHub

---
+++ 1101-the-earliest-moment-when-everyone-become-friends/1101-the-earliest-moment-when-everyone-become-friends.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int earliestAcq(int[][] logs, int n) {
+        Arrays.sort(logs, (a, b) -> a[0] - b[0]);
+        UnionFind uf = new UnionFind(n);
+        for (int[] cur : logs) {
+            int time = cur[0];
+            uf.union(cur[1], cur[2]);
+            if (uf.count == 1) {
+                return time;
+            }
+        }
+        return -1;
+    }
+    class Log{
+        int time;
+        int a;
+        int b;
+        public Log(int[] log) {
+            this.time = log[0];
+            a = log[1];
+            b = log[2];
+        }
+    }
+    class UnionFind{
+        int[] parent;
+        int count;
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; ++i) {
+                parent[i] = i;
+            }
+            count = n;
+        }
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) return;
+            parent[rootP] = rootQ;
+            count--;
+        }
+        public int find(int p) {
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+    }
+}
