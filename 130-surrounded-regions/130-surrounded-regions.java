00fa77ded60c235520f0e16fc83bd504a7680824
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-02 20:21:27 -0800

    Time: 3 ms (55.08%), Space: 52.6 MB (6.87%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -2,10 +2,16 @@ class Solution {
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
-        
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        // first column, last column
-        for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (board[0][j] == 'O') {
+                dfs(0, j, board, dirs);
+            }
+            if (board[m - 1][j] == 'O') {
+                dfs(m - 1, j, board, dirs);
+            }
+        }
+        for (int i = 1; i < m - 1; ++i) {
             if (board[i][0] == 'O') {
                 dfs(i, 0, board, dirs);
             }
@@ -13,37 +19,30 @@ class Solution {
                 dfs(i, n - 1, board, dirs);
             }
         }
-        // first row, last row
-        for (int i = 0; i < n; ++i) {
-            if (board[0][i] == 'O') {
-                dfs(0, i, board, dirs);
-            }
-            if (board[m - 1][i] == 'O') {
-                dfs(m - 1, i, board, dirs);
-            }
-        }   
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (board[i][j] == 'O') {
                     board[i][j] = 'X';
-                } else if (board[i][j] == '*') {
+                }
+                if (board[i][j] == '*') {
                     board[i][j] = 'O';
                 }
             }
         }
     }
-    private void dfs(int i, int j, char[][] board, int[][] dirs) {
+    private void dfs(int i, int j, char[][] board, int[][] dirs) {  
         if (board[i][j] == 'O') {
             board[i][j] = '*';
         }
         for (int[] dir : dirs) {
             int row = i + dir[0];
             int col = j + dir[1];
-            if (row < 0 || col < 0 || row >= board.length || col >= board[0].length
-               || board[row][col] != 'O') {
+            if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
                 continue;
             }
-            dfs(row, col, board, dirs);
+            if (board[row][col] == 'O') {
+                dfs(row, col, board, dirs);
+            }
         }
     }
 }
