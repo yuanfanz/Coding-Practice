9b2c475aaa441f3e440d04a452065658b377a5e9
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-02 17:44:26 -0800

    Time: 0 ms (100.00%), Space: 42.2 MB (5.40%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int i = 0;
+        int j = 1000000001;
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (!isValid(nums, m, mid)) {
+                i = mid + 1;
+            } else {
+                j = mid;
+            }
+        }
+        return j;
+    }
+    private boolean isValid(int[] nums, int m, int target) {
+        int i = 0;
+        int count = 0;
+        while (i < nums.length) {
+            int sum = 0;
+            if (nums[i] > target) {
+                return false;
+            }
+            while (i < nums.length && sum + nums[i] <= target) {
+                sum += nums[i++];
+            }
+            count++;
+        }
+        return count <= m;
+    }
+}
