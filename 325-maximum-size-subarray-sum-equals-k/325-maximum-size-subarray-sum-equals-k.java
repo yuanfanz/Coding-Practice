9aef7e335659870e79547a4ce809cc8b471244b3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-03 22:28:28 -0800

    Time: 190 ms (5.06%), Space: 91 MB (15.52%) - LeetHub

---
+++ 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSubArrayLen(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        map.put(0, -1);
+        int sum = 0;
+        int max = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            sum += nums[i];
+            if (map.containsKey(sum - k)) {
+                max = Math.max(max, i - map.get(sum - k));
+                System.out.println(max);
+            }
+            if (!map.containsKey(sum)) {
+                map.put(sum, i);
+            }
+        }
+        return max;
+    }
+}
