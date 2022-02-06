15b46e35a777333db336bece9055d13e9fc355bb
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-05 17:21:25 -0800

    Time: 10 ms (68.16%), Space: 54 MB (9.34%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,26 +1,26 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int i = 1;
-        int j = 500 * weights.length + 1;
+        int i = 0;
+        int j = weights.length * 500;
         while (i < j) {
             int mid = i + (j - i) / 2;
-            if (!isValid(weights, mid, days)) {
+            if (!isValid(weights, days, mid)) {
                 i = mid + 1;
             } else {
                 j = mid;
             }
         }
-        return j;
+        return i;
     }
-    private boolean isValid(int[] weights, int cap, int days) {
+    private boolean isValid(int[] weights, int days, int cap) {
         int count = 0;
         int i = 0;
         
         while (i < weights.length) {
-            int sum = 0;
-            if (cap < weights[i]) {
+            if (weights[i] > cap) {
                 return false;
             }
+            int sum = 0;
             while (i < weights.length && sum + weights[i] <= cap) {
                 sum += weights[i++];
             }
