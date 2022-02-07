ade7c5f0b9e655864329e5eef9ac1e8096c82228
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-06 20:03:37 -0800

    Time: 76 ms (34.02%), Space: 102.2 MB (7.54%) - LeetHub

--- 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.java
+++ 325-maximum-size-subarray-sum-equals-k/325-maximum-size-subarray-sum-equals-k.java
@@ -8,7 +8,8 @@ class Solution {
         for (int i = 0; i < nums.length; ++i) {
             sum += nums[i];
             if (map.containsKey(sum - k)) {
-                max = Math.max(max, i - map.get(sum - k));
+                int len = i - map.get(sum - k);
+                max = Math.max(max, len);
             }
             if (!map.containsKey(sum)) {
                 map.put(sum, i);
