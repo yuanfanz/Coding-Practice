1aa7516b68740c5f09693c98df58d885c5853fd5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-06 19:02:24 -0800

    Time: 3 ms (50.99%), Space: 47.9 MB (6.73%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,35 +1,40 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        ArrayList[] chart = new ArrayList[n];
-        
+        List[] map = new ArrayList[n];
         for (int i = 0; i < n; ++i) {
-            chart[i] = new ArrayList<>();
+            map[i] = new ArrayList<>();
         }
         for (int[] cur : edges) {
             int first = cur[0];
             int second = cur[1];
-            chart[first].add(second);
-            chart[second].add(first);
+            map[first].add(second);
+            map[second].add(first);
         }
         int[] visited = new int[n];
-        if (dfs(0, -1, visited, chart)) return false;
-        
+        if (dfs(0, -1, visited, map)) {
+            return false;
+        }
         for (int i = 0; i < n; ++i) {
-            if (visited[i] == 0) return false;
+            if (visited[i] == 0) {
+                return false;
+            }
         }
         return true;
     }
-    private boolean dfs(int cur, int parent, int[] visited, ArrayList[] chart) {
+    private boolean dfs(int cur, int parent, int[] visited, List[] map) {
         visited[cur] = 1;
-        for (int i = 0; i < chart[cur].size(); ++i) {
-            int next = (int) chart[cur].get(i);
+        for (int i = 0; i < map[cur].size(); ++i) {
+            int next = (int) map[cur].get(i);
             if (next == parent) continue;
-            if (visited[next] == 1) return true;
+            if (visited[next] == 1) {
+                return true;
+            }
             if (visited[next] == 0) {
-                if (dfs(next, cur, visited, chart)) return true;
+                if (dfs(next, cur, visited, map)) {
+                    return true;
+                }
             }
         }
-        visited[cur] = 2;
         return false;
     }
 }
