4cfc6218a672d8013e63ab4034f7c84052fe4d92
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-06 21:09:59 -0800

    Time: 22 ms (26.86%), Space: 54.3 MB (22.15%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.java
@@ -2,65 +2,51 @@ class Solution {
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         List<List<Integer>> result = new ArrayList<>();
         
-        int m = heights.length;
+        int m = heights.length; 
         int n = heights[0].length;
-            
-        int[][] ocean = new int[m][n];
-        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[m][n];
+        int[][] dirs = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
+        boolean[][] pacific = new boolean[m][n];
+        boolean[][] atlantic = new boolean[m][n];
+        Queue<int[]> upQueue = new LinkedList<>();
+        Queue<int[]> downQueue = new LinkedList<>();
+        
         for (int j = 0; j < n; ++j) {
-            ocean[0][j]++;
-            visited[0][j] = true;
-            queue.offer(new int[]{0, j});
+            upQueue.offer(new int[]{0, j});
+            downQueue.offer(new int[]{m - 1, j});
+            pacific[0][j] = true;
+            atlantic[m - 1][j] = true;
         }
-        for (int i = 1; i < m; ++i) {
-            ocean[i][0]++;
-            visited[i][0] = true;
-            queue.offer(new int[]{i, 0});
+        for (int i = 0; i < m; ++i) {
+            upQueue.offer(new int[]{i, 0});
+            downQueue.offer(new int[]{i, n - 1});
+            pacific[i][0] = true;
+            atlantic[i][n - 1] = true;
         }
-        // pacific BFS
-        bfs(queue, dirs, heights, visited, ocean);
-        visited = new boolean[m][n];
-        for (int j = 0; j < n; ++j) {
-            ocean[m - 1][j]++;
-            visited[m - 1][j] = true;
-            queue.offer(new int[]{m - 1, j});
-        }
-        for (int i = 0; i < m - 1; ++i) {
-            ocean[i][n - 1]++;
-            visited[i][n - 1] = true;
-            queue.offer(new int[]{i, n - 1});
-        }
-        // atlantic BFS
-        bfs(queue, dirs, heights, visited, ocean);
+        bfs(upQueue, pacific, heights, dirs);
+        bfs(downQueue, atlantic, heights, dirs);
+        
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (ocean[i][j] == 2) {
+                if (pacific[i][j] && atlantic[i][j]) {
                     result.add(new ArrayList<>(Arrays.asList(i, j)));
                 }
             }
         }
         return result;
     }
-    private void bfs(Queue<int[]> queue, int[][] dirs, int[][] heights, 
-                     boolean[][] visited, int[][] ocean) {
-        int m = heights.length;
-        int n = heights[0].length;
+    private void bfs(Queue<int[]> queue, boolean[][] visited, int[][] heights, int[][] dirs) {
         while (queue.size() != 0) {
             int[] cur = queue.poll();
             for (int[] dir : dirs) {
                 int row = cur[0] + dir[0];
                 int col = cur[1] + dir[1];
-                if (row < 0 || col < 0 || row >= m || col >= n
-                   || visited[row][col]) {
+                if (row < 0 || col < 0 || row >= heights.length || col >= heights[0].length) {
                     continue;
                 }
-                if (heights[row][col] < heights[cur[0]][cur[1]]) {
+                if (visited[row][col] || heights[row][col] < heights[cur[0]][cur[1]]) {
                     continue;
                 }
                 visited[row][col] = true;
-                ocean[row][col]++;
                 queue.offer(new int[]{row, col});
             }
         }
