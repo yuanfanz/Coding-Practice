13c5a317996c7d23fcfc44b21355494de68a7548
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-07 19:58:21 -0800

    Time: 6 ms (54.18%), Space: 42.4 MB (10.45%) - LeetHub

--- 269-alien-dictionary/269-alien-dictionary.java
+++ 269-alien-dictionary/269-alien-dictionary.java
@@ -1,19 +1,18 @@
 class Solution {
     public String alienOrder(String[] words) {
+        Map<Character, Set<Character>> map = new HashMap<>();
+        
         int[] visited = new int[26];
         Arrays.fill(visited, -1);
-        
-        Map<Character, List<Character>> map = new HashMap<>();
-        for (String word : words) {
-            char[] arr = word.toCharArray();
-            for (char ch : arr) {
-                map.put(ch, new ArrayList<>());
-                visited[ch - 'a'] = 0;
+        for (String s : words) {
+            for (int i = 0; i < s.length(); ++i) {
+                visited[s.charAt(i) - 'a'] = 0;
+                map.put(s.charAt(i), new HashSet<>());
             }
         }
-        for (int i = 1; i < words.length; ++i) {
-            String first = words[i - 1];
-            String second = words[i];
+        for (int i = 0; i < words.length - 1; ++i) {
+            String first = words[i];
+            String second = words[i + 1];
             if (first.length() > second.length() && first.startsWith(second)) {
                 return "";
             }
@@ -21,9 +20,7 @@ class Solution {
                 char ch1 = first.charAt(j);
                 char ch2 = second.charAt(j);
                 if (ch1 != ch2) {
-                    List<Character> list = map.getOrDefault(ch1, new ArrayList<>());
-                    list.add(ch2);
-                    map.put(ch1, list);
+                    map.get(ch1).add(ch2);
                     break;
                 }
             }
@@ -31,33 +28,48 @@ class Solution {
         Stack<Character> stack = new Stack<>();
         for (int i = 0; i < visited.length; ++i) {
             if (visited[i] == 0) {
-                if (dfs(i, visited, map, stack)) {
-                    return "";
-                }
+                if (dfs(visited, i, map, stack)) return "";
+            }
+        }
+        for (int i = 0; i < visited.length; ++i) {
+            if (visited[i] == 0) {
+                return "";
             }
         }
         StringBuilder sb = new StringBuilder();
         while (!stack.isEmpty()) {
             sb.append(stack.pop());
         }
-        if (sb.length() < map.keySet().size()) {
-            return "";
-        }
         return sb.toString();
     }
-    private boolean dfs(int cur, int[] visited, Map<Character, List<Character>> map, Stack<Character> stack) {
+    private boolean dfs(int[] visited, int cur, Map<Character, Set<Character>> map, Stack<Character> stack) {
         visited[cur] = 1;
-        char ch = (char)(cur + 'a');
-        for (char next : map.get(ch)) {
-            if (visited[next - 'a'] == 1) return true;
-            if (visited[next - 'a'] == 0) {
-                if (dfs(next - 'a', visited, map, stack)) {
-                    return true;
-                }
+        char ch = (char) (cur + 'a');
+        for (char ch2 : map.get(ch)) {
+            int next = ch2 - 'a';
+            if (visited[next] == 1) return true;
+            if (visited[next] == 0) {
+                if (dfs(visited, next, map, stack)) return true;
             }
         }
-        stack.push((char)(cur + 'a'));
         visited[cur] = 2;
+        stack.push(ch);
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
