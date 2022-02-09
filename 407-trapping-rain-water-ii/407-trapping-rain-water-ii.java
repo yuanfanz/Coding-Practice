239329abe2ef17e98e88fa767deeac65b802395a
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-09 07:28:35 -0800

    Time: 17 ms (87.35%), Space: 45.9 MB (60.04%) - LeetHub

--- 407-trapping-rain-water-ii/407-trapping-rain-water-ii.java
+++ 407-trapping-rain-water-ii/407-trapping-rain-water-ii.java
@@ -4,35 +4,35 @@ class Solution {
         int n = heightMap[0].length;
         
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         boolean[][] visited = new boolean[m][n];
-        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
         for (int i = 0; i < m; ++i) {
             pq.offer(new int[]{i, 0, heightMap[i][0]});
             pq.offer(new int[]{i, n - 1, heightMap[i][n - 1]});
-            visited[i][0] = true;
-            visited[i][n - 1] = true;
+            visited[i][0] = visited[i][n - 1] = true;
         }
         for (int j = 1; j < n - 1; ++j) {
             pq.offer(new int[]{0, j, heightMap[0][j]});
             pq.offer(new int[]{m - 1, j, heightMap[m - 1][j]});
-            visited[0][j] = true;
-            visited[m - 1][j] = true;
+            visited[0][j] = visited[m - 1][j] = true;
         }
-        int total = 0;
+        int res = 0;
         while (pq.size() != 0) {
-            int[] cell = pq.poll();
+            int[] cur = pq.poll();
+            int i = cur[0];
+            int j = cur[1];
+            int h = cur[2];
             for (int[] dir : dirs) {
-                int i = cell[0] + dir[0];
-                int j = cell[1] + dir[1];
-                if (i < 0 || j < 0 || i >= m || j >= n || visited[i][j]) {
+                int row = i + dir[0];
+                int col = j + dir[1];
+                if (row < 0 || col < 0 || row >= m || col >= n || visited[row][col]) {
                     continue;
                 }
-                visited[i][j] = true;
-                total += Math.max(0, cell[2] - heightMap[i][j]);
-                pq.offer(new int[]{i, j, Math.max(cell[2], heightMap[i][j])});
+                res += Math.max(0, h - heightMap[row][col]);
+                visited[row][col] = true;
+                pq.offer(new int[]{row, col, Math.max(h, heightMap[row][col])});
             }
         }
-        return total;
+        return res;
     }
 }
