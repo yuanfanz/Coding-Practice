044a1d651bcc648db4658769becf9455955a9075
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-08 19:16:52 -0800

    Time: 75 ms (43.89%), Space: 46 MB (34.18%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,22 +1,23 @@
 class Solution {
     public String longestPalindrome(String s) {
-        int n = s.length();
-        
         int max = 0;
         String[] result = new String[1];
         result[0] = "";
         for (int i = 0; i < s.length(); ++i) {
-            int left = i - 1;
-            int right = i + 1;
-            while (left >= 0 && s.charAt(left) == s.charAt(i)) left--;
-            while (right < n && s.charAt(right) == s.charAt(i)) right++;
-            while (left >= 0 && right < n && s.charAt(left) == s.charAt(right)) {
+            int left = i;
+            int right = i;
+            while (left >= 0 && s.charAt(left) == s.charAt(i)) {
+                left--;
+            }
+            while (right < s.length() && s.charAt(right) == s.charAt(i)) {
+                right++;
+            }
+            while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
                 left--;
                 right++;
             }
-            int len = right - left - 1;
-            if (len > max) {
-                max = len;
+            if (right - left > max) {
+                max = right - left - 1;
                 result[0] = s.substring(left + 1, right);
             }
         }
