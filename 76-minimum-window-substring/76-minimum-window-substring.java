1905f8bc396710cb5034b8780fd08cb432e98570
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-08 18:47:26 -0800

    Time: 126 ms (10.09%), Space: 51.7 MB (11.55%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.java
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -1,26 +1,25 @@
 class Solution {
     public String minWindow(String s, String t) {
-        int[] hash1 = new int[256];
-        int[] hash2 = new int[256];
+        int[] target = new int[256];
         for (int i = 0; i < t.length(); ++i) {
-            hash2[t.charAt(i)]++;
+            target[t.charAt(i)]++;
         }
-        
+        int[] hash = new int[256];
         int j = 0;
         int min = s.length() + 1;
         String[] result = new String[1];
         result[0] = "";
         for (int i = 0; i < s.length(); ++i) {
-            while (j < s.length() && !isValid(hash1, hash2)) {
-                hash1[s.charAt(j++)]++;
+            while (j < s.length() && !isValid(hash, target)) {
+                hash[s.charAt(j++)]++;
             }
-            if (isValid(hash1, hash2)) {
-                if (min > j - i) {
+            if (isValid(hash, target)) {
+                if (j - i < min) {
                     min = j - i;
                     result[0] = s.substring(i, j);
                 }
             }
-            hash1[s.charAt(i)]--;
+            hash[s.charAt(i)]--;
         }
         return result[0];
     }
