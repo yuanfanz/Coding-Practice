42ce3ee1a4ed713b6004fbfdf2fb03a22740e59b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-10 18:52:26 -0800

    Time: 279 ms (5.02%), Space: 54.7 MB (13.98%) - LeetHub

--- 498-diagonal-traverse/498-diagonal-traverse.java
+++ 498-diagonal-traverse/498-diagonal-traverse.java
@@ -3,16 +3,16 @@ class Solution {
         int m = mat.length;
         int n = mat[0].length;
         
-        int[] result = new int[m * n];
-        boolean flag = true;
-        int index = 0;
         int total = m + n - 2;
-        for (int cur = 0; cur <= total; ++cur) {
-            for (int i = 0; i <= cur; ++i) {
-                int j = cur - i;
-                int row = i;
-                int col = j;
-                if (flag) {
+        boolean right = true;
+        int[] result = new int[m * n];
+        int index = 0;
+        for (int i = 0; i <= total; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                int row = j;
+                int col = i - j;
+                // if (row >= m || col >= n) continue;
+                if (right) {
                     int tmp = row;
                     row = col;
                     col = tmp;
@@ -21,7 +21,7 @@ class Solution {
                     result[index++] = mat[row][col];
                 }
             }
-            flag = !flag;
+            right = !right;
         }
         return result;
     }
