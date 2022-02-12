2cf9c9b11ce5e0ce3b7bd84981ccd8cf48c5b994
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-12 15:00:26 -0800

    Time: 2246 ms (7.28%), Space: 42.2 MB (19.65%) - LeetHub

---
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        if (sum % k != 0) {
+            return false;
+        }
+        Arrays.sort(nums);
+        int target = sum / k;
+        boolean[] visited = new boolean[nums.length];
+        return dfs(nums, target, k, visited, nums.length - 1, 0);
+    }
+    private boolean dfs(int[] nums, int target, int k, boolean[] visited, int index, int sum) {
+        if (k == 0) return true;
+        if (sum == target && dfs(nums, target, k - 1, visited, nums.length - 1, 0)) {
+            return true;
+        }
+        for (int i = index; i >= 0; --i) {
+            if (!visited[i] && sum + nums[i] <= target) {
+                visited[i] = true;
+                if (dfs(nums, target, k, visited, i - 1, sum + nums[i])) return true;
+                visited[i] = false;
+            }
+        }
+        return false;
+    }
+}
