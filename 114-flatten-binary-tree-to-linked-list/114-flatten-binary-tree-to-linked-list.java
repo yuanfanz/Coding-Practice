acd1c97e825d7e090df6c10c4848e010936e1df8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-11 22:06:41 -0800

    Time: 1 ms (64.80%), Space: 42.9 MB (7.06%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -1,22 +1,19 @@
 
 class Solution {
     public void flatten(TreeNode root) {
-        if (root == null) {
+        if (root == null){
             return;
         }
-        TreeNode cur = root;
-        while (cur != null) {
-            if (cur.left == null) {
-                cur = cur.right;
-            } else {
-                TreeNode prev = cur.left;
-                while (prev.right != null) {
-                    prev = prev.right;
-                }
-                prev.right = cur.right;
-                cur.right = cur.left;
-                cur.left = null;
-            }
+        flatten(root.left);
+        flatten(root.right);
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        root.left = null;
+        root.right = left;
+        while (root.right != null) {
+            root = root.right;
         }
+        root.right = right;
+        
     }
 }
