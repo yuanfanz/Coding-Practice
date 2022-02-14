6eff6d9cd81d7f2784a5086a35b3f9d715c56edc
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-14 14:36:31 -0800

    Time: 1 ms (100.00%), Space: 84 MB (70.32%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.java
@@ -1,33 +1,16 @@
 class Solution {
     public boolean increasingTriplet(int[] nums) {
-        int n = nums.length;
-        
-        int[] sequence = new int[n];
-        int len = 0;
-        sequence[len++] = nums[0];
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] > sequence[len - 1]) {
-                sequence[len++] = nums[i];
-                if (len >= 3) return true;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; ++i) {
+            if (nums[i] <= min) {
+                min = nums[i];
+            } else if (nums[i] <= max) {
+                max = nums[i];
             } else {
-                int index = findFirstPosition(sequence, 0, len - 1, nums[i]);
-                sequence[index] = nums[i];
+                return true;
             }
         }
         return false;
-    }
-    
-    private int findFirstPosition(int[] nums, int i, int j, int target) {
-        while (i <= j){
-            int mid = i + (j - i) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                i = mid + 1;
-            } else {
-                j = mid - 1;
-            }
-        }
-        return i;
     }
 }
