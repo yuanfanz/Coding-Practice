7d5d28c16c7606a6805943d59f98a454443aa81d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-14 20:39:32 -0800

    Time: 39 ms (59.15%), Space: 41.8 MB (13.23%) - LeetHub

--- 465-optimal-account-balancing/465-optimal-account-balancing.java
+++ 465-optimal-account-balancing/465-optimal-account-balancing.java
@@ -1,22 +1,21 @@
 class Solution {
     public int minTransfers(int[][] transactions) {
-        int[] arr = buildDebt(transactions);
+        int[] debt = buildDebt(transactions);
         
-        return getMinTransactions(0, arr);
+        return getNum(0, debt);
     }
-    private int getMinTransactions(int cur, int[] arr) {
+    private int getNum(int cur, int[] arr) {
         while (cur < arr.length && arr[cur] == 0) {
             cur++;
         }
-        // only 1 person, no transactions needed, base case
         if (cur == arr.length) {
             return 0;
         }
         int min = Integer.MAX_VALUE;
         for (int i = cur + 1; i < arr.length; ++i) {
-            if (arr[i] * arr[cur] < 0) {
+            if (arr[cur] * arr[i] < 0) {
                 arr[i] += arr[cur];
-                min = Math.min(min, getMinTransactions(cur + 1, arr) + 1);
+                min = Math.min(min, getNum(cur + 1, arr) + 1);
                 arr[i] -= arr[cur];
             }
         }
@@ -24,13 +23,12 @@ class Solution {
     }
     private int[] buildDebt(int[][] transactions) {
         Map<Integer, Integer> map = new HashMap<>();
-        
         for (int[] cur : transactions) {
             int giver = cur[0];
             int taker = cur[1];
-            int amount = cur[2];
-            map.put(giver, map.getOrDefault(giver, 0) + amount);
-            map.put(taker, map.getOrDefault(taker, 0) - amount);
+            int num = cur[2];
+            map.put(giver, map.getOrDefault(giver, 0) + num);
+            map.put(taker, map.getOrDefault(taker, 0) - num);
         }
         int[] arr = new int[map.size()];
         int index = 0;
