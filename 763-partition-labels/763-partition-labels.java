aee1ce92fdf63680238bde19622fd6da7692fec2
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-15 11:28:07 -0800

    Time: 6 ms (56.38%), Space: 42.5 MB (19.11%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        List<Integer> result = new ArrayList<>();
+        if (s == null || s.length() == 0) {
+            return result;
+        }
+        int[] map = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            map[s.charAt(i) - 'a'] = i;
+        }
+        int last = 0;
+        int start = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            last = Math.max(last, map[s.charAt(i) - 'a']);
+            if (last == i) {
+                result.add(i - start + 1);
+                start = last + 1;
+            }
+        }
+        return result;
+    }
+}
