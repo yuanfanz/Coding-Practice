f3fa4edc785b628156288dba4dca929da6eacd39
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-15 17:14:17 -0800

    Time: 2802 ms (5.02%), Space: 41.6 MB (27.63%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.java
@@ -4,23 +4,27 @@ class Solution {
         for (int i : nums) {
             sum += i;
         }
-        if (nums.length < k || sum % k != 0) {
+        if (sum % k != 0) {
             return false;
         }
         Arrays.sort(nums);
         int target = sum / k;
         boolean[] visited = new boolean[nums.length];
-        return dfs(nums, target, k, visited, nums.length - 1, 0);
+        return dfs(nums, target, k, 0, nums.length - 1, visited);
     }
-    private boolean dfs(int[] nums, int target, int k, boolean[] visited, int index, int sum) {
-        if (k == 0) return true;
-        if (sum == target && dfs(nums, target, k - 1, visited, nums.length - 1, 0)) {
+    private boolean dfs(int[] nums, int target, int k, int sum, int index, boolean[] visited) {
+        if (k == 0) {
+            return true;
+        }
+        if (sum == target && dfs(nums, target, k - 1, 0, nums.length - 1, visited)) {
             return true;
         }
         for (int i = index; i >= 0; --i) {
             if (!visited[i] && sum + nums[i] <= target) {
                 visited[i] = true;
-                if (dfs(nums, target, k, visited, i - 1, sum + nums[i])) return true;
+                if (dfs(nums, target, k, sum + nums[i], i - 1, visited)) {
+                    return true;
+                }
                 visited[i] = false;
             }
         }
