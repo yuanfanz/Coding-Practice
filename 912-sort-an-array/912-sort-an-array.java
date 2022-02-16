c0a725cd04b9f22735056b8c6e3f4f40f2d85c28
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-15 17:51:53 -0800

    Time: 1159 ms (14.43%), Space: 73.4 MB (18.70%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.java
+++ 912-sort-an-array/912-sort-an-array.java
@@ -13,30 +13,13 @@ class Solution {
         sort(nums, i, pivot);
         sort(nums, pivot + 1, j);
     }
-    private int[] merge(int[] left, int[] right) {
-        if (left.length == 0) {
-            return right;
-        }
-        if (right.length == 0) {
-            return left;
-        }
-        int[] res = new int[left.length + right.length];
-        int index = 0;
-        for (int i = 0; i < left.length; ++i) {
-            res[index++] = left[i];
-        }
-        for (int i = 0; i < right.length; ++i) {
-            res[index++] = right[i];
-        }
-        return res;
-    }
     private int partition(int[] nums, int i, int j) {
         int pivot = nums[i];
         while (i < j) {
             while (i < j && nums[j] >= pivot) {
                 j--;
             }
-            nums[i] = nums[j];
+            nums[i]= nums[j];
             while (i < j && nums[i] <= pivot) {
                 i++;
             }
