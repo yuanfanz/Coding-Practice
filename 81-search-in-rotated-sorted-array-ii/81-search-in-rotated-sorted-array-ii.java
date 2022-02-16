fbc2d73673fd52413aba92a0bcaf1a1ce1f9c7e2
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-15 17:48:32 -0800

    Time: 0 ms (100.00%), Space: 42.1 MB (29.97%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.java
@@ -8,17 +8,17 @@ class Solution {
             if (nums[mid] == target) {
                 return true;
             }
-            if (nums[mid] > nums[i] || nums[mid] > nums[j]) {
+            if (nums[mid] < nums[j] || nums[mid] < nums[i]) {
+                if (target <= nums[j] && target >= nums[mid]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
+                }
+            } else if (nums[mid] > nums[i] || nums[mid] > nums[j]) {
                 if (target <= nums[mid] && target >= nums[i]) {
                     j = mid - 1;
                 } else {
                     i = mid + 1;
-                }
-            } else if (nums[mid] < nums[j] || nums[mid] < nums[i]) {
-                if (target >= nums[mid] && target <= nums[j]) {
-                    i = mid + 1;
-                } else {
-                    j = mid - 1;
                 }
             } else {
                 i++;
