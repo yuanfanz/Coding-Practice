6c0a5b4d04148753c65f74810d1a4c7824010a2c
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-15 17:29:07 -0800

    Time: 58 ms (5.10%), Space: 54.9 MB (14.28%) - LeetHub

--- 759-employee-free-time/759-employee-free-time.java
+++ 759-employee-free-time/759-employee-free-time.java
@@ -1,43 +1,31 @@
 
 class Solution {
     public List<Interval> employeeFreeTime(List<List<Interval>> schedule) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>
-            ((a, b) -> schedule.get(a[0]).get(a[1]).start - schedule.get(b[0]).get(b[1]).start);
-        
-        for (int i = 0; i < schedule.size(); ++i) {
-            // add first shift of this employee
-            pq.offer(new int[]{i, 0});
-        }
         List<Interval> result = new ArrayList<>();
-        int prevEnd = schedule.get(pq.peek()[0]).get(pq.peek()[1]).end;
-        while (pq.size() != 0) {
-            int[] cur = pq.poll();
-            Interval interval = schedule.get(cur[0]).get(cur[1]);
-            if (prevEnd < interval.start) {
-                result.add(new Interval(prevEnd, interval.start));
+        
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (List<Interval> list : schedule) {
+            for (Interval cur : list) {
+                map.put(cur.start, map.getOrDefault(cur.start, 0) + 1);
+                map.put(cur.end, map.getOrDefault(cur.end, 0) - 1);
             }
-            prevEnd = Math.max(prevEnd, interval.end);
-            if (schedule.get(cur[0]).size() > cur[1] + 1) {
-                // add next shift of this employee
-                pq.offer(new int[]{cur[0], cur[1] + 1});
+        }
+        int start = -1;
+        int sum = 0;
+        for (int key : map.keySet()) {
+            sum += map.get(key);
+            if (sum == 0) {
+                if (start == -1) {
+                    start = key;
+                }
+            } else {
+                if (start != -1) {
+                    int end = key;
+                    result.add(new Interval(start, end));
+                    start = -1;
+                }
             }
         }
         return result;
     }
 }
-
-
-/*
-// Definition for an Interval.
-class Interval {
-    public int start;
-    public int end;
-
-    public Interval() {}
-
-    public Interval(int _start, int _end) {
-        start = _start;
-        end = _end;
-    }
-};
-*/
