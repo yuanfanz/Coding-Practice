4b551def8c4b735619770a95f513e44acb50d976
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-16 17:04:21 -0800

    Time: 39 ms (52.95%), Space: 43.2 MB (22.02%) - LeetHub

--- 133-clone-graph/133-clone-graph.java
+++ 133-clone-graph/133-clone-graph.java
@@ -5,32 +5,33 @@ class Solution {
         if (node == null) {
             return node;
         }
-        Set<Node> set = getNodes(node);
+        Set<Node> set = getNode(node);
         Map<Node, Node> map = new HashMap<>();
-        
         for (Node cur : set) {
             Node copy = new Node(cur.val);
+            copy.neighbors = new ArrayList<>();
             map.put(cur, copy);
         }
         for (Node cur : set) {
             Node copy = map.get(cur);
             for (Node next : cur.neighbors) {
-                Node copyNext = map.get(next);
-                copy.neighbors.add(copyNext);
+                Node neighbor = map.get(next);
+                copy.neighbors.add(neighbor);
             }
         }
         return map.get(node);
     }
-    private Set<Node> getNodes(Node node) {
+    private Set<Node> getNode(Node node) {
         Set<Node> set = new HashSet<>();
+        set.add(node);
         Queue<Node> queue = new LinkedList<>();
         queue.offer(node);
         while (queue.size() != 0) {
             Node cur = queue.poll();
-            if (set.contains(cur)) continue;
-            set.add(cur);
             for (Node next : cur.neighbors) {
-                queue.offer(next);
+                if (set.add(next)) {
+                    queue.offer(next);
+                }
             }
         }
         return set;
