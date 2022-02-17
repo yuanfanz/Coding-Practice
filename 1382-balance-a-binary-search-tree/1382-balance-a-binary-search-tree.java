1c62c5682069dda2272140476ee46b274f6c0dc5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-16 16:50:52 -0800

    Time: 11 ms (12.70%), Space: 46.2 MB (66.51%) - LeetHub

--- 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -1,27 +1,50 @@
 
 class Solution {
     public TreeNode balanceBST(TreeNode root) {
-        List<Integer> arr = getArr(root);
-        return dfs(arr, 0, arr.size() - 1);
-    }
-    private TreeNode dfs(List<Integer> list, int start, int end) {
-        if (start > end) {
-            return null;
+        List<Integer> list = getList(root);
+        int[] arr = new int[list.size()];
+        int index = 0;
+        for (int i : list) {
+            arr[index++] = i;
         }
-        int mid = start + (end - start) / 2;
-        TreeNode root = new TreeNode(list.get(mid));
-        root.left = dfs(list, start, mid - 1);
-        root.right = dfs(list, mid + 1, end);
-        return root;
+        return getTree(arr, 0, arr.length - 1);
     }
-    private List<Integer> getArr(TreeNode root) {
+    private List<Integer> getList(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if (root == null) {
             return result;
         }
-        result.addAll(getArr(root.left));
+        result.addAll(getList(root.left));
         result.add(root.val);
-        result.addAll(getArr(root.right));
+        result.addAll(getList(root.right));
         return result;
     }
+    private TreeNode getTree(int[] arr, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return new TreeNode(arr[start]);
+        }
+        int mid = start + (end - start) / 2;
+        TreeNode root = new TreeNode(arr[mid]);
+        root.left = getTree(arr, start, mid - 1);
+        root.right = getTree(arr, mid + 1, end);
+        return root;
+    }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
