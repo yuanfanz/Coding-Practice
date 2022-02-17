27ef26abe4c5a381e40bde94715d8c8505108385
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-17 12:33:23 -0800

    Time: 46 ms (27.74%), Space: 43.4 MB (69.69%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -1,25 +1,25 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
         Map<Character, Integer> map = new HashMap<>();
-        for (char ch : tasks) {
-            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        
+        for (char c : tasks) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         for (char ch : map.keySet()) {
             pq.offer(map.get(ch));
         }
-        
-        Map<Integer, Integer> cooldown = new HashMap<>();
         int total = 0;
-        while (pq.size() != 0 || cooldown.size() != 0) {
-            if (cooldown.containsKey(total - n - 1)) {
-                pq.offer(cooldown.remove(total - n - 1));
+        Map<Integer, Integer> timeMap = new HashMap<>();
+        while (pq.size() != 0 || timeMap.size() != 0) {
+            if (timeMap.containsKey(total - n - 1)) {
+                pq.offer(timeMap.remove(total - n - 1));
             }
             if (pq.size() != 0) {
                 int cur = pq.poll();
                 cur--;
                 if (cur != 0) {
-                    cooldown.put(total, cur);
+                    timeMap.put(total, cur);
                 }
             }
             total++;
