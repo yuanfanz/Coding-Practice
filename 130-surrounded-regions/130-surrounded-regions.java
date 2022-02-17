a5b32ba6c763adfe12c310d818842be9c3174377
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-17 12:00:05 -0800

    Time: 7 ms (18.53%), Space: 52.2 MB (13.14%) - LeetHub

--- 130-surrounded-regions/130-surrounded-regions.java
+++ 130-surrounded-regions/130-surrounded-regions.java
@@ -2,23 +2,26 @@ class Solution {
     public void solve(char[][] board) {
         int m = board.length;
         int n = board[0].length;
+        
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        for (int j = 0; j < n; ++j) {
-            if (board[0][j] == 'O') {
-                dfs(0, j, board, dirs);
-            }
-            if (board[m - 1][j] == 'O') {
-                dfs(m - 1, j, board, dirs);
-            }
-        }
-        for (int i = 1; i < m - 1; ++i) {
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; ++i) {
             if (board[i][0] == 'O') {
-                dfs(i, 0, board, dirs);
+                queue.offer(new int[]{i, 0});
             }
             if (board[i][n - 1] == 'O') {
-                dfs(i, n - 1, board, dirs);
+                queue.offer(new int[]{i, n - 1});
             }
         }
+        for (int j = 0; j < n; ++j) {
+            if (board[0][j] == 'O') {
+                queue.offer(new int[]{0, j});
+            }
+            if (board[m - 1][j] == 'O') {
+                queue.offer(new int[]{m - 1, j});
+            }
+        }
+        mark(queue, dirs, board);
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (board[i][j] == 'O') {
@@ -30,19 +33,37 @@ class Solution {
             }
         }
     }
-    private void dfs(int i, int j, char[][] board, int[][] dirs) {  
-        if (board[i][j] == 'O') {
+    private void mark(Queue<int[]> queue, int[][] dirs, char[][] board) {
+        while (queue.size() != 0) {
+            int[] cur = queue.poll();
+            int i = cur[0];
+            int j = cur[1];
             board[i][j] = '*';
-        }
-        for (int[] dir : dirs) {
-            int row = i + dir[0];
-            int col = j + dir[1];
-            if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
-                continue;
-            }
-            if (board[row][col] == 'O') {
-                dfs(row, col, board, dirs);
+            for (int[] dir : dirs) {
+                int row = i + dir[0];
+                int col = j + dir[1];
+                if (row < 0 || col < 0 || row >= board.length || col >= board[0].length) {
+                    continue;
+                }
+                if (board[row][col] == 'O') {
+                    queue.offer(new int[]{row, col});
+                }
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
