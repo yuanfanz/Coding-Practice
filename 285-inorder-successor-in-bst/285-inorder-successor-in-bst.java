c1f59fb77d598b1b776f69ad40e3e05415777826
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-17 22:02:10 -0800

    Time: 5 ms (28.18%), Space: 49 MB (22.36%) - LeetHub

---
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        Stack<TreeNode> stack = new Stack<>();
+        addAll(root, stack);
+        while (stack.size() > 0) {
+            TreeNode node = stack.pop();
+            addAll(node.right, stack);
+            if (node == p) {
+                return stack.isEmpty() ? null : stack.pop();
+            }
+        }
+        return root;
+    }
+    private void addAll(TreeNode root, Stack<TreeNode> stack) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}
