e95c2fc092c478fe5e65fcbf85886292981067fd
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-17 18:33:26 -0800

    Time: 21 ms (38.81%), Space: 45.8 MB (21.44%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+        if (k == num.length()) {
+            return "0";
+        }
+        Stack<Character> stack = new Stack<>();
+        
+        char[] arr = num.toCharArray();
+        for (char ch : arr) {
+            while (k > 0 && stack.size() > 0 && ch < stack.peek()) {
+                stack.pop();
+                k--;
+            }
+            stack.push(ch);
+        }
+        while (stack.size() > 0 && k > 0) {
+            stack.pop();
+            k--;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (stack.size() > 0) {
+            sb.insert(0, stack.pop());
+        }
+        while (sb.length() > 1 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        // return sb.length() > 0 ? sb.toString() : "0";
+        return sb.toString();
+    }
+}
