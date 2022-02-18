fc95841c28ed023f9377cf0450bc1df6ef7f4ca6
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-17 19:43:36 -0800

    Time: 901 ms (53.18%), Space: 43 MB (70.18%) - LeetHub

--- 254-factor-combinations/254-factor-combinations.java
+++ 254-factor-combinations/254-factor-combinations.java
@@ -7,10 +7,11 @@ class Solution {
     }
     private void dfs(List<List<Integer>> result, List<Integer> list,
                     int n, int index) {
-        if (n == 1){
+        if (n == 1) {
             if (list.size() > 1) {
                 result.add(new ArrayList<>(list));
             }
+            return;
         }
         for (int i = index; i <= n; ++i) {
             if (n % i == 0) {
