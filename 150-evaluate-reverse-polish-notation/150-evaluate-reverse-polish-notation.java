5cd23f58772842a07d2fad94b1f4dfcfdf12ce88
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-17 18:47:25 -0800

    Time: 5 ms (78.91%), Space: 42.1 MB (34.72%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.java
@@ -1,30 +1,30 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
-        
-        for (String cur : tokens) {
-            if (Character.isDigit(cur.charAt(cur.length() - 1))) {
-                int num = Integer.valueOf(cur);
-                stack.push(num);
+        int num = 0;
+        for (String s : tokens) {
+            if (!Character.isDigit(s.charAt(s.length() - 1))) {
+                int first = stack.pop();
+                int second = stack.pop();
+                if (s.equals("+")) {
+                    stack.push(first + second);
+                }
+                if (s.equals("-")) {
+                    stack.push(second - first);
+                }
+                if (s.equals("*")) {
+                    stack.push(first * second);
+                }
+                if (s.equals("/")) {
+                    stack.push(second / first);
+                }
             } else {
-                if (cur.equals("+")) {
-                    stack.push(stack.pop() + stack.pop());
-                }
-                if (cur.equals("-")) {
-                    stack.push(-stack.pop() + stack.pop());
-                }
-                if (cur.equals("*")) {
-                    stack.push(stack.pop() * stack.pop());
-                }
-                if (cur.equals("/")) {
-                    int first = stack.pop();
-                    int second = stack.pop();
-                    stack.push(second/first);
-                }
+                num = Integer.valueOf(s);
+                stack.push(num);
             }
         }
         int res = 0;
-        while (!stack.isEmpty()) {
+        while (stack.size() > 0) {
             res += stack.pop();
         }
         return res;
