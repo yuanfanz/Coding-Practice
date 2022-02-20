fedad7f59d2906320cc9db949742ef18790be101
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-19 17:33:57 -0800

    Time: 128 ms (53.32%), Space: 128.4 MB (26.82%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -4,8 +4,38 @@ class NumArray {
         root = buildTree(nums, 0, nums.length - 1);
     }
     
+    private Node buildTree(int[] nums, int start, int end) {
+        if (start > end) {
+            return null;
+        }
+        if (start == end) {
+            return new Node(start, end, nums[start]);
+        }
+        int mid = start + (end - start) / 2;
+        Node left = buildTree(nums, start, mid);
+        Node right = buildTree(nums, mid + 1, end);
+        Node root = new Node(start, end, left.sum + right.sum);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+    
     public void update(int index, int val) {
         update(root, index, val);
+    }
+    
+    private void update(Node root, int index, int val) {
+        if (root == null) return;
+        if (index < root.start) return;
+        if (index > root.end) return;
+        
+        if (index == root.start && index == root.end) {
+            root.sum = val;
+            return;
+        }
+        update(root.left, index, val);
+        update(root.right, index, val);
+        root.sum = root.left.sum + root.right.sum;
     }
     
     public int sumRange(int left, int right) {
@@ -24,52 +54,19 @@ class NumArray {
         
         int left = sumRange(root.left, start, end);
         int right = sumRange(root.right, start, end);
-        
         return left + right;
     }
     
-    private void update(Node root, int index, int val) {
-        if (root == null) return;
-        if (index < root.start) return;
-        if (index > root.end) return;
-        
-        if (root.start == index && root.end == index) {
-            root.sum = val;
-            return;
-        }
-        update(root.left, index, val);
-        update(root.right, index, val);
-        root.sum = root.left.sum + root.right.sum;
-    }
-    
-    private Node buildTree(int[] nums, int start, int end) {
-        if (nums == null || nums.length == 0) return null;
-        if (start == end) {
-            return new Node(start, end, nums[start]);
-        }
-        int mid = start + (end - start) / 2;
-        Node left = buildTree(nums, start, mid);
-        Node right = buildTree(nums, mid + 1, end);
-        
-        Node root = new Node(start, end, left.sum + right.sum);
-        root.left = left;
-        root.right = right;
-        
-        return root;
-    }
-    
     class Node{
+        Node left;
+        Node right;
         int start;
         int end;
         int sum;
-        Node left;
-        Node right;
         public Node(int start, int end, int sum) {
             this.start = start;
             this.end = end;
             this.sum = sum;
-            this.left = null;
-            this.right = null;
         }
     }
 }
