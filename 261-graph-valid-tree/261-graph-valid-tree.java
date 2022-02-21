e8bed5ec88e4fa8c723f74053b663b6d6d8e669b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-20 19:52:32 -0800

    Time: 3 ms (53.49%), Space: 47.5 MB (14.65%) - LeetHub

--- 261-graph-valid-tree/261-graph-valid-tree.java
+++ 261-graph-valid-tree/261-graph-valid-tree.java
@@ -1,6 +1,7 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        List[] map = new ArrayList[n];
+        ArrayList[] map = new ArrayList[n];
+        
         for (int i = 0; i < n; ++i) {
             map[i] = new ArrayList<>();
         }
@@ -11,30 +12,29 @@ class Solution {
             map[second].add(first);
         }
         int[] visited = new int[n];
-        if (dfs(0, -1, visited, map)) {
+        if (dfs(visited, map, 0, -1)) {
             return false;
         }
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < visited.length; ++i) {
             if (visited[i] == 0) {
                 return false;
             }
         }
         return true;
     }
-    private boolean dfs(int cur, int parent, int[] visited, List[] map) {
+    private boolean dfs(int[] visited, ArrayList[] map, int cur, int parent) {
         visited[cur] = 1;
         for (int i = 0; i < map[cur].size(); ++i) {
             int next = (int) map[cur].get(i);
             if (next == parent) continue;
-            if (visited[next] == 1) {
-                return true;
-            }
+            if (visited[next] == 1) return true;
             if (visited[next] == 0) {
-                if (dfs(next, cur, visited, map)) {
+                if (dfs(visited, map, next, cur)) {
                     return true;
                 }
             }
         }
+        visited[cur] = 2;
         return false;
     }
 }
