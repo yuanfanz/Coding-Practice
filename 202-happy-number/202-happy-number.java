41bd45f9b219a758c706f3e6256a0ac2358fb82d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-20 19:55:43 -0800

    Time: 3 ms (37.59%), Space: 41.3 MB (18.03%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,23 +1,19 @@
 class Solution {
     public boolean isHappy(int n) {
         Set<Integer> set = new HashSet<>();
-        return helper(n, set);
-    }
-    private boolean helper(int n, Set<Integer> set) {
-        if (n == 1) {
-            return true;
-        }
+        while (n != 1) {
             if (set.contains(n)) {
                 return false;
             }
             set.add(n);
             int sum = 0;
-            int num = n;
-            while (num > 0) {
-                int rem = num % 10;
+            while (n > 0) {
+                int rem = n % 10;
+                n /= 10;
                 sum += rem * rem;
-                num /= 10;
             }
-            return helper(sum, set);
+            n = sum;
+        }
+        return true;
     }
 }
