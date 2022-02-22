17721c8adb7e81fb86fdea8bd0004fe3c08dc35e
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-21 16:03:16 -0800

    Time: 10 ms (58.01%), Space: 54.5 MB (32.09%) - LeetHub

--- 271-encode-and-decode-strings/271-encode-and-decode-strings.java
+++ 271-encode-and-decode-strings/271-encode-and-decode-strings.java
@@ -2,6 +2,9 @@ public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
+        if (strs.size() == 0) {
+            return "";
+        }
         StringBuilder sb = new StringBuilder();
         for (String s : strs) {
             sb.append(s.length());
@@ -13,14 +16,17 @@ public class Codec {
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
+        if (s.length() == 0) {
+            return new ArrayList<>();
+        }
         List<String> result = new ArrayList<>();
         int i = 0;
         while (i < s.length()) {
             int index = s.indexOf("/", i);
             int len = Integer.valueOf(s.substring(i, index));
             i = index + len + 1;
-            String str = String.valueOf(s.substring(index + 1, i));
-            result.add(str);
+            String cur = s.substring(index + 1, i);
+            result.add(cur);
         }
         return result;
     }
