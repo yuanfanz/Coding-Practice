4ae120f85239199a4de9f66a6e8035f330675c65
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-23 14:32:33 -0800

    Time: 1 ms (99.01%), Space: 43.6 MB (23.77%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -1,32 +1,17 @@
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
+        return dfs(root, 1, 0, new ArrayList<>());
+    }
+    private int dfs(TreeNode root, int id, int depth, List<Integer> leftMost) {
         if (root == null) {
             return 0;
         }
-        Queue<TreeNode> queue = new LinkedList<>();
-        Map<TreeNode, Integer> map = new HashMap<>();
-        queue.offer(root);
-        map.put(root, 1);
-        int max = Integer.MIN_VALUE;
-        while (queue.size() > 0) {
-            int size = queue.size();
-            int start = 0;
-            int end = 0;
-            for (int i = 0; i < size; ++i) {
-                TreeNode cur = queue.poll();
-                if (i == 0) start = map.get(cur);
-                if (i == size - 1) end = map.get(cur);
-                if (cur.left != null) {
-                    map.put(cur.left, map.get(cur) * 2);
-                    queue.offer(cur.left);
-                }
-                if (cur.right != null) {
-                    map.put(cur.right, map.get(cur) * 2 + 1);
-                    queue.offer(cur.right);
-                }
-            }
-            max = Math.max(max, end - start + 1);
+        if (depth >= leftMost.size()) {
+            leftMost.add(id);
         }
-        return max;
+        int left = dfs(root.left, id * 2, depth + 1, leftMost);
+        int right = dfs(root.right, id * 2 + 1, depth + 1, leftMost);
+        int max = Math.max(left, right);
+        return Math.max(max, id - leftMost.get(depth) + 1);
     }
 }
