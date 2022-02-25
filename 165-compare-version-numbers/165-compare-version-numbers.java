ac286f1ecfdda3e4d338489bb5f97f4f1177df52
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-24 19:23:01 -0800

    Time: 1 ms (90.83%), Space: 42.3 MB (11.72%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,34 +1,18 @@
 class Solution {
     public int compareVersion(String version1, String version2) {
-        String[] arr1 = version1.split("\\.");
-        String[] arr2 = version2.split("\\.");
-        
-        if (arr1.length > arr2.length) {
-            return -compareVersion(version2, version1);
-        }
-        for (int i = 0; i < arr1.length; ++i) {
-            if (compare(arr1[i], arr2[i]) == 0) {
-                continue; 
-            }
-            return compare(arr1[i], arr2[i]);
-        }
-        if (arr2.length > arr1.length) {
-            for (int i = arr1.length; i < arr2.length; ++i) {
-                if (Integer.valueOf(arr2[i]) == 0) {
-                    continue;
-                }
-                return -1;
+        String[] levels1 = version1.split("\\.");
+        String[] levels2 = version2.split("\\.");
+
+        int length = Math.max(levels1.length, levels2.length);
+        for (int i=0; i<length; i++) {
+            Integer v1 = i < levels1.length ? Integer.parseInt(levels1[i]) : 0;
+            Integer v2 = i < levels2.length ? Integer.parseInt(levels2[i]) : 0;
+            int compare = v1.compareTo(v2);
+            if (compare != 0) {
+                return compare;
             }
         }
+
         return 0;
-    }
-    private int compare(String s1, String s2) {
-        if (Integer.valueOf(s1) < Integer.valueOf(s2)) {
-            return -1;
-        } else if (Integer.valueOf(s1) > Integer.valueOf(s2)) {
-            return 1;
-        } else {
-            return 0;
-        }
     }
 }
