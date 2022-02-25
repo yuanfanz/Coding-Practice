3b4e93724ad9d86240a5a6de51b23592f7288e78
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-24 18:04:12 -0800

    Time: 12 ms (15.01%), Space: 121.8 MB (5.56%) - LeetHub

---
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (original.length != m * n) {
+            return new int[0][0];
+        }
+        int[][] result = new int[m][n];
+        for (int i = 0; i < original.length; ++i) {
+            int row = i / n;
+            int col = i % n;
+            result[row][col] = original[i];
+        }
+        return result;
+    }
+}
