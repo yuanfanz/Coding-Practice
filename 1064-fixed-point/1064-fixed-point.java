38da892abc448228ba5b5bc0d01a8f576533ba10
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-24 21:30:16 -0800

    Time: 0 ms (100.00%), Space: 42 MB (85.69%) - LeetHub

--- 1064-fixed-point/1064-fixed-point.java
+++ 1064-fixed-point/1064-fixed-point.java
@@ -4,12 +4,10 @@ class Solution {
         int j = arr.length - 1;
         while (i <= j) {
             int mid = i + (j - i) / 2;
-            if (arr[mid] == mid) {
+            if (arr[mid] >= mid) {
                 j = mid - 1;
-            } else if (arr[mid] < mid) {
-                i = mid + 1;
             } else {
-                j = mid - 1;
+                i = mid + 1;
             }
         }
         if (i >= arr.length || arr[i] != i) {
