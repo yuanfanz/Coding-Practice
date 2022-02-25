9e22c14cec7e8a62f7054de48fd86353223414f0
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-24 20:03:43 -0800

    Time: 16 ms (38.51%), Space: 44.9 MB (14.57%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -3,7 +3,6 @@ class Solution {
         int n = s.length();
         
         boolean[][] isPalin = new boolean[n][n];
-        
         int count = 0;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j <= i; ++j) {
