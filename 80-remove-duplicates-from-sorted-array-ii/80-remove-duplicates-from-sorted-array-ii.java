74282d99f9e2dab66cd3edb4428ceb18e84745c5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-24 20:00:45 -0800

    Time: 1 ms (78.31%), Space: 44.6 MB (50.51%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -2,7 +2,9 @@ class Solution {
     public int removeDuplicates(int[] nums) {
         int i = 2;
         for (int j = 2; j < nums.length; ++j) {
-            if (nums[j] == nums[i - 1] && nums[j] == nums[i - 2]) continue;
+            if (nums[j] == nums[i - 1] && nums[j] == nums[i - 2]) {
+                continue;
+            }
             nums[i++] = nums[j];
         }
         return i;
