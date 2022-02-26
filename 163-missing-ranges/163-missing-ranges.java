7077278a112445c4486c41133616e70de8e2dfd7
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-26 08:03:54 -0800

    Time: 11 ms (27.28%), Space: 42.8 MB (6.81%) - LeetHub

--- 163-missing-ranges/163-missing-ranges.java
+++ 163-missing-ranges/163-missing-ranges.java
@@ -1,17 +1,20 @@
 class Solution {
     public List<String> findMissingRanges(int[] nums, int lower, int upper) {
         List<String> result = new ArrayList<>();
-        if (nums == null || nums.length == 0) {
+        if (nums.length == 0) {
             result.add(formRange(lower, upper));
             return result;
         }
+        
         if (nums[0] > lower) {
             result.add(formRange(lower, nums[0] - 1));
         }
-        for (int i = 0; i < nums.length - 1; ++i) {
+        int i = 0;
+        while (i < nums.length - 1) {
             if (nums[i] < nums[i + 1] - 1) {
                 result.add(formRange(nums[i] + 1, nums[i + 1] - 1));
             }
+            i++;
         }
         if (nums[nums.length - 1] < upper) {
             result.add(formRange(nums[nums.length - 1] + 1, upper));
@@ -21,8 +24,7 @@ class Solution {
     private String formRange(int low, int high) {
         if (low == high) {
             return low + "";
-        } else {
-            return low + "->" + high;
         }
+        return low + "->" + high;
     }
 }
