229b1e8f6e69b67c4db0911f984050904ae35caf
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-27 12:19:58 -0800

    Time: 33 ms (55.37%), Space: 55 MB (15.27%) - LeetHub

--- 407-trapping-rain-water-ii/407-trapping-rain-water-ii.java
+++ 407-trapping-rain-water-ii/407-trapping-rain-water-ii.java
@@ -3,9 +3,8 @@ class Solution {
         int m = heightMap.length;
         int n = heightMap[0].length;
         
-        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        boolean[][] visited = new boolean[m][n];
         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        boolean[][] visited = new boolean[m][n];
         for (int i = 0; i < m; ++i) {
             pq.offer(new int[]{i, 0, heightMap[i][0]});
             pq.offer(new int[]{i, n - 1, heightMap[i][n - 1]});
@@ -16,21 +15,24 @@ class Solution {
             pq.offer(new int[]{m - 1, j, heightMap[m - 1][j]});
             visited[0][j] = visited[m - 1][j] = true;
         }
+        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         int res = 0;
-        while (pq.size() != 0) {
+        while (pq.size() > 0) {
             int[] cur = pq.poll();
             int i = cur[0];
             int j = cur[1];
-            int h = cur[2];
             for (int[] dir : dirs) {
                 int row = i + dir[0];
                 int col = j + dir[1];
-                if (row < 0 || col < 0 || row >= m || col >= n || visited[row][col]) {
+                if (row < 0 || col < 0 || row >= m || col >= n) {
                     continue;
                 }
-                res += Math.max(0, h - heightMap[row][col]);
+                if (visited[row][col]) {
+                    continue;
+                }
                 visited[row][col] = true;
-                pq.offer(new int[]{row, col, Math.max(h, heightMap[row][col])});
+                res += Math.max(0, cur[2] - heightMap[row][col]);
+                pq.offer(new int[]{row, col, Math.max(cur[2], heightMap[row][col])});
             }
         }
         return res;
