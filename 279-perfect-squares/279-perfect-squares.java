30c358e7f3496cdecb8a958b1b5857dbf17d3c7e
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-27 20:12:29 -0800

    Time: 31 ms (86.36%), Space: 41.5 MB (60.78%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -5,7 +5,7 @@ class Solution {
         dp[0] = 0;
         for (int i = 1; i <= n; ++i) {
             dp[i] = i;
-            for (int j = 1; j * j <= i; ++j) {
+            for (int j = 0; j * j <= i; ++j) {
                 dp[i] = Math.min(dp[i], dp[i - j*j] + 1);
             }
         }
