45d38147c56fd853003d4b1a26f836f73b7b2525
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-27 16:33:05 -0800

    Time: 3 ms (96.61%), Space: 44.3 MB (27.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -1,21 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        
-        int[] dp = new int[n + 1];
         int len = 1;
-        dp[0] = nums[0];
+        int n = nums.length;
+        int[] sequence = new int[n];
+        sequence[len - 1] = nums[0];
         for (int i = 1; i < n; ++i) {
-            if (dp[len - 1] >= nums[i]) {
-                int index = find(dp, nums[i], 0, len - 1);
-                dp[index] = nums[i];
+            if (sequence[len - 1] >= nums[i]) {
+                int index = search(sequence, nums[i], 0, len - 1);
+                sequence[index] = nums[i];
             } else {
-                dp[len++] = nums[i];
+                sequence[len++] = nums[i];
             }
         }
         return len;
     }
-    private int find(int[] nums, int target, int i, int j) {
+    private int search(int[] nums, int target, int i, int j) {
         while (i <= j) {
             int mid = i + (j - i) / 2;
             if (nums[mid] == target) {
