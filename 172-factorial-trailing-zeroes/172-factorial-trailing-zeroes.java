ec2fecc83b0567f26683254f31fd0ac6c9c2adc0
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-27 20:09:35 -0800

    Time: 1 ms (81.44%), Space: 41.5 MB (15.92%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.java
@@ -1,10 +1,9 @@
 class Solution {
     public int trailingZeroes(int n) {
         int count = 0;
-        while (n != 0) {
-            int tmp = n / 5;
-            count += tmp;
-            n = tmp;
+        while (n > 0) {
+            count += n / 5;
+            n /= 5;
         }
         return count;
     }
