428110ba36642ea81a5971ecebeaced232fc4e5e
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-27 20:08:17 -0800

    Time: 45 ms (63.07%), Space: 76.2 MB (55.70%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.java
+++ 310-minimum-height-trees/310-minimum-height-trees.java
@@ -4,16 +4,17 @@ class Solution {
             return new ArrayList<>(Arrays.asList(0));
         }
         HashSet[] map = new HashSet[n];
-        
         for (int i = 0; i < n; ++i) {
             map[i] = new HashSet<>();
         }
+        int[] indegree = new int[n];
         for (int[] cur : edges) {
             int first = cur[0];
             int second = cur[1];
             map[first].add(second);
             map[second].add(first);
         }
+        Queue<Integer> queue = new LinkedList<>();
         List<Integer> leaves = new ArrayList<>();
         for (int i = 0; i < n; ++i) {
             if (map[i].size() == 1) {
@@ -22,18 +23,17 @@ class Solution {
         }
         while (n > 2) {
             n -= leaves.size();
-            List<Integer> newLeaves = new ArrayList<>();
-            for (int i = 0; i < leaves.size(); ++i) {
-                int cur = leaves.get(i);
+            List<Integer> list = new ArrayList<>();
+            for (int cur : leaves) {
                 for (int j = 0; j < map[cur].size(); ++j) {
                     int next = (int) map[cur].iterator().next();
                     map[next].remove(cur);
                     if (map[next].size() == 1) {
-                        newLeaves.add(next);
+                        list.add(next);
                     }
                 }
             }
-            leaves = newLeaves;
+            leaves = list;
         }
         return leaves;
     }
