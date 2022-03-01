6b7d958b57a5293ec363c14e4a3664b5cda25fad
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-28 20:41:34 -0800

    Time: 9 ms (83.36%), Space: 47.3 MB (62.61%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,30 +1,31 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> result = new ArrayList<>();
-        int[] target = getHash(p);
-        int len = p.length();
         
-        for (int i = 0; i < s.length() - len + 1; ++i) {
-            int[] hash = getHash(s.substring(i, i + len));
-            if (isValid(hash, target)) {
+        int[] hash = new int[26];
+        for (int i = 0; i < p.length(); ++i) {
+            hash[p.charAt(i) - 'a']++;
+        }
+        int len = p.length();
+        int j = 0;
+        int[] cur = new int[26];
+        for (int i = 0; i <= s.length() - len; ++i) {
+            while (j < i + len) {
+                cur[s.charAt(j++) - 'a']++;
+            }
+            if (isValid(cur, hash)) {
                 result.add(i);
             }
+            cur[s.charAt(i) - 'a']--;
         }
         return result;
     }
-    private boolean isValid(int[] hash, int[] target) {
-        for (int i = 0; i < hash.length; ++i) {
-            if (hash[i] != target[i]) {
+    private boolean isValid(int[] cur, int[] hash) {
+        for (int i = 0; i < cur.length; ++i) {
+            if (cur[i] != hash[i]) {
                 return false;
             }
         }
         return true;
-    }
-    private int[] getHash(String p) {
-        int[] target = new int[26];
-        for (int i = 0; i < p.length(); ++i) {
-            target[p.charAt(i) - 'a']++;
-        }
-        return target;
     }
 }
