c1d9cb8c2f118bc391f8357348a53c1ddb39745d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-28 21:42:04 -0800

    Time: 61 ms (70.33%), Space: 54.9 MB (35.94%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.java
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.java
@@ -4,24 +4,21 @@ class Solution {
         
         int i = 0;
         int max = 0;
-        int count = 0;
         for (int j = 0; j < s.length(); ++j) {
             char ch = s.charAt(j);
             map.put(ch, map.getOrDefault(ch, 0) + 1);
-            count++;
             while (i < s.length() && map.size() > 2) {
-                int freq = map.get(s.charAt(i));
-                freq--;
+                int count = map.get(s.charAt(i));
                 count--;
-                if (freq == 0) {
+                if (count == 0) {
                     map.remove(s.charAt(i));
                 } else {
-                    map.put(s.charAt(i), freq);
+                    map.put(s.charAt(i), count);
                 }
                 i++;
             }
             if (map.size() <= 2) {
-                max = Math.max(max, count);
+                max = Math.max(max, j - i + 1);
             }
         }
         return max;
