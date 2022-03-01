f9aa3494948b53ff008640073dffae90ed8af03c
Yuanfan Zhang <yuanfanz@uci.edu>
2022-02-28 21:24:42 -0800

    Time: 11 ms (95.12%), Space: 48.3 MB (79.30%) - LeetHub

--- 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.java
+++ 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.java
@@ -1,15 +1,11 @@
 class NumMatrix {
+    int[][] arr;
     int[][] fenwickTree;
-    int[][] nums;
-    int m;
-    int n;
-    
     public NumMatrix(int[][] matrix) {
-        m = matrix.length;
-        n = matrix[0].length;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        arr = new int[m][n];
         fenwickTree = new int[m + 1][n + 1];
-        nums = new int[m][n];
-        
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 update(i, j, matrix[i][j]);
@@ -18,18 +14,18 @@ class NumMatrix {
     }
     
     public void update(int row, int col, int val) {
-        int diff = val - nums[row][col];
-        nums[row][col] = val;
-        for (int i = row + 1; i <= m; i += (i & -i)) {
-            for (int j = col + 1; j <= n; j += (j & -j)) {
+        int diff = val - arr[row][col];
+        arr[row][col] = val;
+        for (int i = row + 1; i < fenwickTree.length; i += (i & -i)) {
+            for (int j = col + 1; j < fenwickTree[0].length; j += (j & -j)) {
                 fenwickTree[i][j] += diff;
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return getSum(row2 + 1, col2 + 1) + getSum(row1, col1) 
-            -getSum(row1, col2 + 1) - getSum(row2 + 1, col1);
+        return getSum(row2 + 1, col2 + 1) + getSum(row1, col1)
+            - getSum(row2 + 1, col1) - getSum(row1, col2 + 1);
     }
     
     private int getSum(int row, int col) {
@@ -49,15 +45,3 @@ class NumMatrix {
  * obj.update(row,col,val);
  * int param_2 = obj.sumRegion(row1,col1,row2,col2);
  */
-
-
-
-
-
-
-
-
-
-
-
-
