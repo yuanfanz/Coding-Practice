b6f3eaa6f0fb702bd900e7c1559cd211da9c3037
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-01 19:22:08 -0800

    Time: 37 ms (74.17%), Space: 137.7 MB (64.30%) - LeetHub

--- 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.java
+++ 2155-all-divisions-with-the-highest-score-of-a-binary-array/2155-all-divisions-with-the-highest-score-of-a-binary-array.java
@@ -1,20 +1,22 @@
 class Solution {
     public List<Integer> maxScoreIndices(int[] nums) {
-        int n = nums.length;
         List<Integer> result = new ArrayList<>();
+        
+        int n = nums.length;
         int[] presum = new int[n + 1];
         for (int i = 0; i < n; ++i) {
             presum[i + 1] = presum[i] + nums[i];
         }
         int max = 0;
-        int[] value = new int[n + 1];
+        int[] value = new int[nums.length + 1];
         for (int i = 0; i <= n; ++i) {
             int left = i - presum[i];
             int right = presum[n] - presum[i];
+            int score = left + right;
             value[i] = left + right;
             max = Math.max(max, left + right);
         }
-        for (int i = 0; i <= n; ++i) {
+        for (int i = 0; i < value.length; ++i) {
             if (max == value[i]) {
                 result.add(i);
             }
