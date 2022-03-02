fae314033ee725991267339b5d2947273fc2b86c
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-02 14:09:33 -0800

    Time: 4 ms (16.78%), Space: 43.1 MB (9.19%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,9 +1,20 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-        int diff = arr[1] - arr[0];
-        for (int i = 1; i < arr.length; ++i) {
-            if (diff != arr[i] - arr[i - 1]) {
+        int min = arr[0];
+        int max = arr[0];
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            set.add(i);
+            min = Math.min(min, i);
+            max = Math.max(max, i);
+        }
+        int diff = max - min;
+        if (diff % (arr.length - 1) != 0) return false;
+        int gap = diff / (arr.length - 1);
+        int n = arr.length;
+        while (--n > 0) {
+            min += gap;
+            if (!set.contains(min)) {
                 return false;
             }
         }
