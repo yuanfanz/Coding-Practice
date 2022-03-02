fe76c64ee79f9cec0d1c70075250a3e6f74e741e
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-01 19:11:23 -0800

    Time: 1 ms (92.98%), Space: 47.2 MB (12.73%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,17 +1,19 @@
 class Solution {
     public int trap(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
+        int i = 0;
+        int j = height.length - 1;
+        int res = 0;
         int leftMax = 0;
         int rightMax = 0;
-        int res = 0;
-        while (left < right) {
-            if (height[left] < height[right]) {
-                leftMax = Math.max(height[left], leftMax);
-                res += leftMax - height[left++];
+        while (i < j) {
+            if (height[i] < height[j]) {
+                leftMax = Math.max(leftMax, height[i]);
+                res += leftMax - height[i];
+                i++;
             } else {
-                rightMax = Math.max(height[right], rightMax);
-                res += rightMax - height[right--];
+                rightMax = Math.max(rightMax, height[j]);
+                res += rightMax - height[j];
+                j--;
             }
         }
         return res;
