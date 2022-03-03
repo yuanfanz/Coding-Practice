8979590fdd1d0bea9cf5c8ce78c5dc08a31bdcf6
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-03 10:24:07 -0800

    Time: 63 ms (61.49%), Space: 73.5 MB (57.11%) - LeetHub

--- 444-sequence-reconstruction/444-sequence-reconstruction.java
+++ 444-sequence-reconstruction/444-sequence-reconstruction.java
@@ -1,56 +1,39 @@
 class Solution {
     public boolean sequenceReconstruction(int[] nums, List<List<Integer>> sequences) {
         int n = nums.length;
-        
-        ArrayList[] map = new ArrayList[n + 1];
-        for (int i = 1; i <= n; ++i) {
-            map[i] = new ArrayList<>();
-        }
         int[] indegree = new int[n + 1];
+        
+        Map<Integer, List<Integer>> map = new HashMap<>();
         for (List<Integer> list : sequences) {
-            for (int i = 0; i < list.size() - 1; ++i) {
-                int first = list.get(i);
-                int second = list.get(i + 1);
-                map[first].add(second);
+            for (int i = 1; i < list.size(); ++i) {
+                int first = list.get(i - 1);
+                int second = list.get(i);
+                List<Integer> cur = map.getOrDefault(first, new ArrayList<>());
+                cur.add(second);
+                map.put(first, cur);
                 indegree[second]++;
             }
         }
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 1; i < indegree.length; ++i) {
             if (indegree[i] == 0) {
                 queue.offer(i);
             }
         }
-        int index = 0;
+        List<Integer> result = new ArrayList<>();
         while (queue.size() > 0) {
             int size = queue.size();
             if (size > 1) return false;
             int cur = queue.poll();
-            if (index == nums.length || cur != nums[index++]) return false;
-            for (int i = 0; i < map[cur].size(); ++i) {
-                int next = (int) map[cur].get(i);
+            result.add(cur);
+            if (!map.containsKey(cur)) continue;
+            for (int next : map.get(cur)) {
                 indegree[next]--;
                 if (indegree[next] == 0) {
                     queue.offer(next);
                 }
             }
         }
-        return index == nums.length;
+        return result.size() == n ? true : false;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
