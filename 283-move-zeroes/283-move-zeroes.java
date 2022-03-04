bc13be2a4b829a1093f4c1ba3a530381b5c83bfc
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-04 11:46:18 -0800

    Time: 2 ms (74.91%), Space: 54.9 MB (20.68%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,12 +1,13 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        if (nums.length < 2) return;
         int i = 0;
         for (int j = 0; j < nums.length; ++j) {
-            if (nums[j] == 0) continue;
-            int tmp = nums[i];
-            nums[i++] = nums[j];
-            nums[j] = tmp;
+            if (nums[j] != 0) {
+                int tmp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = tmp;
+                i++;
+            }
         }
     }
 }
