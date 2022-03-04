88d5bbf702fc3e83efc3effd9eebdc2305e03c5b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-04 11:41:08 -0800

    Time: 39 ms (36.91%), Space: 54.3 MB (38.79%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.java
+++ 85-maximal-rectangle/85-maximal-rectangle.java
@@ -3,28 +3,29 @@ class Solution {
         int m = matrix.length;
         int n = matrix[0].length;
         
-        int[] dp = new int[n];
         int max = 0;
+        int[] dp = new int[n];
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 dp[j] = matrix[i][j] == '1' ? dp[j] + 1 : 0;
             }
-            max = Math.max(max, histogram(dp));
+            max = Math.max(max, getMax(dp));
         }
         return max;
     }
-    private int histogram(int[] nums) {
+
+    private int getMax(int[] nums) {
         Stack<Integer> stack = new Stack<>();
-        int n = nums.length;
+        
         int max = 0;
+        int n = nums.length;
         for (int i = 0; i < nums.length; ++i) {
             if (stack.size() == 0) {
                 stack.push(i);
             } else {
                 while (stack.size() > 0 && nums[i] < nums[stack.peek()]) {
-                    int index = stack.pop();
-                    int height = nums[index];
-                    int width = i - (stack.isEmpty() ? 0 : stack.peek() + 1);
+                    int height = nums[stack.pop()];
+                    int width = i - (stack.size() == 0 ? 0 : stack.peek() + 1);
                     max = Math.max(max, height * width);
                 }
                 stack.push(i);
