dfa3fcb8a2c5ae8cd7d38a81f6a07e0f03d27759
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-04 11:31:43 -0800

    Time: 89 ms (23.21%), Space: 57.4 MB (17.73%) - LeetHub

--- 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.java
+++ 632-smallest-range-covering-elements-from-k-lists/632-smallest-range-covering-elements-from-k-lists.java
@@ -1,26 +1,28 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> nums.get(a[0]).get(a[1]) - nums.get(b[0]).get(b[1]));
         
-        int max = 0;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.size(); ++i) {
-            pq.offer(new int[]{i, 0, nums.get(i).get(0)});
+            pq.offer(new int[]{i, 0});
             max = Math.max(max, nums.get(i).get(0));
         }
-        int range = Integer.MAX_VALUE;
+        int res = Integer.MAX_VALUE;
         int start = -1;
         int end = -1;
         while (pq.size() == nums.size()) {
             int[] cur = pq.poll();
-            if (max - cur[2] < range) {
-                range = max - cur[2];
-                start = cur[2];
+            int val = nums.get(cur[0]).get(cur[1]);
+            if (max - val < res) {
+                res = max - val;
+                start = val;
                 end = max;
             }
             if (cur[1] + 1 < nums.get(cur[0]).size()) {
-                int val = nums.get(cur[0]).get(cur[1] + 1);
-                pq.offer(new int[]{cur[0], cur[1] + 1, val});
-                max = Math.max(max, val);
+                int next = nums.get(cur[0]).get(cur[1] + 1);
+                pq.offer(new int[]{cur[0], cur[1] + 1});
+                max = Math.max(max, next);
             }
         }
         return new int[]{start, end};
