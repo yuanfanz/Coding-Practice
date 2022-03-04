3e742e68ea820f9cff90d3a20da3785a5c713950
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-04 12:13:13 -0800

    Time: 71 ms (9.60%), Space: 54.4 MB (15.82%) - LeetHub

--- 358-rearrange-string-k-distance-apart/358-rearrange-string-k-distance-apart.java
+++ 358-rearrange-string-k-distance-apart/358-rearrange-string-k-distance-apart.java
@@ -3,41 +3,36 @@ class Solution {
         if (k == 0) {
             return s;
         }
-        PriorityQueue<Tuple> pq = new PriorityQueue<>((a, b) -> b.count - a.count);
         Map<Character, Integer> map = new HashMap<>();
         
         for (int i = 0; i < s.length(); ++i) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
+        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
         for (char ch : map.keySet()) {
-            pq.offer(new Tuple(ch, map.get(ch)));
+            pq.offer(ch);
         }
         StringBuilder sb = new StringBuilder();
-        Map<Integer, Tuple> posMap = new HashMap<>();
+        Map<Integer, Character> indexMap = new HashMap<>();
         
-        while (pq.size() > 0 || posMap.size() > 0) {
-            if (posMap.containsKey(sb.length())) {
-                pq.offer(posMap.remove(sb.length()));
+        while (pq.size() > 0 || indexMap.size() > 0) {
+            if (indexMap.containsKey(sb.length())) {
+                pq.offer(indexMap.remove(sb.length()));
             }
             if (pq.size() > 0) {
-                Tuple t = pq.poll();
-                sb.append(t.ch);
-                int count = t.count - 1;
+                char cur = pq.poll();
+                sb.append(cur);
+                int count = map.get(cur);
+                count--;
                 if (count > 0) {
-                    posMap.put(sb.length() + k - 1, new Tuple(t.ch, count));
+                    map.put(cur, count);
+                    indexMap.put(sb.length() + k - 1, cur);
                 }
             } else {
                 return "";
             }
         }
         return sb.toString();
-    }
-    class Tuple{
-        char ch;
-        int count;
-        public Tuple(char ch, int count) {
-            this.ch = ch;
-            this.count = count;
-        }
     }
 }
