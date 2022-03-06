be887cad4a5463738258bd295a8e5425fbac919d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-06 11:10:26 -0800

    Time: 43 ms (59.45%), Space: 56.6 MB (20.56%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -2,29 +2,27 @@ class Solution {
     public int shortestPath(int[][] grid, int k) {
         int m = grid.length;
         int n = grid[0].length;
-        
-        
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+
         boolean[][][] visited = new boolean[m][n][k + 1];
+        
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0, 0});
         int step = 0;
         while (queue.size() > 0) {
             int size = queue.size();
-            for (int g = 0; g < size; ++g) {
+            for (int i = 0; i < size; ++i) {
                 int[] cur = queue.poll();
-                int i = cur[0];
-                int j = cur[1];
-                if (i == m - 1 && j == n - 1) {
+                if (cur[0] == m - 1 && cur[1] == n - 1) {
                     return step;
                 }
                 for (int[] dir : dirs) {
-                    int row = i + dir[0];
-                    int col = j + dir[1];
-                    int curK = cur[2];
+                    int row = cur[0] + dir[0];
+                    int col = cur[1] + dir[1];
                     if (row < 0 || col < 0 || row >= m || col >= n) {
                         continue;
                     }
+                    int curK = cur[2];
                     if (grid[row][col] == 1) {
                         curK++;
                     }
@@ -40,16 +38,3 @@ class Solution {
         return -1;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
