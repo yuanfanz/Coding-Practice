b96dec88889c2278b45793bb6dd1fd89a98332a3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-06 11:00:52 -0800

    Time: 0 ms (100.00%), Space: 41.3 MB (29.44%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countOrders(int n) {
+        int mod = (int) Math.pow(10, 9) + 7;
+        long[] dp = new long[501];
+        dp[1] = 1L;
+        dp[2] = 6L;
+        for (int i = 3; i <= n; ++i) {
+            int spaces = i * 2 - 1;
+            int combinations = spaces * (1 + spaces) / 2;
+            dp[i] = (dp[i - 1] * combinations) % mod;
+        }
+        return (int) dp[n];
+    }
+}
