c09ccc84c6620bde8a4840b3babeea17efb45af5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-06 18:44:25 -0800

    Time: 216 ms (32.36%), Space: 68.2 MB (44.36%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,7 +1,6 @@
 class Solution {
     public int countPrimes(int n) {
         boolean[] notPrime = new boolean[n];
-        
         int count = 0;
         for (int i = 2; i < n; ++i) {
             if (!notPrime[i]) {
