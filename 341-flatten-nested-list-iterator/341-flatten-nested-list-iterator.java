cc79c356b309e544c3b41b63dd0bd4c2cd03e45e
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-09 15:58:52 -0800

    Time: 6 ms (39.06%), Space: 47.2 MB (5.29%) - LeetHub

--- 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.java
+++ 341-flatten-nested-list-iterator/341-flatten-nested-list-iterator.java
@@ -23,13 +23,30 @@ public class NestedIterator implements Iterator<Integer> {
         return false;
     }
     
-    private void addAll(List<NestedInteger> list) {
-        for (int i = list.size() - 1; i >= 0; --i) {
-            stack.push(list.get(i));
+    private void addAll(List<NestedInteger> nestedList) {
+        for (int i = nestedList.size() - 1; i >= 0; --i) {
+            stack.push(nestedList.get(i));
         }
     }
 }
 
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
 /**
  * Your NestedIterator object will be instantiated and called as such:
  * NestedIterator i = new NestedIterator(nestedList);
