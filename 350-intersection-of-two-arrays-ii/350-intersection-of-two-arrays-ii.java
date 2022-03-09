a8626696b89cadb09b3ecb0fac65295f1d8a4d11
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-09 11:46:35 -0800

    Time: 9 ms (14.34%), Space: 44.4 MB (12.82%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.java
@@ -2,18 +2,19 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Map<Integer, Integer> map = new HashMap<>();
         
-        for (int i = 0; i < nums1.length; ++i) {
-            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        for (int i : nums1) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
         List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < nums2.length; ++i) {
-            if (map.containsKey(nums2[i])) {
-                int val = map.get(nums2[i]);
-                if (val > 0) {
-                list.add(nums2[i]);
+        for (int i : nums2) {
+            if (map.containsKey(i)) {
+                list.add(i);
+                int count = map.get(i);
+                map.remove(i);
+                count--;
+                if (count > 0) {
+                    map.put(i, count);
                 }
-                val--;
-                map.put(nums2[i], val);
             }
         }
         return list.stream().mapToInt(i -> i).toArray();
