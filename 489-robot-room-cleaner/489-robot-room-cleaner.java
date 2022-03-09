f27d2c337f3ee154d038993c270c1e251698c123
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-08 22:07:55 -0800

    Time: 20 ms (27.08%), Space: 47.4 MB (8.09%) - LeetHub

--- 489-robot-room-cleaner/489-robot-room-cleaner.java
+++ 489-robot-room-cleaner/489-robot-room-cleaner.java
@@ -1,12 +1,10 @@
-
 class Solution {
     public void cleanRoom(Robot robot) {
         int[][] dirs = new int[][]{{-1,0},{0,1},{1,0},{0,-1}};
-        
         Set<String> set = new HashSet<>();
-        dfs(robot, dirs, set, 0, 0, 0);
+        dfs(set, dirs, robot, 0, 0, 0);
     }
-    private void dfs(Robot robot, int[][] dirs, Set<String> set, int i, int j, int turn) {
+    private void dfs(Set<String> set, int[][] dirs, Robot robot, int turn, int i, int j) {
         set.add(i + "->" + j);
         robot.clean();
         for (int cur = turn; cur < turn + 4; ++cur) {
@@ -14,32 +12,14 @@ class Solution {
             int col = j + dirs[cur % 4][1];
             String next = row + "->" + col;
             if (!set.contains(next) && robot.move()) {
-                dfs(robot, dirs, set, row, col, cur % 4);
+                dfs(set, dirs, robot, cur % 4, row, col);
             }
-            robot.turnLeft();
+            robot.turnRight();
         }
-        robot.turnLeft();
-        robot.turnLeft();
+        robot.turnRight();
+        robot.turnRight();
         robot.move();
-        robot.turnLeft();
-        robot.turnLeft();
+        robot.turnRight();
+        robot.turnRight();
     }
 }
-
-/**
- * // This is the robot's control interface.
- * // You should not implement it, or speculate about its implementation
- * interface Robot {
- *     // Returns true if the cell in front is open and robot moves into the cell.
- *     // Returns false if the cell in front is blocked and robot stays in the current cell.
- *     public boolean move();
- *
- *     // Robot will stay in the same cell after calling turnLeft/turnRight.
- *     // Each turn will be 90 degrees.
- *     public void turnLeft();
- *     public void turnRight();
- *
- *     // Clean the current cell.
- *     public void clean();
- * }
- */
