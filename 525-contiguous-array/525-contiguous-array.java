0d7dbde9c0e461a9779dc40b7fcd7ed03eb0929f
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-08 19:12:21 -0800

    Time: 45 ms (32.01%), Space: 85 MB (29.23%) - LeetHub

--- 525-contiguous-array/525-contiguous-array.java
+++ 525-contiguous-array/525-contiguous-array.java
@@ -5,10 +5,10 @@ class Solution {
                 nums[i] = -1;
             }
         }
-        int sum = 0;
         Map<Integer, Integer> map = new HashMap<>();
         map.put(0, -1);
         int max = 0;
+        int sum = 0;
         for (int i = 0; i < nums.length; ++i) {
             sum += nums[i];
             if (map.containsKey(sum)) {
