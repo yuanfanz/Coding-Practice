6bd2ee42def29d334889326dfeb45b3cb84456e3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-09 16:30:31 -0800

    Time: 7 ms (49.78%), Space: 42.8 MB (63.06%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
         int[] hash1 = getHash(s);
         int[] hash2 = getHash(t);
-        
+        return isValid(hash1, hash2);
+    }
+    private int[] getHash(String s) {
+        int[] hash = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            hash[s.charAt(i) - 'a']++;
+        }
+        return hash;
+    }
+
+    private boolean isValid(int[] hash1, int[] hash2) {
         for (int i = 0; i < hash1.length; ++i) {
             if (hash1[i] != hash2[i]) {
                 return false;
             }
         }
         return true;
-    }
-    private int[] getHash(String s) {
-        int[] hash = new int[256];
-        for (int i = 0; i < s.length(); ++i) {
-            hash[s.charAt(i)]++;
-        }
-        return hash;
     }
 }
