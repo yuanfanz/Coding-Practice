176ff9dddeae0cd9b2a67086136ef4287014c8a9
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-10 10:21:12 -0800

    Time: 287 ms (62.23%), Space: 122.6 MB (53.07%) - LeetHub

--- 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
+++ 2191-sort-the-jumbled-numbers/2191-sort-the-jumbled-numbers.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] sortJumbled(int[] mapping, int[] nums) {
         List<int[]> list = new ArrayList<>();
+        
         for (int num : nums) {
-            StringBuilder sb = new StringBuilder();
             char[] arr = String.valueOf(num).toCharArray();
+            StringBuilder sb = new StringBuilder();
             for (char ch : arr) {
                 sb.append(mapping[ch - '0']);
             }
             list.add(new int[]{Integer.valueOf(sb.toString()), num});
         }
         Collections.sort(list, (a, b) -> a[0] - b[0]);
-        int[] result = new int[nums.length];
         int index = 0;
+        int[] result = new int[nums.length];
         for (int[] cur : list) {
             result[index++] = cur[1];
         }
