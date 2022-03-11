cf6a060dbfca1c590f415f7737450592af17e1d9
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-10 19:44:36 -0800

    Time: 9 ms (64.57%), Space: 45.2 MB (9.95%) - LeetHub

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -1,18 +1,14 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> (b + a).compareTo(a + b));
-        
-        for (int cur : nums) {
-            String str = String.valueOf(cur);
-            pq.offer(str);
-        }
-        if (pq.peek().charAt(0) == '0'){
-            return "0";
+        PriorityQueue<String> pq = new PriorityQueue<>
+            ((a, b) -> (b + a).compareTo(a + b));
+        for (int i : nums) {
+            pq.offer(String.valueOf(i));
         }
         StringBuilder sb = new StringBuilder();
         while (pq.size() > 0) {
             sb.append(pq.poll());
         }
-        return sb.toString();
+        return sb.charAt(0) == '0' ? "0" : sb.toString();
     }
 }
