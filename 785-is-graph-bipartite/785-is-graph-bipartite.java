a4330a94eba58e8d4779fda3888eece416bcaea7
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-13 16:49:10 -0700

    Time: 8 ms (13.42%), Space: 53.7 MB (37.91%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        int[] colors = new int[n];
         
         for (int i = 0; i < n; ++i) {
-            if (colors[i] != 0) continue;
+        int[] colors = new int[n];
             colors[i] = 1;
             if (!dfs(graph, i, colors, 1)) return false;
         }
