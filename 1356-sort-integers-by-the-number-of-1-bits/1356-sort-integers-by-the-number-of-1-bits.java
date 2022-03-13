5a0decf8b62215aa7a6959903c8f954edb6d4b9a
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-13 10:51:39 -0700

    Time: 22 ms (25.43%), Space: 46.3 MB (34.93%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,8 +1,16 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
         Integer[] input = Arrays.stream(arr).boxed().toArray(Integer[]::new);
-        Arrays.sort(input, (a, b) -> Integer.bitCount(a) == Integer.bitCount(b) ? 
-                    a - b : Integer.bitCount(a) - Integer.bitCount(b));
+        Arrays.sort(input, (a, b) -> getBit(a) == getBit(b) ? 
+                    a - b : getBit(a) - getBit(b));
         return Arrays.stream(input).mapToInt(Integer::intValue).toArray();
+    }
+    private int getBit(int a) {
+        int count = 0;
+        while (a > 0) {
+            count += a & 1;
+            a >>>= 1;
+        }
+        return count;
     }
 }
