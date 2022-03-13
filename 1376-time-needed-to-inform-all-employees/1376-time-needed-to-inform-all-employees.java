cf625e2a05b25b1b2ce984dad14295f1dcf59e28
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-13 15:08:55 -0700

    Time: 188 ms (37.03%), Space: 139.9 MB (15.84%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,12 +1,12 @@
 class Solution {
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-        int sum = 0;
-        // build graph
         Map<Integer, List<Integer>> map = new HashMap<>();
+        
         for (int i = 0; i < n; ++i) {
-            List<Integer> list = map.getOrDefault(manager[i], new ArrayList<>());
+            int head = manager[i];
+            List<Integer> list = map.getOrDefault(head, new ArrayList<>());
             list.add(i);
-            map.put(manager[i], list);
+            map.put(head, list);
         }
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{headID, 0});
@@ -14,11 +14,11 @@ class Solution {
         while (queue.size() > 0) {
             int[] cur = queue.poll();
             int node = cur[0];
-            max = Math.max(max, cur[1]);
-            if (map.containsKey(node)) {
-                for (int next : map.get(node)) {
-                    queue.offer(new int[]{next, cur[1] + informTime[node]});
-                }
+            int cost = cur[1];
+            max = Math.max(max, cost);
+            if (!map.containsKey(node)) continue;
+            for (int next : map.get(node)) {
+                queue.offer(new int[]{next, cost + informTime[node]});
             }
         }
         return max;
