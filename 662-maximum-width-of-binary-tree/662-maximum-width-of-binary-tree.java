3c2a809224ba576a19b4b707e9c9ed80942af7dc
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-14 09:18:45 -0700

    Time: 1 ms (99.17%), Space: 44.2 MB (15.17%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.java
@@ -1,37 +1,57 @@
 
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        Map<TreeNode, Integer> map = new HashMap<>();
-        map.put(root, 1);
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
+        Queue<Tuple> queue = new LinkedList<>();
+        queue.offer(new Tuple(root, 1));
+        
         int max = 0;
         while (queue.size() > 0) {
             int size = queue.size();
             int start = 0;
             int end = 0;
             for (int i = 0; i < size; ++i) {
-                TreeNode cur = queue.poll();
+                Tuple t = queue.poll();
+                TreeNode cur = t.node;
+                int index = t.index;
                 if (i == 0) {
-                    start = map.get(cur);
+                    start = index;
                 }
                 if (i == size - 1) {
-                    end = map.get(cur);
+                    end = index;
                 }
                 if (cur.left != null) {
-                    map.put(cur.left, map.get(cur) * 2);
-                    queue.offer(cur.left);
+                    queue.offer(new Tuple(cur.left, index * 2));
                 }
                 if (cur.right != null) {
-                    map.put(cur.right, map.get(cur) * 2 + 1);
-                    queue.offer(cur.right);
+                    queue.offer(new Tuple(cur.right, index * 2 + 1));
                 }
             }
             max = Math.max(max, end - start + 1);
         }
         return max;
     }
+    class Tuple{
+        TreeNode node;
+        int index;
+        public Tuple(TreeNode node, int index) {
+            this.node = node;
+            this.index = index;
+        }
+    }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
