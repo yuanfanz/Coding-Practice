30550ef1b76fc3a3584b4bfeac5e94cd4a057fd6
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-14 10:47:48 -0700

    Time: 17 ms (32.88%), Space: 49.2 MB (16.35%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
@@ -1,16 +1,22 @@
 class Solution {
-    public int[] shortestAlternatingPaths(int n, int[][] red_edges, int[][] blue_edges) {
-        Set<Integer>[][] map = new HashSet[2][n];
+    public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
+        HashSet<Integer>[][] map = new HashSet[2][n];
         
+        // 0 is red
+        // 1 is blue
         for (int i = 0; i < n; ++i) {
             map[0][i] = new HashSet<>();
             map[1][i] = new HashSet<>();
         }
-        for (int[] red : red_edges) {
-            map[0][red[0]].add(red[1]);
+        for (int[] cur : redEdges) {
+            int from = cur[0];
+            int to = cur[1];
+            map[0][from].add(to);
         }
-        for (int[] blue : blue_edges) {
-            map[1][blue[0]].add(blue[1]);
+        for (int[] cur : blueEdges) {
+            int from = cur[0];
+            int to = cur[1];
+            map[1][from].add(to);
         }
         int[] result = new int[n];
         Arrays.fill(result, Integer.MAX_VALUE);
@@ -19,7 +25,7 @@ class Solution {
         Queue<int[]> queue = new LinkedList<>();
         queue.offer(new int[]{0, 0});
         queue.offer(new int[]{0, 1});
-        int len = 1;
+        int step = 1;
         Set<String> visited = new HashSet<>();
         while (queue.size() > 0) {
             int size = queue.size();
@@ -29,12 +35,12 @@ class Solution {
                 int color = cur[1];
                 int nextColor = 1 - color;
                 for (int next : map[nextColor][node]) {
-                    if (!visited.add(next + "" + nextColor)) continue;
+                    if (!visited.add(next + "->" + nextColor)) continue;
                     queue.offer(new int[]{next, nextColor});
-                    result[next] = Math.min(result[next], len);
+                    result[next] = Math.min(result[next], step);
                 }
             }
-            len++;
+            step++;
         }
         for (int i = 1; i < n; i++) {
             if (result[i] == Integer.MAX_VALUE) {
@@ -44,14 +50,6 @@ class Solution {
         return result;
     }
 }
-
-
-
-
-
-
-
-
 
 
 
