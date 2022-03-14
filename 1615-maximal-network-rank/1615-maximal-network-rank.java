a94328374516941170278cb0a09fd729b0344888
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-13 17:26:48 -0700

    Time: 56 ms (15.63%), Space: 54.3 MB (39.47%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.java
+++ 1615-maximal-network-rank/1615-maximal-network-rank.java
@@ -1,17 +1,31 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        boolean[][] connected = new boolean[n][n];
-        int[] cnts = new int[n];
-        for (int[] r : roads) {
-            cnts[r[0]]++;
-            cnts[r[1]]++;
-            connected[r[0]][r[1]] = true;
-            connected[r[1]][r[0]] = true;  // cache if i and j directly connected
+        int[] indegree = new int[n];
+        
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            map.put(i, new ArrayList<>());
         }
-        int res = 0;
-        for (int i = 0; i < n; i++) 
-            for (int j = i + 1; j < n; j++) 
-                res = Math.max(res, cnts[i] + cnts[j] - (connected[i][j] ? 1 : 0));  // loop all pairs
-        return res;
+        for (int[] cur : roads) {
+            int p = cur[0];
+            int q = cur[1];
+            indegree[p]++;
+            indegree[q]++;
+            List<Integer> list1 = map.get(p);
+            List<Integer> list2 = map.get(q);
+            list1.add(q);
+            list2.add(p);
+            map.put(p, list1);
+            map.put(q, list2);
+        }
+        int max = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int count = indegree[i] + indegree[j];
+                if (map.get(i).contains(j)) count--;
+                max = Math.max(max, count);
+            }
+        }
+        return max;
     }
 }
