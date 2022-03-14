d219b2382565099ecc18bc8370df0f8c9fb18770
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-14 11:07:30 -0700

    Time: 47 ms (33.49%), Space: 66 MB (60.70%) - LeetHub

--- 267-palindrome-permutation-ii/267-palindrome-permutation-ii.java
+++ 267-palindrome-permutation-ii/267-palindrome-permutation-ii.java
@@ -1,52 +1,59 @@
 class Solution {
     public List<String> generatePalindromes(String s) {
-        Set<String> set = new HashSet<>();
+        List<String> result = new ArrayList<>();
         
-        int[] map = new int[26];
-        for (int i = 0; i < s.length(); ++i) {
-            map[s.charAt(i) - 'a']++;
-        }
+        int[] hash = new int[26];
         boolean flag = false;
-        for (int i = 0; i < 26; ++i) {
-            if (map[i] % 2 == 1) {
-                if (!flag) {
-                    flag = true;
-                } else return new ArrayList<>();
-            }
-        }
-        int len = 0;
         String mid = "";
-        for (int i = 0; i < 26; ++i) {
-            if (map[i] > 0) {
-                if (map[i] % 2 == 1) {
-                    mid = (char) (i + 'a') + "";
-                    map[i]--;
-                }
-                map[i] = map[i] / 2;
-                len += map[i];
+        int len = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            hash[ch - 'a']++;
+            if (hash[i] * 2 > s.length() + 1) {
+                return result;
             }
         }
-        dfs(set, "", map, len, mid);
+        for (int i = 0; i < 26; ++i) {
+            if (hash[i] % 2 == 1) {
+                if (flag) return result;
+                flag = true;
+                mid = (char) (i + 'a') + "";
+            }
+            hash[i] /= 2;
+            len += hash[i];
+        }
+        Set<String> set = new HashSet<>();
+        dfs(set, new StringBuilder(), hash, len, mid);
         return new ArrayList<>(set);
     }
-    private void dfs(Set<String> set, String cur, int[] map, int len, String mid) {
-        if (len == cur.length()) {
-            String prefix = cur;
-            StringBuilder half = new StringBuilder(cur).reverse();
-            String suffix = half.toString();
-            set.add(prefix + mid + suffix);
+    private void dfs(Set<String> result, StringBuilder sb, int[] hash, int len, String mid) {
+        if (sb.length() == len) {
+            // System.out.println(len);
+            // System.out.println(sb.toString());
+            String prefix = sb.toString();
+            String suffix = new StringBuilder(prefix).reverse().toString();
+            result.add(prefix + mid + suffix);
             return;
         }
-        for (int i = 0; i < map.length; ++i) {
-            if (map[i] > 0) {
-                char ch = (char) (i + 'a');
-                map[i]--;
-                dfs(set, cur + (char) (i + 'a'), map, len, mid);
-                map[i]++;
+            int sbLen = sb.length();
+        for (int i = 0; i < hash.length; ++i) {
+            if (hash[i] > 0) {
+                // System.out.println((char)(i + 'a') + "  " + hash[i]);
+                hash[i]--;
+                sb.append((char)(i + 'a'));
+                // System.out.println("CUR: "+sb.toString());
+                dfs(result, sb, hash, len, mid);
+                sb.setLength(sbLen);
+                // System.out.println("AFT: "+sb.toString());
+                hash[i]++;
             }
         }
     }
 }
+
+
+
+
 
 
 
