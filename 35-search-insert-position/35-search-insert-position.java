65ababa1a33cd17b133ecfa5ac4df1b36e9b420a
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-16 11:08:43 -0700

    Time: 0 ms (100.00%), Space: 41.6 MB (73.48%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -2,16 +2,19 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int i = 0;
         int j = nums.length - 1;
-        while (i < j) {
+        while (i <= j) {
             int mid = i + (j - i) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
                 i = mid + 1;
             } else {
-                j = mid;
+                j = mid - 1;
             }
         }
-        return nums[i] < target ? i + 1 : i;
+        if (j < 0 || nums[j] != target) {
+            return j + 1;
+        }
+        return j + 1;
     }
 }
