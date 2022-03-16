ca7921f91e52473b8acbfb32bbdc7ef8d69be0c1
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-16 11:14:01 -0700

    Time: 22 ms (37.97%), Space: 40.8 MB (38.23%) - LeetHub

---
+++ 278-first-bad-version/278-first-bad-version.java
@@ -0,0 +1,21 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int i = 1;
+        int j = n;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (isBadVersion(mid)) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        if (i > n || !isBadVersion(i)) {
+            return i - 1;
+        }
+        return i;
+    }
+}
