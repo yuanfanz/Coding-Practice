a75136427496b15dd4d3f23d501a1a7afb74e9ea
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-17 16:46:48 -0700

    Time: 7 ms (65.13%), Space: 42.6 MB (65.13%) - LeetHub

---
+++ 1258-synonymous-sentences/1258-synonymous-sentences.java
@@ -0,0 +1,93 @@
+class Solution {
+    public List<String> generateSentences(List<List<String>> synonyms, String text) {
+        Map<String, Integer> map = new HashMap<>();
+        Map<Integer, Set<String>> setMap = new HashMap<>();
+        int n = synonyms.size() * 2;
+        UnionFind uf = new UnionFind(n);
+        int count = 0;
+        for (List<String> list : synonyms) {
+            String first = list.get(0);
+            String second = list.get(1);
+            if (!map.containsKey(first)) map.put(first, count);
+            if (!map.containsKey(second)) map.put(second, count);
+            // int root = uf.find(count);
+            uf.union(map.get(first), map.get(second));
+            count++;
+        }
+        for (List<String> list : synonyms) {
+            for (String s : list) {
+                int root = uf.find(map.get(s));
+                Set<String> set = setMap.getOrDefault(root, new HashSet<>());
+                set.add(s);
+                setMap.put(root, set);
+            }
+        }
+        // print(setMap);
+        Set<String> result = new HashSet<>();
+        String[] strs = text.split(" ");
+        dfs(result, new StringBuilder(), strs, 0, uf, text, setMap, map);
+        List<String> list = new ArrayList<>(result);
+        Collections.sort(list);
+        return list;
+    }
+    private void print(Map<Integer, Set<String>> map) {
+        for (int num : map.keySet()) {
+            System.out.print(num + " ");
+            List<String> list = new ArrayList<>(map.get(num));
+            for (String i : list) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+    private void dfs(Set<String> result, StringBuilder sb, String[] strs, int index, UnionFind uf, String text,
+                     Map<Integer, Set<String>> setMap, Map<String, Integer> map) {
+        if (index == strs.length) {
+            sb.setLength(sb.length() - 1);
+            result.add(sb.toString());
+            return;
+        }
+        int len = sb.length();
+        if (!map.containsKey(strs[index])) {
+            sb.append(strs[index] + " ");
+            dfs(result, sb, strs, index + 1, uf, text, setMap, map);
+            sb.setLength(len);
+        } else {
+            int root = uf.find(map.get(strs[index]));
+            Set<String> set = setMap.get(root);
+            if (set.size() == 0) return;
+            for (String next : set) {
+                sb.append(next + " ");
+                dfs(result, sb, strs, index + 1, uf, text, setMap, map);
+                sb.setLength(len);
+            }
+        }
+    }
+    
+    class UnionFind{
+        int[] parent;
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; ++i) {
+                parent[i] = i;
+            }
+        }
+        public int find(int p) {
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) return;
+            if (rootP < rootQ) {
+                parent[rootP] = rootQ;
+            } else {
+                parent[rootQ] = rootP;
+            }
+        }
+    }
+}
