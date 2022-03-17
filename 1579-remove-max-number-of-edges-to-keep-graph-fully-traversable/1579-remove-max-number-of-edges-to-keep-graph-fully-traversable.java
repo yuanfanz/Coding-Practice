2a0bc1d98e91a8ed33e66da84798c6dc9e1e501d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-17 11:59:49 -0700

    Time: 63 ms (16.39%), Space: 139.1 MB (67.23%) - LeetHub

--- 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
+++ 1579-remove-max-number-of-edges-to-keep-graph-fully-traversable/1579-remove-max-number-of-edges-to-keep-graph-fully-traversable.java
@@ -1,9 +1,10 @@
 class Solution {
     public int maxNumEdgesToRemove(int n, int[][] edges) {
+        Arrays.sort(edges, (a, b) -> b[0] - a[0]);
+        
         UnionFind alice = new UnionFind(n);
         UnionFind bob = new UnionFind(n);
         
-        Arrays.sort(edges, (a, b) -> b[0] - a[0]);
         int count = 0;
         for (int[] cur : edges) {
             int p = cur[1];
@@ -16,7 +17,7 @@ class Solution {
                 if (!alice.union(p, q)) {
                     count++;
                 }
-            } else if (cur[0] == 2) {
+            } else {
                 if (!bob.union(p, q)) {
                     count++;
                 }
@@ -24,16 +25,15 @@ class Solution {
         }
         return (alice.count == 1 && bob.count == 1) ? count : -1;
     }
-    
     class UnionFind{
         int[] parent;
         int count;
         public UnionFind(int n) {
             parent = new int[n + 1];
+            count = n;
             for (int i = 0; i <= n; ++i) {
                 parent[i] = i;
             }
-            count = n;
         }
         public int find(int p) {
             while (p != parent[p]) {
@@ -47,9 +47,9 @@ class Solution {
             int rootQ = find(q);
             if (rootP == rootQ) return false;
             if (rootP < rootQ) {
-                parent[rootP] = rootQ;
-            } else {
                 parent[rootQ] = rootP;
+            } else {
+                parent[rootP] = rootQ;
             }
             count--;
             return true;
