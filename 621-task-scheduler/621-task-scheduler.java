be67cd6c32098896844dcbdf4afba8b473444e6d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-18 10:36:53 -0700

    Time: 153 ms (5.05%), Space: 84.3 MB (5.02%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.java
+++ 621-task-scheduler/621-task-scheduler.java
@@ -6,25 +6,24 @@ class Solution {
             map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        Map<Integer, Integer> timeMap = new HashMap<>();
-        
         for (char ch : map.keySet()) {
             pq.offer(map.get(ch));
         }
-        int total = 0;
+        int time = 0;
+        Map<Integer, Integer> timeMap = new HashMap<>();
         while (pq.size() > 0 || timeMap.size() > 0) {
-            if (timeMap.containsKey(total)) {
-                pq.offer(timeMap.remove(total));
+            if (timeMap.containsKey(time)) {
+                pq.offer(timeMap.remove(time));
             }
             if (pq.size() > 0) {
                 int cur = pq.poll();
                 cur--;
                 if (cur > 0) {
-                    timeMap.put(total + n + 1, cur);
+                    timeMap.put(time + n + 1, cur);
                 }
             }
-            total++;
+            time++;
         }
-        return total;
+        return time;
     }
 }
