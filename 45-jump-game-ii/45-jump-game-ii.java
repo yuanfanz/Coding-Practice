947bd1d9296190fc38ddaa7991641c903fb0185b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-17 17:37:17 -0700

    Time: 2 ms (75.72%), Space: 49.1 MB (56.46%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public int jump(int[] nums) {
-        int jump = 0;
         int max = 0;
+        int jump = 0;
         int end = 0;
         for (int i = 0; i < nums.length - 1; ++i) {
             max = Math.max(max, nums[i] + i);
