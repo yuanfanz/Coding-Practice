deb18b37670c9497bbe715c2fdf17fe6bf7d04ea
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-17 17:36:09 -0700

    Time: 2 ms (88.06%), Space: 68.3 MB (31.61%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -2,14 +2,9 @@ class Solution {
     public boolean canJump(int[] nums) {
         int max = 0;
         for (int i = 0; i < nums.length; ++i) {
+            if (i > max) return false;
             max = Math.max(max, nums[i] + i);
-            if (i == nums.length - 1 || max >= nums.length - 1) {
-                return true;
-            }
-            if (i == max) {
-                return false;
-            }
         }
-        return false;
+        return true;
     }
 }
