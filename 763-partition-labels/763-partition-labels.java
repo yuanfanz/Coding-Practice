2ec8ecc548710c3de6d3325a74fd40dbf75888ae
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-18 21:00:31 -0700

    Time: 3 ms (94.01%), Space: 42.8 MB (42.02%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
         int[] pos = new int[26];
-        for (int i = 0; i < s.length(); ++i) {
-            pos[s.charAt(i) - 'a'] = i;
-        }
-        List<Integer> result = new ArrayList<>();
-        int max = 0;
-        int start = 0;
+        
         for (int i = 0; i < s.length(); ++i) {
             char ch = s.charAt(i);
-            int index = pos[ch - 'a'];
-            max = Math.max(max, index);
-            if (max == i) {
+            pos[ch - 'a'] = i;
+        }
+        List<Integer> result = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            end = Math.max(end, pos[s.charAt(i) - 'a']);
+            if (i == end) {
                 result.add(i + 1 - start);
                 start = i + 1;
             }
