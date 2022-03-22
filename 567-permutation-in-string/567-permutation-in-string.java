7a1ab9cb22201b4f0a5a675ab5fa494e9e82705f
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-21 19:34:26 -0700

    Time: 10 ms (52.92%), Space: 43.8 MB (30.34%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -7,7 +7,7 @@ class Solution {
         int len = s1.length();
         for (int j = 0; j < s2.length(); ++j) {
             hash[s2.charAt(j) - 'a']++;
-            while (j - i + 1 > len) {
+            while (i < s2.length() && j - i + 1 > len) {
                 hash[s2.charAt(i++) - 'a']--;
             }
             if (isValid(hash, target)) {
@@ -16,6 +16,7 @@ class Solution {
         }
         return false;
     }
+    
     private int[] getHash(String s) {
         int[] hash = new int[26];
         for (int i = 0; i < s.length(); ++i) {
@@ -23,6 +24,7 @@ class Solution {
         }
         return hash;
     }
+    
     private boolean isValid(int[] hash1, int[] hash2) {
         for (int i = 0; i < hash1.length; ++i) {
             if (hash1[i] != hash2[i]) {
