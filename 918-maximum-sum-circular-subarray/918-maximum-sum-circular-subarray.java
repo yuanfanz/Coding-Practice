d305cbde177e05b483ee987acfd155355554d487
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-22 09:45:37 -0700

    Time: 4 ms (80.64%), Space: 63.6 MB (61.27%) - LeetHub

--- 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
+++ 918-maximum-sum-circular-subarray/918-maximum-sum-circular-subarray.java
@@ -1,34 +1,23 @@
 class Solution {
     public int maxSubarraySumCircular(int[] nums) {
-        int n = nums.length;
-        
-        int nonCircularMax = maxSubArray(nums);
-        int totalSum = 0;
-        for (int i = 0; i < n; ++i) {
-            totalSum += nums[i];
+        int noncircularMax = maxSub(nums);
+        int total = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            total += nums[i];
             nums[i] = -nums[i];
         }
-        // after inverting every number sign
-        // we can calculate min sub array sum using same method
-        int minSubArraySum = maxSubArray(nums);
-        int circularMax = totalSum + minSubArraySum;
-        
-        // if all numbers are negtive, totalSum == minSubArraySum
-        // circularMax will be 0
-        if (circularMax == 0) {
-            return nonCircularMax;
-        }
-        return Math.max(circularMax, nonCircularMax);
+        int minSub = maxSub(nums);
+        int circularMax = total + minSub;
+        if (circularMax == 0) return noncircularMax;
+        return Math.max(circularMax, noncircularMax);
     }
-    public int maxSubArray(int[] nums) {
-        int n = nums.length;
-        
-        int[] presum = new int[n];
-        presum[0] = nums[0];
+    
+    private int maxSub(int[] nums) {
+        int sum = nums[0];
         int max = nums[0];
-        for (int i = 1; i < n; ++i) {
-            presum[i] = nums[i] + (presum[i - 1] > 0 ? presum[i - 1] : 0);
-            max = Math.max(max, presum[i]);
+        for (int i = 1; i < nums.length; ++i) {
+            sum = nums[i] + (sum > 0 ? sum : 0);
+            max = Math.max(max, sum);
         }
         return max;
     }
