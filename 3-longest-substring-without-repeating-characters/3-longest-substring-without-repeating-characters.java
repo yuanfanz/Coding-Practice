32d53efc1d744e304d7b1edd8762ee14a4ea5558
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-21 19:29:38 -0700

    Time: 18 ms (29.18%), Space: 46.1 MB (30.36%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,22 +1,17 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) {
-            return 0;
-        }
         Set<Character> set = new HashSet<>();
-        int i = 0;
-        int j = 1;
-        int max = 1;
-        set.add(s.charAt(0));
-        while (i <= j && j < s.length()) {
-            if (set.contains(s.charAt(j))) {
-                set.remove(s.charAt(i));
-                i++;
-            } else {
-                set.add(s.charAt(j));
-                max = Math.max(max, (j - i + 1));
-                j++;
+        
+        int j = 0;
+        int max = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            while (j < s.length() && !set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
             }
+            if (j - i > max) {
+                max = j - i;
+            }
+            set.remove(s.charAt(i));
         }
         return max;
     }
