253887a995cde39232d9e43e41ea037674af31de
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-23 11:35:57 -0700

    Time: 0 ms (100.00%), Space: 40.6 MB (62.47%) - LeetHub

--- 556-next-greater-element-iii/556-next-greater-element-iii.java
+++ 556-next-greater-element-iii/556-next-greater-element-iii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int nextGreaterElement(int n) {
         char[] arr = String.valueOf(n).toCharArray();
-        
+        int len = arr.length;
         int i = arr.length - 2;
         while (i >= 0) {
-            if (arr[i] - '0' < arr[i + 1] - '0') {
+            if (arr[i] < arr[i + 1]) {
                 break;
             }
             i--;
         }
         if (i >= 0) {
             int j = arr.length - 1;
-            while (arr[j] - '0' <= arr[i] - '0') {
+            while (j > i && arr[j] <= arr[i]) {
                 j--;
             }
             swap(arr, i, j);
@@ -19,6 +19,11 @@ class Solution {
         reverse(arr, i + 1);
         long result = Long.valueOf(String.valueOf(arr));
         return result <= Integer.MAX_VALUE && result > n ? (int) result : -1;
+    }
+    private void swap(char[] arr, int i, int j) {
+        char ch = arr[i];
+        arr[i] = arr[j];
+        arr[j] = ch;
     }
     private void reverse(char[] arr, int start) {
         int i = start;
@@ -28,10 +33,5 @@ class Solution {
             i++;
             j--;
         }
-    }
-    private void swap(char[] arr, int i, int j) {
-        char tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
     }
 }
