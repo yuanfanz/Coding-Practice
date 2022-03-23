358e799fce785e2680644ddac3cf71631ba9a8e5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-23 09:08:18 -0700

    Time: 1 ms (99.87%), Space: 44.1 MB (87.28%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.java
@@ -1,21 +1,23 @@
 class Solution {
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-        List<List<Integer>> result = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
         
+        int n = graph.length;
+        List<List<Integer>> result = new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         list.add(0);
-        dfs(result, list, graph, 0);
+        dfs(result, list, graph, 0, n);
         return result;
     }
-    private void dfs(List<List<Integer>> result, List<Integer> list,
-                    int[][] graph, int index) {
-        if (index == graph.length - 1) {
+    private void dfs(List<List<Integer>> result, List<Integer> list, 
+                     int[][] graph, int index, int n) {
+        if (index == n - 1) {
             result.add(new ArrayList<>(list));
             return;
         }
         for (int next : graph[index]) {
             list.add(next);
-            dfs(result, list, graph, next);
+            dfs(result, list, graph, next, n);
             list.remove(list.size() - 1);
         }
     }
