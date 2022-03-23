6d4f236f8df9feb048a8c254f5ff6c8037519252
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-23 09:47:05 -0700

    Time: 8 ms (19.46%), Space: 44 MB (61.03%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -4,19 +4,16 @@ class Solution {
         Map<Integer, Integer> map = new HashMap<>();
         
         for (int i = 0; i < nums2.length; ++i) {
-            if (stack.isEmpty()) {
-                stack.push(nums2[i]);
-            } else {
-                while (stack.size() > 0 && nums2[i] > stack.peek()) {
-                    map.put(stack.pop(), nums2[i]);
-                }
-                stack.push(nums2[i]);
+            while (stack.size() > 0 && nums2[i] > stack.peek()) {
+                int cur = stack.pop();
+                map.put(cur, nums2[i]);
             }
+            stack.push(nums2[i]);
         }
-        int[] res = new int[nums1.length];
+        int[] result = new int[nums1.length];
         for (int i = 0; i < nums1.length; ++i) {
-            res[i] = map.getOrDefault(nums1[i], -1);
+            result[i] = map.getOrDefault(nums1[i], -1);
         }
-        return res;
+        return result;
     }
 }
