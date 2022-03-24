20bc7ef94d40249a91f9a37b619a28f905da6150
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-24 15:39:28 -0700

    Time: 14 ms (53.02%), Space: 45 MB (48.19%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.java
@@ -3,23 +3,23 @@ class Solution {
         Map<Character, Integer> map = new HashMap<>();
         
         int i = 0;
-        int count = 0;
         int max = 0;
         for (int j = 0; j < s.length(); ++j) {
             map.put(s.charAt(j), map.getOrDefault(s.charAt(j), 0) + 1);
-            count++;
+            
             while (i < s.length() && map.size() > k) {
-                int val = map.get(s.charAt(i));
+                char ch = s.charAt(i++);
+                int val = map.get(ch);
                 val--;
-                if (val == 0) {
-                    map.remove(s.charAt(i));
+                if (val > 0) {
+                    map.put(ch, val);
                 } else {
-                    map.put(s.charAt(i), val);
+                    map.remove(ch);
                 }
-                count--;
-                i++;
             }
-            max = Math.max(max, count);
+            if (map.size() <= k) {
+                max = Math.max(max, j - i + 1);
+            }
         }
         return max;
     }
