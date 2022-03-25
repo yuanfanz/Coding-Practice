6c59baef6b03c149034783ecb095348c0f58b7ca
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-24 19:20:33 -0700

    Time: 9 ms (17.24%), Space: 48.1 MB (40.69%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.java
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.java
@@ -1,3 +1,40 @@
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+        
+        while (l1 != null) {
+            s1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            s2.push(l2.val);
+            l2 = l2.next;
+        }
+        int val = 0;
+        while (s1.size() > 0 || s2.size() > 0) {
+            val += s1.size() > 0 ? s1.pop() : 0;
+            val += s2.size() > 0 ? s2.pop() : 0;
+            ListNode node = new ListNode(val % 10);
+            node.next = head;
+            head = node;
+            val /= 10;
+        }
+        if (val > 0) {
+            ListNode node = new ListNode(val);
+            node.next = head;
+            head = node;
+        }
+        return head == null ? null : head;
+    }
+}
+
+
+
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,29 +45,3 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        Stack<Integer> s1 = new Stack<>();
-        Stack<Integer> s2 = new Stack<>();
-        while (l1 != null) {
-            s1.push(l1.val);
-            l1 = l1.next;
-        }
-        while (l2 != null) {
-            s2.push(l2.val);
-            l2 = l2.next;
-        }
-        ListNode head = new ListNode(0);
-        int val = 0;
-        while (!s1.isEmpty() || !s2.isEmpty()) {
-            val += s1.isEmpty() ? 0 : s1.pop();
-            val += s2.isEmpty() ? 0 : s2.pop();
-            head.val = val % 10;
-            val /= 10;
-            ListNode cur = new ListNode(val);
-            cur.next = head;
-            head = cur;
-        }
-        return head.val == 0 ? head.next : head;
-    }
-}
