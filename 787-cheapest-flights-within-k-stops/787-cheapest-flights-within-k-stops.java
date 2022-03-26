82be32e72651b0cb584b3a577e7652378671e694
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-26 11:27:29 -0700

    Time: 83 ms (10.04%), Space: 53.5 MB (12.04%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+        
+        for (int[] cur : flights) {
+            int from = cur[0];
+            int to = cur[1];
+            Map<Integer, Integer> costMap = map.getOrDefault(from, new HashMap<>());
+            costMap.put(to, cur[2]);
+            map.put(from, costMap);
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
+        pq.offer(new int[]{src, 0, 0});
+        int[] visited = new int[n];
+        Arrays.fill(visited, -1);
+        int min = Integer.MAX_VALUE;
+        while (pq.size() > 0) {
+            int[] cur = pq.poll();
+            int node = cur[0];
+            int cost = cur[1];
+            int stops = cur[2];
+            if (node == dst) {
+                if (stops <= k + 1) {
+                    min = Math.min(min, cost);
+                }
+            }
+            if (visited[node] != -1 && visited[node] < stops) continue;
+            visited[node] = stops;
+            if (map.containsKey(node)) {
+                for (int next : map.get(node).keySet()) {
+                    int nextCost = map.get(node).get(next);
+                    pq.offer(new int[]{next, cost + nextCost, stops + 1});
+                }
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
