09b1306d187a16695c280686e30754c32874bfc9
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-27 14:39:31 -0700

    Time: 28 ms (13.94%), Space: 48.7 MB (34.57%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.java
@@ -1,46 +1,43 @@
 class Solution {
     public int[] shortestAlternatingPaths(int n, int[][] redEdges, int[][] blueEdges) {
-        HashSet<Integer>[][] map = new HashSet[2][n];
+        Map<Integer, List<Integer>> redmap = new HashMap<>();
+        Map<Integer, List<Integer>> bluemap = new HashMap<>();
         
-        // 0 is red
-        // 1 is blue
-        for (int i = 0; i < n; ++i) {
-            map[0][i] = new HashSet<>();
-            map[1][i] = new HashSet<>();
-        }
-        for (int[] cur : redEdges) {
-            int from = cur[0];
-            int to = cur[1];
-            map[0][from].add(to);
-        }
-        for (int[] cur : blueEdges) {
-            int from = cur[0];
-            int to = cur[1];
-            map[1][from].add(to);
-        }
+        buildGraph(redmap, redEdges);
+        buildGraph(bluemap, blueEdges);
+        
         int[] result = new int[n];
         Arrays.fill(result, Integer.MAX_VALUE);
         result[0] = 0;
-        
         Queue<int[]> queue = new LinkedList<>();
+        // 0 is red, 1 is blue
         queue.offer(new int[]{0, 0});
         queue.offer(new int[]{0, 1});
-        int step = 0;
+        int level = 0;
         Set<String> visited = new HashSet<>();
         while (queue.size() > 0) {
             int size = queue.size();
             for (int i = 0; i < size; ++i) {
                 int[] cur = queue.poll();
                 int node = cur[0];
-                int color = cur[1];
-                if (!visited.add(node + "->" + color)) continue;
-                result[node] = Math.min(result[node], step);
-                int nextColor = 1 - color;
-                for (int next : map[nextColor][node]) {
-                    queue.offer(new int[]{next, nextColor});
+                if (!visited.add(node + "->" + cur[1])) continue;
+                result[node] = Math.min(result[node], level);
+                int nextColor = 1 - cur[1];
+                if (nextColor == 0) {
+                    if (redmap.containsKey(node)) {
+                        for (int next : redmap.get(node)) {
+                            queue.offer(new int[]{next, nextColor});
+                        }
+                    }
+                } else {
+                    if (bluemap.containsKey(node)) {
+                        for (int next : bluemap.get(node)) {
+                            queue.offer(new int[]{next, nextColor});
+                        }
+                    }
                 }
             }
-            step++;
+            level++;
         }
         for (int i = 1; i < n; i++) {
             if (result[i] == Integer.MAX_VALUE) {
@@ -49,16 +46,14 @@ class Solution {
         }
         return result;
     }
+    
+    private void buildGraph(Map<Integer, List<Integer>> map, int[][] edges) {
+        for (int[] cur : edges) {
+            int from = cur[0];
+            int to = cur[1];
+            List<Integer> list1 = map.getOrDefault(from, new ArrayList<>());
+            list1.add(to);
+            map.put(from, list1);
+        }
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
