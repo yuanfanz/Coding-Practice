8affa2fe7ce3c74aa6274bf280f5db51e672cf02
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-27 10:11:15 -0700

    Time: 273 ms (9.62%), Space: 151.2 MB (5.00%) - LeetHub

---
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.java
@@ -0,0 +1,24 @@
+class SeatManager {
+    TreeSet<Integer> set;
+    public SeatManager(int n) {
+        set = new TreeSet<>();
+        for (int i = 1; i <= n; ++i) {
+            set.add(i);
+        }
+    }
+    
+    public int reserve() {
+        return set.pollFirst();
+    }
+    
+    public void unreserve(int seatNumber) {
+        set.add(seatNumber);
+    }
+}
+
+/**
+ * Your SeatManager object will be instantiated and called as such:
+ * SeatManager obj = new SeatManager(n);
+ * int param_1 = obj.reserve();
+ * obj.unreserve(seatNumber);
+ */
