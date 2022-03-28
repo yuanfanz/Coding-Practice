394c7237c3b881f8cf7576f7766b501707aa7499
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-27 20:16:55 -0700

    Time: 8 ms (59.11%), Space: 42.6 MB (33.72%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -1,15 +1,21 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         List<String> result = new ArrayList<>();
-        
-        int i = 0;
-        while (i < nums.length) {
-            int cur = nums[i];
-            while (i + 1 < nums.length && nums[i + 1] == nums[i] + 1) {
-                i++;
-            }
-            result.add(formRange(cur, nums[i++]));
+        if (nums == null || nums.length == 0) {
+            return result;
         }
+        int prev = nums[0];
+        int start = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            if (prev + 1 == nums[i]) {
+                prev = nums[i];
+                continue;
+            }
+            result.add(formRange(start, prev));
+            prev = nums[i];
+            start = nums[i];
+        }
+        result.add(formRange(start, prev));
         return result;
     }
     private String formRange(int low, int high) {
