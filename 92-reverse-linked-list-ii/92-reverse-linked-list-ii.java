212c38077696959613d2f896276156496d256bb5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-27 20:12:13 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (48.02%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -1,21 +1,33 @@
-
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode pre1 = dummy;
         ListNode cur1 = dummy;
-        for (int i = 0; i < left; ++i) {
+        while (left > 0) {
             pre1 = cur1;
             cur1 = cur1.next;
+            left--;
+            right--;
         }
         ListNode pre2 = pre1;
         ListNode cur2 = cur1;
-        for (int i = left; i <= right; ++i) {
+        while (right >= 0) {
             ListNode tmp = cur2.next;
             cur2.next = pre2;
             pre2 = cur2;
             cur2 = tmp;
+            right--;
         }
         pre1.next = pre2;
         cur1.next = cur2;
