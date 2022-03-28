31adb5d47e9a73de347699c414a97f020bee2361
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-28 16:24:55 -0700

    Time: 104 ms (96.70%), Space: 64.9 MB (91.77%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,49 +1,26 @@
 class NumMatrix {
-    int[][] dp;
+    private int[][] dp;
     public NumMatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         
         dp = new int[m + 1][n + 1];
+        dp[0][0] = 0;
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
         }
-        print(dp);
     }
     
-    private void print(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                System.out.print(grid[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-    // 0  3  3  4  8  10 
-    // 3  9  9  11 19 23 
-    // 8  19 25 30 40 45 
-    // 9  21 29 34 45 55 
-    // 13 29 38 43 55 72 
-    // 14 31 40 48 60 82 
-
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int rowMin = Math.min(row1, row2);
-        int rowMax = Math.max(row1, row2);
-        int colMin = Math.min(col1, col2);
-        int colMax = Math.max(col1, col2);
+        int minRow = Math.min(row1, row2);
+        int maxRow = Math.max(row1, row2);
+        int minCol = Math.min(col1, col2);
+        int maxCol = Math.max(col1, col2);
         
-        
-                // System.out.println(rowMin);
-                // System.out.println(rowMax);
-                // System.out.println(colMin);
-                // System.out.println(colMax);
-        
-        return dp[rowMax + 1][colMax + 1] - dp[rowMin][colMax + 1] - dp[rowMax + 1][colMin]
-            + dp[rowMin][colMin];
+        return dp[maxRow + 1][maxCol + 1] - dp[minRow][maxCol + 1]
+            - dp[maxRow + 1][minCol] + dp[minRow][minCol];
     }
 }
 
