8078c02ad6346e992a8007659d8dbd4c3effdba3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-28 17:40:44 -0700

    Time: 3 ms (11.84%), Space: 40.9 MB (75.78%) - LeetHub

--- 231-power-of-two/231-power-of-two.java
+++ 231-power-of-two/231-power-of-two.java
@@ -1,11 +1,5 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        if (n <= 0) return false;
-        if (n == 1) return true;
-        while (n > 1) {
-            if (n % 2 != 0) return false;
-            n /= 2;
-        }
-        return true;
+        return n > 0 && n - (n & -n) == 0;
     }
 }
