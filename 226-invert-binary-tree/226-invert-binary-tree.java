cadec57a72352f21a6c479572d7c82fd0e0116a3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-29 20:09:05 -0700

    Time: 0 ms (100.00%), Space: 42.3 MB (12.19%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.java
+++ 226-invert-binary-tree/226-invert-binary-tree.java
@@ -1,27 +1,3 @@
-class Solution {
-    public TreeNode invertTree(TreeNode root) {
-        if (root == null || root.left == null && root.right == null) {
-            return root;
-        }
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(root);
-        while (queue.size() > 0) {
-            TreeNode cur = queue.poll();
-            TreeNode left = cur.left;
-            cur.left = cur.right;
-            cur.right = left;
-            
-            if (cur.left != null) {
-                queue.offer(cur.left);
-            }
-            if (cur.right != null) {
-                queue.offer(cur.right);
-            }
-        }
-        return root;
-    }
-}
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -37,3 +13,15 @@ class Solution {
  *     }
  * }
  */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
