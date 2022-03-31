731e27b95ac731239b212d768d5f4e29a39c13e5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-31 16:53:54 -0700

    Time: 2 ms (65.08%), Space: 43.1 MB (12.43%) - LeetHub

--- 91-decode-ways/91-decode-ways.java
+++ 91-decode-ways/91-decode-ways.java
@@ -1,9 +1,7 @@
 class Solution {
     public int numDecodings(String s) {
-        if (s == null || s.length() == 0) {
-            return 0;
-        }
         int n = s.length();
+        
         int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = s.charAt(0) == '0' ? 0 : 1;
