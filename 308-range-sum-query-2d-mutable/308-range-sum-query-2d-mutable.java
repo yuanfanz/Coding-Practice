7aa84881820ece3aaad52af891d8988d555a1ac0
Yuanfan Zhang <yuanfanz@uci.edu>
2022-03-31 11:17:55 -0700

    Time: 26 ms (45.04%), Space: 52 MB (32.72%) - LeetHub

--- 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.java
+++ 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.java
@@ -14,18 +14,20 @@ class NumMatrix {
     }
     
     public void update(int row, int col, int val) {
+        int m = arr.length;
+        int n = arr[0].length;
         int diff = val - arr[row][col];
         arr[row][col] = val;
-        for (int i = row + 1; i < fenwickTree.length; i += (i & -i)) {
-            for (int j = col + 1; j < fenwickTree[0].length; j += (j & -j)) {
+        for (int i = row + 1; i <= m; i += (i & -i)) {
+            for (int j = col + 1; j <= n; j += (j & -j)) {
                 fenwickTree[i][j] += diff;
             }
         }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        return getSum(row2 + 1, col2 + 1) + getSum(row1, col1)
-            - getSum(row2 + 1, col1) - getSum(row1, col2 + 1);
+        return getSum(row2 + 1, col2 + 1) - getSum(row2 + 1, col1)
+            - getSum(row1, col2 + 1) + getSum(row1, col1);
     }
     
     private int getSum(int row, int col) {
