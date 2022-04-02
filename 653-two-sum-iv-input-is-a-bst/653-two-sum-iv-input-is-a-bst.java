3fff9d43ae1568c66a62b4cac029f67b31758f58
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-01 21:30:24 -0700

    Time: 2 ms (98.84%), Space: 50.4 MB (71.74%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.java
@@ -3,18 +3,16 @@ class Solution {
     public boolean findTarget(TreeNode root, int k) {
         return dfs(root, root, k);
     }
-    
     private boolean dfs(TreeNode first, TreeNode second, int k) {
         if (second == null) return false;
         return search(first, second, k - second.val) || dfs(first, second.left, k)
             || dfs(first, second.right, k);
     }
-    
-    private boolean search(TreeNode first, TreeNode second, int val) {
+    private boolean search(TreeNode first, TreeNode second, int k) {
         if (first == null) return false;
-        return first.val == val && first != second ||
-            first.val < val && search(first.right, second, val) ||
-            first.val > val && search(first.left, second, val);
+        return first.val == k && first != second || 
+            first.val < k && search(first.right, second, k) ||
+            first.val > k && search(first.left, second, k);
     }
 }
 
