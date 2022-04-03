8d1bf7b05f5c3020bb97b0c56412c22c1c62ebad
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-03 11:11:24 -0700

    Time: 12 ms (18.55%), Space: 44.7 MB (64.84%) - LeetHub

--- 72-edit-distance/72-edit-distance.java
+++ 72-edit-distance/72-edit-distance.java
@@ -12,10 +12,10 @@ class Solution {
         }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
-                int min = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
                 if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else {
+                    int min = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
                     dp[i][j] = Math.min(min, dp[i - 1][j - 1] + 1);
                 }
             }
