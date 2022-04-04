0563ce7eb4c9d5457116303db3bdacdd12fe9e57
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-03 20:52:34 -0700

    Time: 3 ms (31.32%), Space: 41.6 MB (49.17%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.java
+++ 377-combination-sum-iv/377-combination-sum-iv.java
@@ -3,7 +3,7 @@ class Solution {
         int[] dp = new int[target + 1];
         
         dp[0] = 1;
-        for (int i = 0; i <= target; ++i) {
+        for (int i = 1; i <= target; ++i) {
             for (int j = 0; j < nums.length; ++j) {
                 if (i >= nums[j]) {
                     dp[i] += dp[i - nums[j]];
