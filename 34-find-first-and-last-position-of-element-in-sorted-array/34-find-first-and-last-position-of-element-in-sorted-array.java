23de37e198f69328b4025deb3c5ed8b1d7540a00
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-04 15:28:27 -0700

    Time: 0 ms (100.00%), Space: 47.5 MB (25.00%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -6,24 +6,24 @@ class Solution {
         int end = -1;
         while (i <= j) {
             int mid = i + (j - i) / 2;
-            if (nums[mid] < target) {
-                i = mid + 1;
-            } else {
+            if (nums[mid] >= target) {
                 j = mid - 1;
+            } else {
+                i = mid + 1;
             }
         }
         start = i;
-        if (i > nums.length - 1 || nums[i] != target) {
+        if (i >= nums.length || nums[i] != target) {
             start = -1;
         }
         i = 0;
         j = nums.length - 1;
         while (i <= j) {
             int mid = i + (j - i) / 2;
-            if (nums[mid] > target) {
-                j = mid - 1;
-            } else {
+            if (nums[mid] <= target) {
                 i = mid + 1;
+            } else {
+                j = mid - 1;
             }
         }
         end = j;
