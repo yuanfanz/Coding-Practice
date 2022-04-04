3144b8c8ce49d31d8f3c37bee45965f0e6a3ec90
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-03 17:50:43 -0700

    Time: 58 ms (48.41%), Space: 42.3 MB (98.86%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
-        String newStr = s + s;
-        return newStr.substring(1, newStr.length() - 1).contains(s);
+        String str = s + s;
+        return str.substring(1, str.length() - 1).contains(s);
     }
 }
