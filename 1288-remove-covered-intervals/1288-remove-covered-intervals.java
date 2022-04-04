158d1d4680ffa8f632810fd69492b12b13a33f8e
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-03 18:21:24 -0700

    Time: 7 ms (67.08%), Space: 46.9 MB (24.44%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -4,12 +4,15 @@ class Solution {
         
         int start = intervals[0][0];
         int end = intervals[0][1];
+        
         int count = 0;
         for (int i = 1; i < intervals.length; ++i) {
-            if (intervals[i][1] <= end) {
+            if (intervals[i][0] >= start && intervals[i][1] <= end) {
                 count++;
+            } else {
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
-            end = Math.max(end, intervals[i][1]);
         }
         return intervals.length - count;
     }
