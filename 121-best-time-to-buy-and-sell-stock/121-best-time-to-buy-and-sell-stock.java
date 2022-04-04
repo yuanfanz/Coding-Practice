cd5effd7c806127705b50a9e1b94a0cda333903e
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-03 20:25:56 -0700

    Time: 4 ms (23.28%), Space: 85.7 MB (5.72%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -6,11 +6,11 @@ class Solution {
         for (int i = 1; i < n; ++i) {
             diff[i] = prices[i] - prices[i - 1];
         }
-        int sum = diff[0];
-        int max = diff[0];
+        int presum = 0;
+        int max = 0;
         for (int i = 1; i < n; ++i) {
-            sum = diff[i] + (sum > 0 ? sum : 0);
-            max = Math.max(max, sum);
+            presum = diff[i] + (presum > 0 ? presum : 0);
+            max = Math.max(max, presum);
         }
         return max;
     }
