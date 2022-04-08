da2c7d3b572c9bed202c04efe9cead924cffccb8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-07 20:24:25 -0700

    Time: 3 ms (53.56%), Space: 48.8 MB (50.95%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.java
@@ -1,21 +1,42 @@
 class Solution {
     public int[] kWeakestRows(int[][] mat, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>
-            ((a, b) -> a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
-        for (int i = 0; i < mat.length; ++i) {
-            int sum = 0;
-            for (int j = 0; j < mat[i].length; ++j) {
-                sum += mat[i][j];
+        int m = mat.length;
+        int n = mat[0].length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> a[1] == b[1] ? b[0] - a[0] : b[1] - a[1]);
+        for (int i = 0; i < m; ++i) {
+            int left = findLeft(mat[i]);
+            if (left == -1) {
+                pq.offer(new int[]{i, n});
+            } else {
+                pq.offer(new int[]{i, left});
             }
-            pq.offer(new int[]{i, sum});
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
         int[] res = new int[k];
-        int index = 0;
-        while (pq.size() > 0 && k > 0) {
-            k--;
-            res[index++] = pq.poll()[0];
+        int index = k - 1;
+        while (pq.size() > 0 && index >= 0) {
+            res[index--] = pq.poll()[0];
         }
         return res;
     }
     
+    private int findLeft(int[] nums) {
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j){
+            int mid = i + (j - i) / 2;
+            if (nums[mid] == 0) {
+                j = mid - 1;
+            } else {
+                i = mid + 1;
+            }
+        }
+        if (i >= nums.length || nums[i] != 0) {
+            return -1;
+        }
+        return i;
+    }
 }
