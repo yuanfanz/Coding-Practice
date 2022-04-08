8e5ecdc282f09cbc561ace5b785ef16dd4392bd2
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-07 19:39:31 -0700

    Time: 5 ms (52.73%), Space: 84.5 MB (7.56%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.java
@@ -1,21 +1,24 @@
 class Solution {
     public int getMaxLen(int[] nums) {
-        int pos = 0;
         int neg = 0;
+        int pos = 0;
         int max = 0;
         for (int i = 0; i < nums.length; ++i) {
             if (nums[i] == 0) {
-                pos = 0;
                 neg = 0;
+                pos = 0;
             } else if (nums[i] > 0) {
                 pos++;
-                neg = neg == 0 ? 0 : neg+1;
-            } else {
+                neg = neg > 0 ? neg + 1 : 0;
+            } else if (nums[i] < 0) {
+                // negtive number
+                // pos > 0: neg = pos + 1
+                // neg > 0: pos = neg + 1
                 int tmp = pos;
-                pos = neg == 0 ? 0 : neg+1;
-                neg = tmp + 1;
+                pos = neg > 0 ? neg + 1 : 0;
+                neg = tmp > 0 ? tmp + 1 : 1;
             }
-            max = Math.max(max, pos);
+            max = Math.max(pos, max);
         }
         return max;
     }
