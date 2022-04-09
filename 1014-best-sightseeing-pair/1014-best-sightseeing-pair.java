ef9f0fc24e566e3af5032de322febb835e95e300
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-09 14:08:18 -0700

    Time: 3 ms (83.78%), Space: 63.9 MB (53.29%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -1,13 +1,12 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        int n = values.length;
+        int presum = values[0];
         
-        int maxSoFar = values[0];
-        int score = values[0];
-        for (int i = 1; i < n; ++i) {
-            score = Math.max(score, maxSoFar + values[i] - i);
-            maxSoFar = Math.max(maxSoFar, values[i] + i);
+        int max = values[0];
+        for (int i = 1; i < values.length; ++i) {
+            max = Math.max(max, presum + values[i] - i);
+            presum = Math.max(presum, values[i] + i);
         }
-        return score;
+        return max;
     }
 }
