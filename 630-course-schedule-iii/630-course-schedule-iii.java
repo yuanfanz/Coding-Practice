07bbc2aa516fdf3354e74da33dd6116a0e605771
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-10 15:41:04 -0700

    Time: 57 ms (48.99%), Space: 69.6 MB (59.28%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -1,12 +1,13 @@
 class Solution {
     public int scheduleCourse(int[][] courses) {
-        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        int count = 0;
+        
+        Arrays.sort(courses, (a, b) -> a[1] - b[1]);
         int end = 0;
+        int count = 0;
         for (int[] cur : courses) {
-            pq.offer(cur[0]);
             end += cur[0];
+            pq.offer(cur[0]);
             if (end > cur[1]) {
                 end -= pq.poll();
             } else {
