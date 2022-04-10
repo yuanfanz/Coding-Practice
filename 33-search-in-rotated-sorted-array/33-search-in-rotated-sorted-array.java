e062be5f7d98590056c11d8f27086ee5aafdb0b7
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-10 14:20:18 -0700

    Time: 0 ms (100.00%), Space: 41.5 MB (96.59%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -4,21 +4,18 @@ class Solution {
         int j = nums.length - 1;
         while (i <= j) {
             int mid = i + (j - i) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            }
-            if (nums[mid] <= nums[j]) {
-                if (target <= nums[j] && nums[mid] <= target) {
-                    i = mid + 1;
-                } else {
-                    j = mid - 1;
-                }
-            }
+            if (nums[mid] == target) return mid;
             if (nums[mid] >= nums[i]) {
-                if (nums[i] <= target && target <= nums[mid]) {
+                if (target <= nums[mid] && target >= nums[i]) {
                     j = mid - 1;
                 } else {
                     i = mid + 1;
+                }
+            } else if (nums[mid] <= nums[j]) {
+                if (target <= nums[j] && target >= nums[mid]) {
+                    i = mid + 1;
+                } else {
+                    j = mid - 1;
                 }
             }
         }
