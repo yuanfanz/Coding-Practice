b5c688b7c1aa355e86135e88872ce52078c5eb70
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-09 18:03:35 -0700

    Time: 3 ms (95.27%), Space: 42.5 MB (87.58%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.java
+++ 799-champagne-tower/799-champagne-tower.java
@@ -6,8 +6,8 @@ class Solution {
         for (int i = 0; i < row; ++i) {
             for (int j = 0; j <= col; ++j) {
                 double remain = Math.max(0, dp[i][j] - 1);
-                dp[i + 1][j] += remain/ 2;
-                dp[i + 1][j + 1] += remain/ 2;
+                dp[i + 1][j] += remain / 2;
+                dp[i + 1][j + 1] += remain / 2;
             }
         }
         return Math.min(1, dp[row][col]);
