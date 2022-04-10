34a4cabdcbb9d3b8bb642bbf8785da43d80fbe8b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-10 15:41:21 -0700

    Time: 35 ms (91.73%), Space: 50.1 MB (86.58%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.java
+++ 630-course-schedule-iii/630-course-schedule-iii.java
@@ -4,14 +4,11 @@ class Solution {
         
         Arrays.sort(courses, (a, b) -> a[1] - b[1]);
         int end = 0;
-        int count = 0;
         for (int[] cur : courses) {
             end += cur[0];
             pq.offer(cur[0]);
             if (end > cur[1]) {
                 end -= pq.poll();
-            } else {
-                count++;
             }
         }
         return pq.size();
