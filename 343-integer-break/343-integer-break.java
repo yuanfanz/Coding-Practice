a7342c0358ca78ec4982e3f2e59f6e931c9c0fc9
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-11 11:02:26 -0700

    Time: 1 ms (63.40%), Space: 39.4 MB (82.46%) - LeetHub

--- 343-integer-break/343-integer-break.java
+++ 343-integer-break/343-integer-break.java
@@ -3,7 +3,7 @@ class Solution {
         int[] dp = new int[n + 1];
         dp[1] = 1;
         for (int i = 2; i <= n; ++i) {
-            for (int j = 1; j <= i / 2; ++j) {
+            for (int j = 1; j < i; ++j) {
                 dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]));
             }
         }
