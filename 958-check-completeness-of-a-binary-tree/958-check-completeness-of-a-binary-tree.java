cc0aac94fc27278fd6927d194129ace180ff97ac
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-10 17:29:52 -0700

    Time: 1 ms (89.20%), Space: 41.4 MB (94.84%) - LeetHub

--- 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.java
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.java
@@ -1,29 +1,18 @@
 
 class Solution {
     public boolean isCompleteTree(TreeNode root) {
-        Map<TreeNode, Integer> map = new HashMap<>();
-        map.put(root, 1);
         Queue<TreeNode> queue = new LinkedList<>();
         queue.offer(root);
-        int max = 0;
-        Set<Integer> set = new HashSet<>();
+        boolean end = false;
         while (queue.size() > 0) {
             TreeNode cur = queue.poll();
-            int val = map.get(cur);
-            max = Math.max(max, val);
-            set.add(val);
-            if (cur.left != null) {
+            if (cur == null) {
+                end = true;
+            }
+            if (cur != null) {
+                if (end) return false;
                 queue.offer(cur.left);
-                map.put(cur.left, val * 2);
-            }
-            if (cur.right != null) {
                 queue.offer(cur.right);
-                map.put(cur.right, val * 2 + 1);
-            }
-        }
-        for (int i = 1; i <= max; ++i) {
-            if (!set.contains(i)) {
-                return false;
             }
         }
         return true;
