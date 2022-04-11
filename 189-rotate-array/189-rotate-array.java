0ff1c850eb5f3243563c98013a9e842426197fbf
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-11 11:59:52 -0700

    Time: 1 ms (81.89%), Space: 65 MB (13.01%) - LeetHub

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -1,9 +1,10 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k = k % nums.length;
-        reverse(nums, 0, nums.length - 1);
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, k, n - 1);
         reverse(nums, 0, k - 1);
-        reverse(nums, k, nums.length - 1);
     }
     private void reverse(int[] nums, int i, int j) {
         while (i < j) {
