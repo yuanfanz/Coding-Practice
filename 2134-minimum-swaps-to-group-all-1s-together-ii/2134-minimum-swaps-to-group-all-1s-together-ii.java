eab0684ea1e838274139752bd83c2180ad9a0d76
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-14 10:05:03 -0700

    Time: 17 ms (38.50%), Space: 50.9 MB (96.50%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
@@ -10,20 +10,51 @@ class Solution {
         for (int i : nums) {
             total += i;
         }
-        int j = 0;
+        
         int count = 0;
         int max = 0;
-        for (int i = 0; i < arr.length; ++i) {
-            while (j < arr.length && j - i + 1 <= total) {
-                if (arr[j++] == 1) {
-                    count++;
+        // int j = 0;
+        // printIndex(arr.length - 1);
+        // print(arr);
+        // for (int i = 0; i < arr.length; ++i) {
+        //     while (j < arr.length && j - i + 1 < total) {
+        //         if (arr[j++] == 1) {
+        //             count++;
+        //         }
+        //         // System.out.print(i + " ");
+        //         // System.out.print(j + "    ");
+        //     }
+        //     max = Math.max(max, count);
+        //     if (arr[i] == 1) {
+        //         count--;
+        //     }
+        // }
+        int i = 0;
+        for (int j = 0; j < arr.length; ++j) {
+            if (arr[j] == 1) {
+                count++;
+            }
+            while (i < arr.length && j - i + 1 > total) {
+                if (arr[i++] == 1) {
+                    count--;
                 }
             }
-            max = Math.max(max, count);
-            if (arr[i] == 1) {
-                count--;
+            if (j - i + 1 == total) {
+                max = Math.max(max, count);
             }
         }
         return total - max;
+    }
+    private void printIndex(int n) {
+        for (int i = 0; i < n; ++i) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+    private void print(int[] grid) {
+        for (int i = 0; i < grid.length; ++i) {
+            System.out.print(grid[i] + " ");
+        }
+        System.out.println();
     }
 }
