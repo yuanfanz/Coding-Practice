81cdffd59994868799db096c4ee1411dd4484274
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-14 21:00:46 -0700

    Time: 25 ms (35.90%), Space: 60.2 MB (84.02%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,16 +1,23 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int slow = nums[0];
-        int fast = nums[nums[0]];
-        while (slow != fast) {
-            slow = nums[slow];
-            fast = nums[nums[fast]];
+        int i = 0;
+        int j = nums.length - 1;
+        
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            int count = 0;
+            for (int k = 0; k < nums.length; ++k) {
+                if (nums[k] <= mid) {
+                    count++;
+                }
+            }
+            // count should equals mid
+            if (count <= mid) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
         }
-        int start = 0;
-        while (slow != start) {
-            start = nums[start];
-            slow = nums[slow];
-        }
-        return start;
+        return i;
     }
 }
