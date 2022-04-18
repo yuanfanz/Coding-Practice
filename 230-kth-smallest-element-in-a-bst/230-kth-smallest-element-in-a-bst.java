3e71e7269d5d0cdb88d7fa50b7f17ce9ea83f802
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-17 17:11:57 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (96.47%) - LeetHub

---
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -0,0 +1,38 @@
+
+class Solution {
+    int count = 0;
+    int target = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        dfs(root, k);
+        return target;
+    }
+    
+    private void dfs(TreeNode root, int k) {
+        if (root == null) {
+            return;
+        }
+        dfs(root.left, k);
+        count++;
+        if (count == k) {
+            target = root.val;
+            return;
+        }
+        dfs(root.right, k);
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
