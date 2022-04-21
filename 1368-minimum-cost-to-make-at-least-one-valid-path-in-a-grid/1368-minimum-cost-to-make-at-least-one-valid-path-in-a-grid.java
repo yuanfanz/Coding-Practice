cfaab8a1ec0646e2f322d41ef79ed2be68d38d96
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-20 22:07:28 -0700

    Time: 29 ms (56.90%), Space: 61.5 MB (14.14%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -4,11 +4,11 @@ class Solution {
         int n = grid[0].length;
         
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        pq.offer(new int[]{0, 0, 0});
+        Deque<int[]> deque = new LinkedList<>();
+        deque.offerFirst(new int[]{0, 0, 0});
         boolean[][] visited = new boolean[m][n];
-        while (pq.size() > 0) {
-            int[] cur = pq.poll();
+        while (deque.size() > 0) {
+            int[] cur = deque.pollFirst();
             int i = cur[0];
             int j = cur[1];
             int cost = cur[2];
@@ -18,14 +18,14 @@ class Solution {
             visited[i][j] = true;
             
             for (int k = 0; k < 4; ++k) {
-                int nextCost = cost;
                 int row = i + dirs[k][0];
                 int col = j + dirs[k][1];
                 if (row < 0 || col < 0 || row >= m || col >= n) continue;
                 if (grid[i][j] != k + 1) {
-                    nextCost++;
+                    deque.offerLast(new int[]{row, col, cost + 1});
+                } else {
+                    deque.offerFirst(new int[]{row, col, cost});
                 }
-                pq.offer(new int[]{row, col, nextCost});
             }
         }
         return 0;
