e4aaefe7a61fb2711cb3b2a9f5f2615ccc72642f
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-22 13:38:06 -0700

    Time: 184 ms (39.92%), Space: 65.9 MB (31.39%) - LeetHub

--- 864-shortest-path-to-get-all-keys/864-shortest-path-to-get-all-keys.java
+++ 864-shortest-path-to-get-all-keys/864-shortest-path-to-get-all-keys.java
@@ -17,6 +17,7 @@ class Solution {
                     queue.offer(new int[]{i, j, 0});
                 }
                 if (isKey(grid[i][j])) {
+                    // add new key to full mask
                     totalKeys |= (1 << (grid[i][j] - 'a'));
                 }
             }
@@ -30,8 +31,9 @@ class Solution {
                 int[] cur = queue.poll();
                 int i = cur[0];
                 int j = cur[1];
-                
+                // found all keys
                 if (cur[2] == totalKeys) return level;
+                
                 for (int[] dir : dirs) {
                     int row = i + dir[0];
                     int col = j + dir[1];
@@ -40,13 +42,13 @@ class Solution {
                     if (!isValid(grid, row, col, m, n)) continue;
                     char c = grid[row][col];
                     if (isLock(c)) {
-                        // if (key & (1 << (c - 'A')) == 0) {
-                        if ((key >> (c - 'A') & 1) == 0) {
+                        if ((key & (1 << (c - 'A'))) == 0) {
                             continue;
                         }
                     } else if (isKey(c)) {
                         key |= (1 << (c - 'a'));
                     }
+                    // all positions possible with different keys
                     if (visited.add(row + " " + col + " " + key)) {
                         queue.offer(new int[]{row, col, key});
                     }
@@ -67,16 +69,5 @@ class Solution {
 
     private boolean isValid(char[][] grid, int i, int j, int m, int n) {
         return i >= 0 && i < m && j >= 0 && j < n && grid[i][j] != '#';
-    }
-    
-    class Tuple{
-        int i;
-        int j;
-        int key;
-        public Tuple(int i, int j, int key) {
-            this.i = i;
-            this.j = j;
-            this.key = key;
-        }
     }
 }
