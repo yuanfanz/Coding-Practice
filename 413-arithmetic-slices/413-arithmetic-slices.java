b359d40f85e47e77ec0affeb11cd2cfc98989d8f
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-23 16:17:30 -0700

    Time: 0 ms (100.00%), Space: 40 MB (92.58%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -2,6 +2,7 @@ class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
         int count = 0;
         int cur = 0;
+        
         for (int i = 1; i < nums.length - 1; ++i) {
             if (nums[i] - nums[i - 1] == nums[i + 1] - nums[i]) {
                 cur++;
