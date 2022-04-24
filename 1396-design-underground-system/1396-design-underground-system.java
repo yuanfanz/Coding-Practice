bded5460561793f7cc4dc0f24c6f381a7c7a7fcf
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-24 12:01:39 -0700

    Time: 143 ms (73.72%), Space: 99.8 MB (65.66%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.java
+++ 1396-design-underground-system/1396-design-underground-system.java
@@ -1,47 +1,70 @@
 class UndergroundSystem {
-    Map<Integer, Tuple> user;
-    Map<String, Map<String, List<Integer>>> map;
-    public UndergroundSystem() {
-        user = new HashMap<>();
-        map = new HashMap<>();
-    }
     
-    public void checkIn(int id, String stationName, int t) {
-        Tuple tuple = new Tuple(stationName, t);
-        user.put(id, tuple);
-    }
-    
-    public void checkOut(int id, String stationName, int t) {
-        Tuple tuple = user.get(id);
-        int startTime = tuple.startTime;
-        int duration = t - startTime;
-        String start = tuple.start;
-        Map<String, List<Integer>> endMap = map.getOrDefault(start, new HashMap<>());
-        List<Integer> list = endMap.getOrDefault(stationName, new ArrayList<>());
-        list.add(duration);
-        endMap.put(stationName, list);
-        map.put(start, endMap);
-    }
-    
-    public double getAverageTime(String startStation, String endStation) {
-        Map<String, List<Integer>> endMap = map.get(startStation);
-        List<Integer> list = endMap.get(endStation);
-        double sum = 0;
-        for (int i : list) {
-            sum += i;
-        }
-        return (double) (sum / list.size());
-    }
-    
-    class Tuple{
+    class Person{
         String start;
         String end;
         int startTime;
         int endTime;
-        public Tuple(String start, int startTime) {
+        
+        public Person(String start, int startTime) {
             this.start = start;
             this.startTime = startTime;
         }
+        
+        public void checkout(String end, int endTime) {
+            this.end = end;
+            this.endTime = endTime;
+        }
+    }
+    
+    class Route{
+        String start;
+        String end;
+        int totalTrips;
+        int totalTime;
+        
+        public Route(String start, String end) {
+            this.start = start;
+            this.end = end;
+        }
+        
+        public void addTrip(int startTime, int endTime) {
+            totalTrips++;
+            totalTime += endTime - startTime;
+        }
+        
+        public double getAverageTime() {
+            return (double) totalTime / totalTrips;
+        }
+    }
+    
+    Map<Integer, Person> userMap;
+    Map<String, Route> routeMap;
+    
+    public UndergroundSystem() {
+        userMap = new HashMap<>();
+        routeMap = new HashMap<>();
+    }
+    
+    public void checkIn(int id, String stationName, int t) {
+        Person person = new Person(stationName, t);
+        userMap.put(id, person);
+    }
+    
+    public void checkOut(int id, String stationName, int t) {
+        Person person = userMap.get(id);
+        person.checkout(stationName, t);
+        String start = person.start;
+        String routeStr = start + "_" + stationName;
+        Route route = routeMap.getOrDefault(routeStr, new Route(start, stationName));
+        route.addTrip(person.startTime, t);
+        routeMap.put(routeStr, route);
+    }
+    
+    public double getAverageTime(String startStation, String endStation) {
+        String routeStr = startStation + "_" + endStation;
+        Route route = routeMap.get(routeStr);
+        return route.getAverageTime();
     }
 }
 
@@ -53,12 +76,6 @@ class UndergroundSystem {
  * double param_3 = obj.getAverageTime(startStation,endStation);
  */
 /**
- * Your UndergroundSystem object will be instantiated and called as such:
- * UndergroundSystem obj = new UndergroundSystem();
- * obj.checkIn(id,stationName,t);
- * obj.checkOut(id,stationName,t);
- * double param_3 = obj.getAverageTime(startStation,endStation);
- *//**
  * Your UndergroundSystem object will be instantiated and called as such:
  * UndergroundSystem obj = new UndergroundSystem();
  * obj.checkIn(id,stationName,t);
