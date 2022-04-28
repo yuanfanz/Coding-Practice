1a91fe37cdc1898f986191450cf9cad3cd80d847
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-28 11:00:01 -0700

    Time: 346 ms (8.16%), Space: 58.6 MB (65.86%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int minSpeedOnTime(int[] dist, double hour) {
+        int i = 1;
+        int j = 100000001;
+        
+        while (i < j) {
+            int mid = i + (j - i) / 2;
+            if (isValid(dist, hour, mid)) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return j == 100000001 ? -1 : j;
+    }
+    
+    private boolean isValid(int[] dist, double hour, int mid) {
+        double count = 0;
+        // System.out.println(mid);
+        for (int i = 0; i < dist.length - 1; ++i) {
+            count += (double) dist[i] / (double) mid;
+            if (dist[i] % mid != 0) {
+                count = Math.ceil(count);
+            }
+        }
+        // System.out.println("  " + count);
+        // if (count > dist[dist.length - 1]) return false;
+        count += (double) dist[dist.length - 1] / (double) mid;
+        return count <= hour;
+    }
+}
