208823cf8959fb3a1929d03592337bbe08d649e6
Yuanfan Zhang <yuanfanz@uci.edu>
2022-04-29 19:52:19 -0700

    Time: 2473 ms (5.09%), Space: 229.6 MB (5.09%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -28,3 +28,35 @@ class Solution {
         return ans;
     }
 }
+// class Solution {
+//     public int maxFrequency(int[] nums, int k) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int i : nums) {
+//             map.put(i, map.getOrDefault(i, 0) + 1);
+//         }
+//         List<Integer> list = new ArrayList<>(map.keySet());
+//         int max = 1;
+//         Collections.sort(list);
+//         for (int i = 0; i < list.size(); ++i) {
+//             int cur = list.get(i);
+//             int count = map.get(list.get(i));
+//             if (i > 0) {
+//                 int rem = k;
+//                 for (int j = i - 1; j >= 0; --j) {
+//                     int next = list.get(j);
+//                     int diff = cur - next;
+//                     if (rem >= diff) {
+//                         int add = rem / diff;
+//                         int min = Math.min(map.get(next), add);
+//                         rem -= min * diff;
+//                         count += min;
+//                     } else {
+//                         break;
+//                     }
+//                 }
+//             }
+//             max = Math.max(max, count);
+//         }
+//         return max;
+//     }
+// }
