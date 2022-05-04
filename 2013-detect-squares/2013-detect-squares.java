00e4fae5127b70aa728d4b80b320341250537466
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-03 18:34:37 -0700

    Time: 162 ms (52.31%), Space: 143.5 MB (5.20%) - LeetHub

---
+++ 2013-detect-squares/2013-detect-squares.java
@@ -0,0 +1,36 @@
+class DetectSquares {
+    List<int[]> list;
+    int[][] count;
+    public DetectSquares() {
+        list = new ArrayList<>();
+        count = new int[1001][1001];
+    }
+    
+    public void add(int[] point) {
+        count[point[0]][point[1]]++;
+        // if (count[point[0]][point[1]] > 1) return;
+        list.add(point);
+    }
+    
+    public int count(int[] point) {
+        int i = point[0];
+        int j = point[1];
+        int sum = 0;
+        for (int[] cur : list) {
+            int x = cur[0];
+            int y = cur[1];
+            if (x == i || Math.abs(x - i) != Math.abs(y - j)) {
+                continue;
+            }
+            sum += count[i][y] * count[x][j];
+        }
+        return sum;
+    }
+}
+
+/**
+ * Your DetectSquares object will be instantiated and called as such:
+ * DetectSquares obj = new DetectSquares();
+ * obj.add(point);
+ * int param_2 = obj.count(point);
+ */
