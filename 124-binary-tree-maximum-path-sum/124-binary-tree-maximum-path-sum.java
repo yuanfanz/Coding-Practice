b821841bd1a500f5d2d4dcaa38f458647baa1472
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-07 16:59:14 -0700

    Time: 80 ms (5.39%), Space: 48.2 MB (27.08%) - LeetHub

---
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -0,0 +1,19 @@
+
+class Solution {
+    private int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        dfs(root);
+        return max;
+    }
+    private int dfs(TreeNode root) {
+        if (root == null) return 0;
+        dfs(root.left);
+        
+        int left = Math.max(0, dfs(root.left));
+        int right = Math.max(0, dfs(root.right));
+        
+        max = Math.max(max, left + right + root.val);
+        
+        return Math.max(left, right) + root.val;
+    }
+}
