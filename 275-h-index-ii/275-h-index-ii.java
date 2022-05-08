fd06fa45dc2e169d6d96c090968c3c2d5c31b84c
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-07 17:14:20 -0700

    Time: 0 ms (100.00%), Space: 52 MB (58.10%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.java
+++ 275-h-index-ii/275-h-index-ii.java
@@ -1,30 +1,18 @@
 class Solution {
     public int hIndex(int[] citations) {
+        int n = citations.length;
         int i = 0;
-        int j = 1001;
-        while (i < j) {
-            int mid = i + (j - i + 1) / 2;
-            if (isValid(citations, mid)) {
-                i = mid;
-            } else {
-                j = mid - 1;
-            }
-        }
-        return i;
-    }
-    
-    private boolean isValid(int[] citations, int h) {
-        int i = 0;
-        int j = citations.length - 1;
+        int j = n - 1;
         while (i <= j) {
             int mid = i + (j - i) / 2;
-            if (citations[mid] >= h) {
+            
+            // (n - mid) means the total number of paper higher than h
+            if (citations[mid] >= n - mid) {
                 j = mid - 1;
             } else {
                 i = mid + 1;
             }
         }
-        if (i >= citations.length) return false;
-        return citations.length - i >= h;
+        return n - i;
     }
 }
