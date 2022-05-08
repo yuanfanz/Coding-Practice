2cd48f49c48b34e8ab0b5808b8c7b70c265017b8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-08 14:44:01 -0700

    Time: 35 ms (24.65%), Space: 46.1 MB (24.70%) - LeetHub

--- 616-add-bold-tag-in-string/616-add-bold-tag-in-string.java
+++ 616-add-bold-tag-in-string/616-add-bold-tag-in-string.java
@@ -1,25 +1,18 @@
 class Solution {
     public String addBoldTag(String s, String[] words) {
-        Map<Integer, String> map = new HashMap<>();
-        
         boolean[] mask = new boolean[s.length()];
         Set<String> set = new HashSet<>();
         for (String word : words) {
             set.add(word);
         }
-        // printSet(set);
         for (int i = 0; i < s.length(); ++i) {
             String cur = checkStart(set, s, i);
             if (cur.length() > 0) {
-                // System.out.println(cur);
-                    // System.out.println(i + " ");
                 for (int j = i; j < i + cur.length(); ++j) {
-                    // System.out.print(j + " ");
                     mask[j] = true;
                 }
             }
         }
-        // print(mask);
         StringBuilder sb = new StringBuilder();
         int i = 0;
         while (i < mask.length) {
@@ -40,28 +33,12 @@ class Solution {
         String res = "";
         for (String cur : set) {
             if (s.startsWith(cur, start)) {
-                // System.out.println(start);
-                // System.out.println(cur);
                 if (cur.length() > res.length()) {
                     res = cur;
                 }
             }
         }
         return res;
-    }
-    
-    private void print(boolean[] grid) {
-        for (int i = 0; i < grid.length; ++i) {
-            System.out.print(grid[i] + " ");
-        }
-        System.out.println();
-    }
-    
-    private void printSet(Set<String> set) {
-        for (String i : set) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
     }
 }
 
