be143a6f7aa4145b4325488444828f06b4da6ffa
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-14 11:31:01 -0700

    Time: 59 ms (66.67%), Space: 62.1 MB (66.67%) - LeetHub

---
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int maximumWhiteTiles(int[][] tiles, int carpetLen) {
+        Arrays.sort(tiles, (a, b) -> a[0] - b[0]);
+        
+        int[] prev = new int[]{tiles[0][0], tiles[0][1], tiles[0][1] - tiles[0][0] + 1};
+        List<int[]> list = new ArrayList<>();
+        list.add(prev);
+        for (int i = 1; i < tiles.length; ++i) {
+            int[] cur = new int[]{tiles[i][0], tiles[i][1], tiles[i][1] - tiles[i][0] + 1};
+            if (cur[0] <= prev[1] + 1) {
+                prev[1] = Math.max(prev[1], cur[1]);
+                prev[2] = prev[1] - prev[0] + 1;
+            } else {
+                prev = cur;
+                list.add(prev);
+            }
+        }
+        
+        int i = 0;
+        int j = 0;
+        int n = tiles.length;
+        int max = 0;
+        int sum = 0;
+        while (j < n) {
+            int left = tiles[i][0];
+            int right = left + carpetLen - 1;
+            // if still more carpet
+            if (tiles[j][1] <= right) {
+                sum += tiles[j][1] - tiles[j][0] + 1;
+                j++;
+                max = Math.max(sum, max);
+            } else {
+                if (right > tiles[j][0]) {
+                    max = Math.max(sum + right - tiles[j][0] + 1, max);
+                }
+                sum -= tiles[i][1]- tiles[i][0] + 1;
+                i++;
+            }
+        }
+        return max;
+    }
+}
+
+
+
+
+
+
+
+
+
