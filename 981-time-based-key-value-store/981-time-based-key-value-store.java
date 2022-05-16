16d5e9cee76a38907190b89d0dbc45f9de41df95
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-15 17:07:04 -0700

    Time: 243 ms (61.99%), Space: 215.5 MB (59.98%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -15,22 +15,18 @@ class TimeMap {
         if (!map.containsKey(key)) return "";
         List<Node> list = map.get(key);
         int i = 0;
-        int j= list.size() - 1;
+        int j = list.size() - 1;
         while (i <= j) {
             int mid = i + (j - i) /2;
-            if (list.get(mid).time < timestamp) {
+            if (list.get(mid).time <= timestamp) {
                 i = mid + 1;
-            } else if (list.get(mid).time > timestamp) {
-                j = mid - 1;
             } else {
-                i = mid + 1;
+                j = mid - 1;
             }
         }
         if (j < 0) {
             return "";
         }
-        // if (i >=0 && i < list.size() && list.get(i).time <= timestamp) return list.get(i).val;
-        // if (j >= 0 && j <list.size() && list.get(j).time <= timestamp) return list.get(j).val;
         return list.get(j).val;
     }
     class Node{
