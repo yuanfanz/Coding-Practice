b021e0c5b1ef94005b0354f8539c7b160fa6b5d8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-17 22:53:06 -0700

    Time: 42 ms (78.58%), Space: 73.1 MB (84.52%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.java
+++ 1146-snapshot-array/1146-snapshot-array.java
@@ -1,16 +1,16 @@
 class SnapshotArray {
-    List<TreeMap<Integer, Integer>> list;
+    List<List<Node>> list;
     int snap;
     public SnapshotArray(int length) {
         snap = 0;
         list = new ArrayList<>();
         for (int i = 0; i < length; ++i) {
-            list.add(new TreeMap<>());
+            list.add(new ArrayList<>());
         }
     }
     
     public void set(int index, int val) {
-        list.get(index).put(snap, val);
+        list.get(index).add(new Node(snap, val));
     }
     
     public int snap() {
@@ -18,18 +18,39 @@ class SnapshotArray {
     }
     
     public int get(int index, int snap_id) {
-        Integer prev = list.get(index).floorKey(snap_id);
-        if (prev != null) {
-            return list.get(index).get(prev);
+        List<Node> nodes = list.get(index);
+        // print(nodes);
+        // Collections.sort(nodes, (a, b) -> a.snap - b.snap);
+        int i = 0;
+        int j = nodes.size() - 1;
+        int ans = -1;
+        while (i <= j) {
+            int mid = i + (j - i) / 2;
+            if (nodes.get(mid).snap <= snap_id) {
+                ans = mid;
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
         }
+        if (ans != -1) return nodes.get(ans).val;
         return 0;
     }
-}
+    
+    class Node {
+        int snap;
+        int val;
 
-/**
- * Your SnapshotArray object will be instantiated and called as such:
- * SnapshotArray obj = new SnapshotArray(length);
- * obj.set(index,val);
- * int param_2 = obj.snap();
- * int param_3 = obj.get(index,snap_id);
- */
+        public Node(int snap, int val) {
+            this.snap = snap;
+            this.val = val;
+        }
+    }
+    
+    private void print(List<Node> list) {
+        for (int i = 0; i < list.size(); ++i) {
+            System.out.print(list.get(i).snap + " ");
+        }
+        System.out.println();
+    }
+}
