a67bdd557c420358fcc2482d1e03229c3555948a
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-19 19:06:58 -0700

    Time: 44 ms (51.93%), Space: 115.7 MB (52.34%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,40 +1,24 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int i = 0;
-        int j = points.length - 1;
-        while (i < j) {
-            int pivot = partition(points, i, j);
-            if (pivot == k) {
-                break;
-            } else if (pivot < k) {
-                i = pivot + 1;
-            } else {
-                j = pivot - 1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> getDistance(b) - getDistance(a));
+        
+        for (int[] cur : points) {
+            pq.offer(cur);
+            if (pq.size() > k) {
+                pq.poll();
             }
         }
-        int[][] result = new int[k][2];
+        
+        int[][] res = new int[k][2];
         int index = 0;
-        for (i = 0; i < k; ++i) {
-            result[index++] = points[i];
+        while (pq.size() > 0 && k > 0) {
+            res[index++] = pq.poll();
+            k--;
         }
-        return result;
+        return res;
     }
-    private int partition(int[][] points, int i, int j) {
-        int[] pivot = points[i];
-        while (i < j) {
-            while (i < j && getDistance(points[j]) >= getDistance(pivot)) {
-                j--;
-            }
-            points[i] = points[j];
-            while (i < j && getDistance(points[i]) <= getDistance(pivot)) {
-                i++;
-            }
-            points[j] = points[i];
-        }
-        points[i] = pivot;
-        return i;
-    }
-    private int getDistance(int[] point) {
-        return point[0] * point[0] + point[1] * point[1];
+    
+    private int getDistance(int[] a) {
+        return a[0] * a[0] + a[1] * a[1];
     }
 }
