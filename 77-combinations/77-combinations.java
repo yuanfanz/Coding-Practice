45537b707a00c769585fd423b0a0f6c1c4756572
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-19 21:22:39 -0700

    Time: 34 ms (41.70%), Space: 54.5 MB (65.31%) - LeetHub

--- 77-combinations/77-combinations.java
+++ 77-combinations/77-combinations.java
@@ -2,18 +2,19 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
         
-        dfs(result, new ArrayList<>(), n, k, 1);
+        dfs(result, new ArrayList<>(), 1, n, k);
         return result;
     }
-    private void dfs(List<List<Integer>> result, List<Integer> list, int n, int k, int index) {
+    
+    private void dfs(List<List<Integer>> result, List<Integer> list, 
+                     int index, int n, int k) {
         if (list.size() == k) {
             result.add(new ArrayList<>(list));
             return;
         }
         for (int i = index; i <= n; ++i) {
-            // if (list.contains(i)) continue;
             list.add(i);
-            dfs(result, list, n, k, i + 1);
+            dfs(result, list, i + 1, n, k);
             list.remove(list.size() - 1);
         }
     }
