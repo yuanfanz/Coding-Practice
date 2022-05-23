726fad5dff7e8cc66a1b1f8ee373126dda7aafe3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-22 18:06:21 -0700

    Time: 6 ms (12.75%), Space: 49.6 MB (20.31%) - LeetHub

--- 285-inorder-successor-in-bst/285-inorder-successor-in-bst.java
+++ 285-inorder-successor-in-bst/285-inorder-successor-in-bst.java
@@ -1,29 +1,3 @@
-
-class Solution {
-    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
-        Stack<TreeNode> stack = new Stack<>();
-        addAll(root, stack);
-        
-        while (stack.size() > 0) {
-            TreeNode cur = stack.pop();
-            addAll(cur.right, stack);
-            if (cur == p) {
-                if (stack.size() == 0) {
-                    return null;
-                }
-                return stack.pop();
-            }
-        }
-        return null;
-    }
-    private void addAll(TreeNode root, Stack<TreeNode> stack) {
-        while (root != null) {
-            stack.push(root);
-            root = root.left;
-        }
-    }
-}
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -33,3 +7,25 @@ class Solution {
  *     TreeNode(int x) { val = x; }
  * }
  */
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        Stack<TreeNode> stack = new Stack<>();
+        addAll(root, stack);
+        while (stack.size() > 0) {
+            TreeNode cur = stack.pop();
+            addAll(cur.right, stack);
+            if (cur == p) {
+                if (stack.size() == 0) return null;
+                return stack.pop();
+            }
+        }
+        return null;
+    }
+    
+    private void addAll(TreeNode root, Stack<TreeNode> stack) {
+        while (root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+}
