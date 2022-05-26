9c23fcb403032144b113ccc955552d386fa3d2e5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-25 20:03:19 -0700

    Time: 663 ms (17.27%), Space: 0B (100.00%) - LeetHub

--- 1479-sales-by-day-of-the-week/1479-sales-by-day-of-the-week.sql
+++ 1479-sales-by-day-of-the-week/1479-sales-by-day-of-the-week.sql
@@ -1,15 +1,19 @@
 # Write your MySQL query statement below
+with cte as (select item_category, ifnull(sum(quantity),0) total , dayname(order_date) weekday
+from orders
+right join items
+on orders.item_id=items.item_id
+group by 1,3)
 
-SELECT i.item_category AS Category,
-    SUM(CASE WHEN DAYOFWEEK(o.order_date) = 2 THEN quantity ELSE 0 END) AS Monday,
-    SUM(CASE WHEN DAYOFWEEK(o.order_date) = 3 THEN quantity ELSE 0 END) AS Tuesday,
-    SUM(CASE WHEN DAYOFWEEK(o.order_date) = 4 THEN quantity ELSE 0 END) AS Wednesday,
-    SUM(CASE WHEN DAYOFWEEK(o.order_date) = 5 THEN quantity ELSE 0 END) AS Thursday,
-    SUM(CASE WHEN DAYOFWEEK(o.order_date) = 6 THEN quantity ELSE 0 END) AS Friday,
-    SUM(CASE WHEN DAYOFWEEK(o.order_date) = 7 THEN quantity ELSE 0 END) AS Saturday,
-    SUM(CASE WHEN DAYOFWEEK(o.order_date) = 1 THEN quantity ELSE 0 END) AS Sunday
-FROM Items i
-LEFT JOIN Orders o
-ON i.item_id = o.item_id
-GROUP BY i.item_category
-ORDER BY i.item_category;
+select item_category as Category 
+,ifnull(sum(case when weekday='Monday' then total end),0) as Monday  
+,ifnull(sum(case when weekday='Tuesday' then total end),0) as Tuesday
+,ifnull(sum(case when weekday='Wednesday' then total end),0) as Wednesday
+,ifnull(sum(case when weekday='Thursday' then total end),0) as Thursday
+,ifnull(sum(case when weekday='Friday' then total end),0) as Friday
+,ifnull(sum(case when weekday='Saturday' then total end),0) as Saturday
+,ifnull(sum(case when weekday='Sunday' then total end),0) as Sunday
+
+from cte
+group by 1
+order by category
