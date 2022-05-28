a6c19ea5e553a46cbff7e82953b37b11bfebab79
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-28 15:40:35 -0700

    Time: 154 ms (26.68%), Space: 133.2 MB (32.75%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,35 +1,73 @@
 class NumArray {
-    int[] arr;
-    int[] fenwickTree;
+    Node root;
     public NumArray(int[] nums) {
-        int n = nums.length;
-        
-        arr = new int[n];
-        fenwickTree = new int[n + 1];
-        for (int i = 0; i < n; ++i) {
-            update(i, nums[i]);
-        }
+        root = buildTree(nums, 0, nums.length - 1);
     }
     
     public void update(int index, int val) {
-        int diff = val - arr[index];
-        arr[index] = val;
-        for (int i = index + 1; i < fenwickTree.length; i += (i & -i)) {
-            fenwickTree[i] += diff;
+        update(root, index, val);
+    }
+    
+    private void update(Node node, int index, int val) {
+        if (node == null) return;
+        if (index > node.end) return;
+        if (index < node.start) return;
+        
+        if (index == node.start && index == node.end) {
+            node.sum = val;
+            return;
         }
+        update(node.left, index, val);
+        update(node.right, index, val);
+        // dont forget to update sum val on node
+        node.sum = node.left.sum + node.right.sum;
     }
     
     public int sumRange(int left, int right) {
-        return getSum(right + 1) - getSum(left);
+        return sumRange(root, left, right);
     }
     
-    private int getSum(int index) {
-        int sum = 0;
-        while (index > 0) {
-            sum += fenwickTree[index];
-            index -= (index & -index);
+    private int sumRange(Node root, int start, int end) {
+        if (start > end) return 0;
+        
+        start = Math.max(start, root.start);
+        end = Math.min(end, root.end);
+        
+        if (start == root.start && end == root.end) {
+            return root.sum;
         }
-        return sum;
+        int left = sumRange(root.left, start, end);
+        int right = sumRange(root.right, start, end);
+        
+        return left + right;
+    }
+    
+    private Node buildTree(int[] nums, int start, int end) {
+        if (start > end) return null;
+        if (start == end) {
+            return new Node(start, end, nums[start]);
+        }
+        int mid = start + (end - start) / 2;
+        Node left = buildTree(nums, start, mid);
+        Node right = buildTree(nums, mid + 1, end);
+        
+        Node root = new Node(start, end, left.sum + right.sum);
+        root.left = left;
+        root.right = right;
+        return root;
+    }
+    
+    class Node{
+        Node left;
+        Node right;
+        int sum;
+        int start;
+        int end;
+        public Node(int start, int end, int sum) {
+            this.start = start;
+            this.end = end;
+            this.sum = sum;
+        }
     }
 }
 
