685105e8c48d6e464f9217b1bc618aec73c85687
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-30 10:35:35 -0700

    Time: 6 ms (52.40%), Space: 45.2 MB (29.90%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        dfs(result, new ArrayList<>(), candidates, target, 0, 0);
+        
+        dfs(candidates, result, new ArrayList<>(), target, 0, 0);
         return result;
     }
-    private void dfs(List<List<Integer>> result, List<Integer> list,
-                    int[] nums, int target, int sum, int index) {
-        if (sum > target) {
+    
+    private void dfs(int[] candidates, List<List<Integer>> result, 
+                     List<Integer> list, int target, int index, int sum) {
+        if (sum > target) return;
+        if (index == candidates.length) {
             return;
         }
         if (sum == target) {
             result.add(new ArrayList<>(list));
             return;
         }
-        for (int i = index; i < nums.length; ++i) {
-            list.add(nums[i]);
-            dfs(result, list, nums, target, sum + nums[i], i);
+        
+        for (int i = index; i < candidates.length; ++i) {
+            list.add(candidates[i]);
+            dfs(candidates, result, list, target, i, sum + candidates[i]);
             list.remove(list.size() - 1);
         }
     }
