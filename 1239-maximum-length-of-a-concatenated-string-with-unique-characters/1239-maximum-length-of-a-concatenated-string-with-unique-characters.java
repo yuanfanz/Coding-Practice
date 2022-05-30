45584c3936192647060d51f9a8aebf9b3ce4d7e5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-29 19:48:44 -0700

    Time: 60 ms (38.96%), Space: 68.3 MB (29.40%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,49 +1,22 @@
 class Solution {
     int max = 0;
-    Set<Integer> set;
     public int maxLength(List<String> arr) {
         int n = arr.size();
-        int[] values = new int[n];
-        set = new HashSet<>();
         for (int i = 0; i < n; ++i) {
             String s = arr.get(i);
-            if (!isUnique(s)) {
-                set.add(i);
-                continue;
-            }
-            int num = 0;
-            for (int j = 0; j < s.length(); ++j) {
-                num = num | 1 << (s.charAt(j) - 'a');
-            }
-            values[i] = num;
         }
         
         for (int i = 0; i < arr.size(); ++i) {
-            dfs(arr, values, i, "");
+            dfs(arr, i, "");
         }
         return max;
     }
-    private void dfs(List<String> arr, int[] values, int index, String cur) {
+    private void dfs(List<String> arr, int index, String cur) {
         max = Math.max(max, cur.length());
-        // if (index == arr.size()) return;
-        // System.out.println(cur);
-        
-        int num = 0;
-        for (int i = 0; i < cur.length(); ++i) {
-            num = num | 1 << (cur.charAt(i) - 'a');
-        }
         
         for (int i = index; i < arr.size(); ++i) {
-            int nextValue = values[i];
-            // System.out.println("     " + arr.get(i));
-            // if (cur.equals("act") && arr.get(i).equals("ers")) {
-            //     System.out.println("num: " + Integer.toBinaryString(num));
-            //     System.out.println("nextValue: " + Integer.toBinaryString(nextValue));
-            //     System.out.println("&: " + (nextValue & num));
-            // }
-            if (set.contains(i)) continue;
-            if ((nextValue & num) == 0) {
-                dfs(arr, values, i + 1, cur + arr.get(i));
+            if (isUnique(cur + arr.get(i))) {
+                dfs(arr, i + 1, cur + arr.get(i));
             }
         }
     }
@@ -58,12 +31,4 @@ class Solution {
         }
         return true;
     }
-
 }
-
-
-
-
-
-
-
