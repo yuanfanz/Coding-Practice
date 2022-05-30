3076e9bdd1168b148e140c36343587653c79fb1c
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-30 10:40:21 -0700

    Time: 7 ms (44.11%), Space: 44.4 MB (22.23%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        
+        Arrays.sort(candidates);
+        boolean[] visited = new boolean[candidates.length];
+        dfs(candidates, result, visited, new ArrayList<>(), target, 0, 0);
+        return result;
+    }
+    
+    private void dfs(int[] candidates, List<List<Integer>> result, boolean[] visited,
+                     List<Integer> list, int target, int index, int sum) {
+        if (sum > target) return;
+        // if (index == candidates.length) {
+        //     return;
+        // }
+        if (sum == target) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        
+        for (int i = index; i < candidates.length; ++i) {
+            if (visited[i] || (i > 0 && !visited[i - 1] && candidates[i] == candidates[i - 1])) continue;
+            list.add(candidates[i]);
+            visited[i] = true;
+            dfs(candidates, result, visited, list, target, i + 1, sum + candidates[i]);
+            visited[i] = false;
+            list.remove(list.size() - 1);
+        }
+    }
+}
