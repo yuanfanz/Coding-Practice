1f44e672a8b68b213b4c32e07bf1aa4e6ca58f1a
Yuanfan Zhang <yuanfanz@uci.edu>
2022-05-30 14:28:10 -0700

    Time: 3 ms (64.66%), Space: 47.8 MB (13.79%) - LeetHub

--- 99-recover-binary-search-tree/99-recover-binary-search-tree.java
+++ 99-recover-binary-search-tree/99-recover-binary-search-tree.java
@@ -1,26 +1,28 @@
 
 class Solution {
-    private TreeNode prev;
-    private TreeNode first;
-    private TreeNode second;
+    TreeNode prev;
+    TreeNode first;
+    TreeNode second;
     public void recoverTree(TreeNode root) {
         dfs(root);
         int tmp = first.val;
         first.val = second.val;
         second.val = tmp;
     }
+    
     private void dfs(TreeNode root) {
         if (root == null) return;
+        
         dfs(root.left);
-        if (prev != null) {
-            if (prev.val > root.val) {
-                if (first == null) {
-                    first = prev;
-                }
-                second = root;
+        
+        if (prev != null && prev.val > root.val) {
+            if (first == null) {
+                first = prev;
             }
+            second = root;
         }
         prev = root;
+        
         dfs(root.right);
     }
 }
