116c3b9c165210eb5521e042baf7cbdcaa3b45b9
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-01 19:42:16 -0700

    Time: 611 ms (5.01%), Space: 100.6 MB (5.01%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.java
@@ -0,0 +1,74 @@
+class Solution {
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<>();
+        
+        int num = 0;
+        char sign = '+';
+        int i = 0;
+        while (i < s.length()) {
+            if (i < s.length() && s.charAt(i) == ' ') {
+                i++;
+            }
+            while (i < s.length() && Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i++) - '0';
+            }
+            if (i < s.length() && s.charAt(i) == '(') {
+                int start = i + 1;
+                int end = i + 1;
+                int count = 1;
+                while (end < s.length() && count > 0) {
+                    if (s.charAt(end) == ')') {
+                        count--;
+                        if (count == 0) break;
+                    } else if (s.charAt(end) == '(') {
+                        count++;
+                    }
+                    end++;
+                }
+                num = calculate(s.substring(start, end));
+                i = end + 1;
+            }
+            if (i >= s.length() || !Character.isDigit(s.charAt(i)) 
+                && s.charAt(i) != '(' && s.charAt(i) != ' ') {
+                if (sign == '+') {
+                    stack.push(num);
+                }
+                if (sign == '-') {
+                    stack.push(-num);
+                }
+                if (sign == '*') {
+                    stack.push(stack.pop() * num);
+                }
+                if (sign == '/') {
+                    stack.push(stack.pop() / num);
+                }
+                if (i < s.length()) {
+                    sign = s.charAt(i);
+                }
+                num = 0;
+                i++;
+            }
+        }
+        int res = 0;
+        while (stack.size() > 0) {
+            res += stack.pop();
+        }
+        return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
