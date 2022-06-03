1390a263581e7193138e1a6bdf7e3477726b623b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-02 22:14:18 -0700

    Time: 196 ms (32.49%), Space: 40 MB (97.48%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -3,11 +3,13 @@ class Solution {
         int m = board.length;
         int n = board[0].length;
         
+        String[] res = new String[1];
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (board[i][j] == word.charAt(0)) {
-                    if (dfs(board, i, j, dirs, 0, word)) {
+                    if (dfs(board, i, j, res, new StringBuilder(), dirs, 0, word)) {
+                        System.out.println(res[0]);
                         return true;
                     }
                 }
@@ -16,9 +18,10 @@ class Solution {
         return false;
     }
     
-    private boolean dfs(char[][] board, int i, int j,
+    private boolean dfs(char[][] board, int i, int j, String[] res, StringBuilder sb,
                         int[][] dirs, int index, String word) {
         if (index == word.length()) {
+            res[0] = sb.toString();
             return true;
         }
         if (i < 0 || j < 0 || i >= board.length || j >= board[0].length) {
@@ -28,14 +31,17 @@ class Solution {
         if (ch == '#' || ch != word.charAt(index)) {
             return false;
         }
+        int len = sb.length();
+        sb.append(ch);
         board[i][j] = '#';
         for (int[] dir : dirs) {
             int row = i + dir[0];
             int col = j + dir[1];
-            if (dfs(board, row, col, dirs, index + 1, word)) {
+            if (dfs(board, row, col, res, sb, dirs, index + 1, word)) {
                 return true;
             }
         }
+        sb.setLength(len);
         board[i][j] = ch;
         return false;
     }
