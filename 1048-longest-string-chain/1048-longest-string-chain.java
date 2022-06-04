26b10d3764334ab77d5aba3ce53bdf86d5dbf9a4
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-04 16:12:37 -0700

    Time: 70 ms (40.37%), Space: 42.2 MB (91.05%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int longestStrChain(String[] words) {
+        int n = words.length;
+        
+        Arrays.sort(words, (a, b) -> a.length() - b.length());
+        int[] dp = new int[n];
+        for (int i = 0; i < n; ++i) {
+            dp[i] = 1;
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                
+                // System.out.println(words[j]);
+                // System.out.println(words[i]);
+                // System.out.println(isValid(words[j], words[i]));
+                
+                if (isValid(words[j], words[i])) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int max = 1;
+        for (int i : dp) {
+            max = Math.max(max, i);
+        }
+        return max;
+    }
+    
+    private boolean isValid(String s1, String s2) {
+        if (s1.length() + 1 != s2.length()) return false;
+        int i = 0;
+        int j = 0;
+        while (i < s1.length() && j < s2.length()) {
+            if (s1.charAt(i) != s2.charAt(j)) {
+                return s1.substring(i + 1).equals(s2.substring(j)) ||
+                        s1.substring(i).equals(s2.substring(j + 1));
+            }
+            i++;
+            j++;
+        }
+        return true;
+    }
+}
