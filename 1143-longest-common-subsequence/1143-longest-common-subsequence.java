5874a0fc5b342d750934dbb92bddb8c7d7c38603
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-04 19:06:14 -0700

    Time: 20 ms (44.20%), Space: 42.3 MB (95.93%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -3,16 +3,17 @@ class Solution {
         int m = text1.length();
         int n = text2.length();
         
-        int[][] dp = new int[m + 1][n + 1];
+        int[][] dp = new int[2][n + 1];
+        
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[i % 2][j] = 1 + dp[(i - 1) % 2][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i % 2][j] = Math.max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
                 }
             }
         }
-        return dp[m][n];
+        return dp[m % 2][n];
     }
 }
