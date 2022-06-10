44ef2b8bd61675b993a51ba8ce63cb5cbf27e923
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-09 19:51:08 -0700

    Time: 95 ms (41.99%), Space: 71.6 MB (25.91%) - LeetHub

--- 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
+++ 516-longest-palindromic-subsequence/516-longest-palindromic-subsequence.java
@@ -3,16 +3,16 @@ class Solution {
         int n = s.length();
         
         int[][] dp = new int[n][n];
-        for (int i = n - 1; i >= 0; --i) {
+        for (int i = 0; i < n; ++i) {
             dp[i][i] = 1;
-            for (int j = i + 1; j < n; ++j) {
+            for (int j = i - 1; j >= 0; --j) {
                 if (s.charAt(i) == s.charAt(j)) {
-                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                    dp[i][j] = dp[i - 1][j + 1] + 2;
                 } else {
-                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j + 1]);
                 }
             }
         }
-        return dp[0][n - 1];
+        return dp[n - 1][0];
     }
 }
