6d478e50688fe986871cb899602189bd5b7823c4
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-10 21:32:23 -0700

    Time: 25 ms (6.14%), Space: 48.8 MB (5.88%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,13 +1,16 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> set = new HashSet<>();
+        for (String cur : wordDict) {
+            set.add(cur);
+        }
         int n = s.length();
-        
         boolean[] dp = new boolean[n + 1];
         dp[0] = true;
-        for (int i = 1; i <= n; ++i) {
+        for (int i = 0; i <= n; ++i) {
             for (int j = 0; j < i; ++j) {
-                if (wordDict.contains(s.substring(j, i))) {
-                    dp[i] = dp[i] || dp[j];
+                if (set.contains(s.substring(j, i))) {
+                    dp[i] = dp[i] | dp[j];
                 }
             }
         }
