cb11f914c6f531729fa9dc02bd52e067e86713c3
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-12 20:07:12 -0700

    Time: 0 ms (100.00%), Space: 40.8 MB (50.34%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,11 +1,12 @@
 class Solution {
     public int numTrees(int n) {
         int[] dp = new int[n + 1];
+        
         dp[0] = 1;
         dp[1] = 1;
-        for (int level = 2; level <= n; ++level) {
-            for (int root = 1; root <= level; ++root) {
-                dp[level] += dp[level - root] * dp[root - 1];
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                dp[i] += dp[j - 1] * dp[i - j];
             }
         }
         return dp[n];
