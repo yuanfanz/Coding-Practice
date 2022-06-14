da4612b273f325b0faa58f9050e789ed0d31da54
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-13 21:41:42 -0700

    Time: 5 ms (68.01%), Space: 42.2 MB (58.16%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.java
+++ 518-coin-change-2/518-coin-change-2.java
@@ -1,12 +1,12 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int[] dp = new int[amount + 1];
-        
         dp[0] = 1;
-        for (int i = 0; i < coins.length; ++i) {
-            for (int j = 1; j <= amount; ++j) {
-                if (j >= coins[i]) {
-                    dp[j] += dp[j - coins[i]];
+        
+        for (int j = 0; j < coins.length; ++j) {
+            for (int i = 1; i <= amount; ++i) {
+                if (i >= coins[j]) {
+                    dp[i] += dp[i - coins[j]];
                 }
             }
         }
