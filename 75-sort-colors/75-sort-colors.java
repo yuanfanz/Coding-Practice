22096dd3dd8fbc84f89c9c65eac1c3dfd6f16f88
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-14 19:40:57 -0700

    Time: 0 ms (100.00%), Space: 42.4 MB (50.03%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,20 +1,30 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zero = 0;
-        int two = nums.length - 1;
-        for (int i = 0; i < nums.length; ++i) {
-            while (i < two && nums[i] == 2) {
-                swap(i, two--, nums);
-            }
-            while (i > zero && nums[i] == 0) {
-                swap(i, zero++, nums);
-            }
-        }
+        int i = 0;
+        int j = nums.length - 1;
+        sort(nums, i, j);
     }
-    
-    private void swap(int i, int j, int[] nums) {
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
+    private void sort(int[] nums, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        int pivot = partition(nums, i, j);
+        sort(nums, i, pivot);
+        sort(nums, pivot + 1, j);
+    }
+    private int partition(int[] nums, int i, int j) {
+        int pivot = nums[i];
+        while (i < j) {
+            while (i < j && nums[j] >= pivot) {
+                j--;
+            }
+            nums[i] = nums[j];
+            while (i < j && nums[i] <= pivot) {
+                i++;
+            }
+            nums[j] = nums[i];
+        }
+        nums[i] = pivot;
+        return i;
     }
 }
