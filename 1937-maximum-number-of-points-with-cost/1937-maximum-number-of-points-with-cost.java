d08459e17399b5c43032fe9575d925aa491217cb
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-16 20:03:04 -0700

    Time: 24 ms (29.38%), Space: 127.4 MB (35.82%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.java
@@ -3,28 +3,31 @@ class Solution {
         int m = points.length;
         int n = points[0].length;
         
-        long[][] dp = new long[m][n];
+        long[] prev = new long[n];
         int index = 0;
         for (int i : points[0]) {
-            dp[0][index++] = (long) i;
+            prev[index++] = i;
         }
         for (int i = 0; i < m - 1; ++i) {
             long[] left = new long[n];
-            left[0] = dp[i][0];
-            for (int j = 1; j < n; ++j) {
-                left[j] = Math.max(left[j - 1] - 1, dp[i][j]);
-            }
             long[] right = new long[n];
-            right[n - 1] = dp[i][n - 1];
+            left[0] = prev[0];
+            right[n - 1] = prev[n - 1];
+            
+            for (int j = 1; j < n; ++j) {
+                left[j] = Math.max(left[j - 1] - 1, prev[j]);
+            }
             for (int j = n - 2; j >= 0; --j) {
-                right[j] = Math.max(right[j + 1] - 1, dp[i][j]);
+                right[j] = Math.max(right[j + 1] - 1, prev[j]);
             }
+            long[] cur = new long[n];
             for (int j = 0; j < n; ++j) {
-                dp[i + 1][j] = points[i + 1][j] + Math.max(left[j], right[j]);
+                cur[j] = points[i + 1][j] + Math.max(left[j], right[j]);
             }
+            prev = cur;
         }
         long res = 0;
-        for (long i : dp[m - 1]) {
+        for (long i : prev) {
             res = Math.max(res, i);
         }
         return res;
