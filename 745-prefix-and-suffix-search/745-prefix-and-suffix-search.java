e4a67fa7db8f3d8abc18cd4a8c5f0a3fc6d13bc8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-17 20:28:22 -0700

    Time: 347 ms (84.12%), Space: 57 MB (98.05%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
@@ -1,64 +1,64 @@
 class WordFilter {
-    private TrieNode root;
-    private Map<String, Integer> indexMap;
-    private int ans;
+    TrieNode root;
+    Map<String, Integer> map;
+    private int res = -1;
     public WordFilter(String[] words) {
         root = new TrieNode();
-        indexMap = new HashMap<>();
-        int index = 0;
-        for (String word : words) {
+        map = new HashMap<>();
+        for (int i = 0; i < words.length; ++i) {
+            String s = words[i];
+            map.put(s, i);
             TrieNode cur = root;
-            indexMap.put(word, index++);
-            for (int i = 0; i < word.length(); ++i) {
-                char ch = word.charAt(i);
+            for (int j = 0; j < s.length(); ++j) {
+                char ch = s.charAt(j);
                 if (!cur.map.containsKey(ch)) {
                     cur.map.put(ch, new TrieNode(ch));
                 }
                 cur = cur.map.get(ch);
-                if (i == word.length() - 1) {
+                if (j == s.length() - 1) {
                     cur.isWord = true;
-                    cur.word = word;
+                    cur.word = s;
                 }
             }
         }
     }
     
     public int f(String prefix, String suffix) {
-        TrieNode cur = findPre(prefix);
+        TrieNode cur = findPrefix(prefix);
         if (cur == null) {
             return -1;
         }
-        ans = -1;
+        res = -1;
         findSuffix(cur, suffix);
-        return ans;
+        return res;
     }
     
-    private TrieNode findPre(String prefix) {
+    private void findSuffix(TrieNode cur, String s) {
+        if (cur.isWord) {
+            int start = cur.word.length() - s.length();
+            if (cur.word.substring(start).equals(s)) {
+                int index = map.get(cur.word);
+                if (index > res) {
+                    res = index;
+                }
+            }
+        } else {
+            for (char ch : cur.map.keySet()) {
+                findSuffix(cur.map.get(ch), s);
+            }
+        }
+    }
+    
+    private TrieNode findPrefix(String s) {
         TrieNode cur = root;
-        for (int i = 0; i < prefix.length(); ++i) {
-            char ch = prefix.charAt(i);
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
             if (!cur.map.containsKey(ch)) {
                 return null;
             }
             cur = cur.map.get(ch);
         }
         return cur;
-    }
-    
-    private void findSuffix(TrieNode cur, String suffix) {
-        if (cur.isWord) {
-            int start = cur.word.length() - suffix.length();
-            if (cur.word.substring(start).equals(suffix)) {
-                int index = indexMap.get(cur.word);
-                if (index > ans) {
-                    ans = index;
-                }
-            }
-        } else {
-            for (char next : cur.map.keySet()) {
-                findSuffix(cur.map.get(next), suffix);
-            }
-        }
     }
     
     class TrieNode{
@@ -68,12 +68,10 @@ class WordFilter {
         String word;
         public TrieNode() {
             map = new HashMap<>();
-            isWord = false;
         }
         public TrieNode(char ch) {
             this.ch = ch;
             map = new HashMap<>();
-            isWord = false;
         }
     }
 }
