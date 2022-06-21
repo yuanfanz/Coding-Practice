d43757be22309749e52254ee22626bcea4515aa7
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-21 09:54:36 -0700

    Time: 13 ms (47.84%), Space: 57.2 MB (34.36%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.java
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -9,7 +9,7 @@ class Solution {
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (rooms[i][j] == 0) {
-                    queue.offer(new int[]{i, j, 0});
+                    queue.offer(new int[]{i, j});
                 }
             }
         }
@@ -17,14 +17,13 @@ class Solution {
             int[] cur = queue.poll();
             int row = cur[0];
             int col = cur[1];
-            int dis = cur[2];
-            rooms[row][col] = Math.min(rooms[row][col], dis);
             for (int[] dir : dirs) {
                 int rr = row + dir[0];
                 int cc = col + dir[1];
                 if (rr < 0 || cc < 0 || rr >= m || cc >= n) continue;
                 if (rooms[rr][cc] != Integer.MAX_VALUE) continue;
-                queue.offer(new int[]{rr, cc, dis + 1});
+                rooms[rr][cc] = rooms[row][col] + 1;
+                queue.offer(new int[]{rr, cc});
             }
         }
     }
