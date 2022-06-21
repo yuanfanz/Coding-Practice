12b798de4882c80b85bc15b5da0c3404e819e194
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-21 09:28:24 -0700

    Time: 439 ms (5.03%), Space: 362 MB (5.02%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.java
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -4,28 +4,25 @@ class Solution {
         int n = rooms[0].length;
         
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        int INF = Integer.MAX_VALUE;
-        
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (rooms[i][j] == 0) {
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[]{i, j, 0});
                     boolean[][] visited = new boolean[m][n];
-                    while (queue.size() > 0) {
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.offer(new int[]{i, j, 1});
+                    while (queue.size() != 0) {
                         int[] cur = queue.poll();
-                        int row = cur[0];
-                        int col = cur[1];
                         int dis = cur[2];
-                        if (visited[row][col]) continue;
-                        visited[row][col] = true;
-                        rooms[row][col] = Math.min(rooms[row][col], dis);
                         for (int[] dir : dirs) {
-                            int rr = row + dir[0];
-                            int cc = col + dir[1];
-                            if (rr < 0 || cc < 0 || rr >= m || cc >= n) continue;
-                            if (rooms[rr][cc] <= 0) continue;
-                            queue.offer(new int[]{rr, cc, dis + 1});
+                            int row = cur[0] + dir[0];
+                            int col = cur[1] + dir[1];
+                            if (row < 0 || col < 0 || row >= m || col >= n
+                               || rooms[row][col] <= 0 || visited[row][col]) {
+                                continue;
+                            }
+                            visited[row][col] = true;
+                            rooms[row][col] = Math.min(rooms[row][col], dis);
+                            queue.offer(new int[]{row, col, dis + 1});
                         }
                     }
                 }
