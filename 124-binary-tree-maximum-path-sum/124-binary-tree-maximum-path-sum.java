69c16bde6649b6bf3f203e0926af570616b9c686
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-20 20:24:59 -0700

    Time: 1 ms (99.42%), Space: 47.4 MB (73.33%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -5,14 +5,30 @@ class Solution {
         dfs(root);
         return max;
     }
+    
     private int dfs(TreeNode root) {
         if (root == null) return 0;
         
-        int left = Math.max(0, dfs(root.left));
-        int right = Math.max(0, dfs(root.right));
+        int left = Math.max(dfs(root.left), 0);
+        int right = Math.max(dfs(root.right), 0);
         
         max = Math.max(max, left + right + root.val);
-        
         return Math.max(left, right) + root.val;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
