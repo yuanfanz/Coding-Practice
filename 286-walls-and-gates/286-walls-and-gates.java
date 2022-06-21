42a7bdab42d83e10d08607a0a61b02c9e03d7fe1
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-21 09:52:38 -0700

    Time: 333 ms (6.64%), Space: 251 MB (5.02%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.java
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -1,30 +1,30 @@
-public class Solution {
+class Solution {
     public void wallsAndGates(int[][] rooms) {
-        if (rooms.length == 0 || rooms[0].length == 0) return;
+        int m = rooms.length;
+        int n = rooms[0].length;
+        
+        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        
         Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < rooms.length; i++) {
-            for (int j = 0; j < rooms[0].length; j++) {
-                if (rooms[i][j] == 0) queue.add(new int[]{i, j});
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (rooms[i][j] == 0) {
+                    queue.offer(new int[]{i, j, 0});
+                }
             }
         }
-        while (!queue.isEmpty()) {
-            int[] top = queue.remove();
-            int row = top[0], col = top[1];
-            if (row > 0 && rooms[row - 1][col] == Integer.MAX_VALUE) {
-                rooms[row - 1][col] = rooms[row][col] + 1;
-                queue.add(new int[]{row - 1, col});
-            }
-            if (row < rooms.length - 1 && rooms[row + 1][col] == Integer.MAX_VALUE) {
-                rooms[row + 1][col] = rooms[row][col] + 1;
-                queue.add(new int[]{row + 1, col});
-            }
-            if (col > 0 && rooms[row][col - 1] == Integer.MAX_VALUE) {
-                rooms[row][col - 1] = rooms[row][col] + 1;
-                queue.add(new int[]{row, col - 1});
-            }
-            if (col < rooms[0].length - 1 && rooms[row][col + 1] == Integer.MAX_VALUE) {
-                rooms[row][col + 1] = rooms[row][col] + 1;
-                queue.add(new int[]{row, col + 1});
+        while (queue.size() > 0) {
+            int[] cur = queue.poll();
+            int row = cur[0];
+            int col = cur[1];
+            int dis = cur[2];
+            rooms[row][col] = Math.min(rooms[row][col], dis);
+            for (int[] dir : dirs) {
+                int rr = row + dir[0];
+                int cc = col + dir[1];
+                if (rr < 0 || cc < 0 || rr >= m || cc >= n) continue;
+                if (rooms[rr][cc] != Integer.MAX_VALUE) continue;
+                queue.offer(new int[]{rr, cc, dis + 1});
             }
         }
     }
