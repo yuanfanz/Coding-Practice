3237817ac863b7c7d33cbedbc955107295ba8e76
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-21 09:29:23 -0700

    Time: 13 ms (47.84%), Space: 56.8 MB (55.39%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.java
+++ 286-walls-and-gates/286-walls-and-gates.java
@@ -1,31 +1,30 @@
-class Solution {
+public class Solution {
     public void wallsAndGates(int[][] rooms) {
-        int m = rooms.length;
-        int n = rooms[0].length;
-        
-        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (rooms[i][j] == 0) {
-                    boolean[][] visited = new boolean[m][n];
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.offer(new int[]{i, j, 1});
-                    while (queue.size() != 0) {
-                        int[] cur = queue.poll();
-                        int dis = cur[2];
-                        for (int[] dir : dirs) {
-                            int row = cur[0] + dir[0];
-                            int col = cur[1] + dir[1];
-                            if (row < 0 || col < 0 || row >= m || col >= n
-                               || rooms[row][col] <= 0 || visited[row][col]) {
-                                continue;
-                            }
-                            visited[row][col] = true;
-                            rooms[row][col] = Math.min(rooms[row][col], dis);
-                            queue.offer(new int[]{row, col, dis + 1});
-                        }
-                    }
-                }
+        if (rooms.length == 0 || rooms[0].length == 0) return;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < rooms.length; i++) {
+            for (int j = 0; j < rooms[0].length; j++) {
+                if (rooms[i][j] == 0) queue.add(new int[]{i, j});
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] top = queue.remove();
+            int row = top[0], col = top[1];
+            if (row > 0 && rooms[row - 1][col] == Integer.MAX_VALUE) {
+                rooms[row - 1][col] = rooms[row][col] + 1;
+                queue.add(new int[]{row - 1, col});
+            }
+            if (row < rooms.length - 1 && rooms[row + 1][col] == Integer.MAX_VALUE) {
+                rooms[row + 1][col] = rooms[row][col] + 1;
+                queue.add(new int[]{row + 1, col});
+            }
+            if (col > 0 && rooms[row][col - 1] == Integer.MAX_VALUE) {
+                rooms[row][col - 1] = rooms[row][col] + 1;
+                queue.add(new int[]{row, col - 1});
+            }
+            if (col < rooms[0].length - 1 && rooms[row][col + 1] == Integer.MAX_VALUE) {
+                rooms[row][col + 1] = rooms[row][col] + 1;
+                queue.add(new int[]{row, col + 1});
             }
         }
     }
