733169871b008f2f910402a5b13fbc99b7940a38
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-22 18:47:23 -0700

    Time: 72 ms (92.11%), Space: 43.7 MB (80.70%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.java
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.java
@@ -11,34 +11,21 @@ class Solution {
     }
     
     private int dfs(int cur, int end, int[] locations, int remain, int[][] cache) {
-        if (remain < 0) return 0;
         if (cache[cur][remain] != -1) {
             return cache[cur][remain];
         }
-        // no more fuel, cannot move
-        if (remain == 0 && cur != end) {
+        // 如果一步到达不了，说明从位置 u 不能到达 end 位置
+        // 将结果 0 写入缓存器并返回
+        int need = Math.abs(locations[cur] - locations[end]);
+        if (need > remain) {
             cache[cur][remain] = 0;
             return 0;
         }
-        // not enough fuel, cannot move
-        boolean canMove = false;
-        for (int i = 0; i < locations.length; ++i) {
-            if (i == cur) continue;
-            if (Math.abs(locations[i] - locations[cur]) <= remain) {
-                canMove = true;
-                break;
-            }
-        }
-        if (remain != 0 && !canMove) {
-            int way = cur == end ? 1 : 0;
-            cache[cur][remain] = way;
-            return way;
-        }
-        
+        // if already at finish point, it is 1 way
         int count = cur == end ? 1 : 0;
         for (int i = 0; i < locations.length; ++i) {
             if (i == cur) continue;
-            int need = Math.abs(locations[i] - locations[cur]);
+            need = Math.abs(locations[i] - locations[cur]);
             if (remain >= need) {
                 count += dfs(i, end, locations, remain - need, cache);
                 count %= mod;
@@ -48,20 +35,3 @@ class Solution {
         return count;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
