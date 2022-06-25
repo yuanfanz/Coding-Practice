396055b1dd199ebfdda83f0f5aa9738bac32504e
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-25 13:46:48 -0700

    Time: 49 ms (19.54%), Space: 44.8 MB (17.99%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.java
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -1,36 +1,31 @@
+import java.util.concurrent.Semaphore;
+
 class FooBar {
     private int n;
-    private int flag = 0;
+    Semaphore semafoo = new Semaphore(1);
+    Semaphore semabar = new Semaphore(0);
 
     public FooBar(int n) {
         this.n = n;
     }
 
     public void foo(Runnable printFoo) throws InterruptedException {
+        
         for (int i = 0; i < n; i++) {
-            synchronized (this) {
-                while (flag == 1) {
-                    this.wait();
-                }
-                // printFoo.run() outputs "foo". Do not change or remove this line.
-                printFoo.run();
-                flag = 1;
-                this.notifyAll();
-            }
+            semafoo.acquire();
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+        	printFoo.run();
+            semabar.release();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
+        
         for (int i = 0; i < n; i++) {
-            synchronized (this) {
-                while (flag == 0) {
-                    this.wait();
-                }
-                // printBar.run() outputs "bar". Do not change or remove this line.
-                printBar.run();
-                flag = 0;
-                this.notifyAll();
-            }
+            semabar.acquire();
+            // printBar.run() outputs "bar". Do not change or remove this line.
+        	printBar.run();
+            semafoo.release();
         }
     }
 }
