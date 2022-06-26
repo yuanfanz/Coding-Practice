99ea3c4f725f2583863148c3942319ef49754900
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-26 11:06:39 -0700

    Time: 0 ms (100.00%), Space: 45.4 MB (56.87%) - LeetHub

--- 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
+++ 669-trim-a-binary-search-tree/669-trim-a-binary-search-tree.java
@@ -2,13 +2,13 @@
 class Solution {
     public TreeNode trimBST(TreeNode root, int low, int high) {
         if (root == null) return root;
-        root.left = trimBST(root.left, low, high);
-        root.right = trimBST(root.right, low, high);
-        if (root.val < low) {
-            return root.right;
-        } else if (root.val > high) {
-            return root.left;
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        } else if (root.val < low) {
+            return trimBST(root.right, low, high);
         }
+        root.left = trimBST(root.left, low, root.val);
+        root.right = trimBST(root.right, root.val, high);
         return root;
     }
 }
