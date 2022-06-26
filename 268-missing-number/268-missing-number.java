99ad7e6bbec2f8b185992149da3be705294922ea
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-26 09:31:58 -0700

    Time: 1 ms (70.89%), Space: 51.5 MB (26.17%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -2,7 +2,8 @@ class Solution {
     public int missingNumber(int[] nums) {
         int xor = nums.length;
         for (int i = 0; i < nums.length; ++i) {
-            xor = xor ^ i ^ nums[i];
+            xor = xor ^ i;
+            xor = xor ^ nums[i];
         }
         return xor;
     }
