09ea760d145a3d6c972b62a97493b20a449f7c13
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-29 23:19:48 -0700

    Time: 658 ms (25.45%), Space: 154.3 MB (12.48%) - LeetHub

--- 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.java
+++ 317-shortest-distance-from-all-buildings/317-shortest-distance-from-all-buildings.java
@@ -1,55 +1,54 @@
 class Solution {
+    int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
     public int shortestDistance(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
         
+        int total = 0;
+        int[][] dis = new int[m][n];
         int[][] reach = new int[m][n];
-        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-        int[][] distance = new int[m][n];
-        int totalBuildings = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (grid[i][j] == 1) {
-                    totalBuildings++;
-                    Queue<int[]> queue = new LinkedList<>();
-                    queue.add(new int[]{i, j});
-                    int level = 1;
-                    boolean[][] visited = new boolean[m][n];
-                    while (queue.size() > 0) {
-                        int size = queue.size();
-                        for (int k = 0; k < size; ++k) {
-                            int[] cur = queue.poll();
-                            // System.out.println(cur[0] + " " + cur[1]);
-                            for (int[] dir : dirs) {
-                                int row = cur[0] + dir[0];
-                                int col = cur[1] + dir[1];
-                                if (row < 0 || col < 0 || row >= m || col >= n) continue;
-                                if (visited[row][col]) continue;
-                                visited[row][col] = true;
-                                if (grid[row][col] != 0) continue;
-                                distance[row][col] += level;
-                                queue.offer(new int[]{row, col});
-                                reach[row][col]++;
-                            }
-                        }
-                        // System.out.println();
-                        level++;
-                    }
+                    total++;
+                    bfs(i, j, grid, dis, reach);
                 }
             }
         }
-        // print(distance);
-        // System.out.println();
-        // print(reach);
-        // System.out.println("totalBuildings "+totalBuildings);
         int min = Integer.MAX_VALUE;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                if (grid[i][j] != 0 || reach[i][j] != totalBuildings) continue;
-                min = Math.min(min, distance[i][j]);
+                if (reach[i][j] != total || grid[i][j] != 0) continue;
+                min = Math.min(min, dis[i][j]);
             }
         }
         return min == Integer.MAX_VALUE ? -1 : min;
+    }
+    
+    private void bfs(int i, int j, int[][] grid, int[][] dis, int[][] reach) {
+        int m = grid.length;
+        int n = grid[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{i, j, 0});
+        boolean[][] visited = new boolean[m][n];
+        visited[i][j] = true;
+        reach[i][j]++;
+        while (queue.size() > 0) {
+            int[] cur = queue.poll();
+            int cost = cur[2];
+            for (int[] dir : dirs) {
+                int row = cur[0] + dir[0];
+                int col = cur[1] + dir[1];
+                if (row < 0 || col < 0 || row >= m || col >= n) {
+                    continue;
+                }
+                if (visited[row][col] || grid[row][col] != 0) continue;
+                visited[row][col] = true;
+                reach[row][col]++;
+                dis[row][col] += cost + 1;
+                queue.offer(new int[]{row, col, cost + 1});
+            }
+        }
     }
     
     private void print(int[][] grid) {
@@ -63,3 +62,22 @@ class Solution {
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
