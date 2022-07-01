ccef68fdf3d88ab3171043ee6fc67edd4b71002b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-06-30 20:07:36 -0700

    Time: 4 ms (62.05%), Space: 46.1 MB (54.95%) - LeetHub

--- 265-paint-house-ii/265-paint-house-ii.java
+++ 265-paint-house-ii/265-paint-house-ii.java
@@ -1,26 +1,50 @@
 class Solution {
     public int minCostII(int[][] costs) {
-        int n = costs.length;
-        int k = costs[0].length;
+        int m = costs.length;
+        int n = costs[0].length;
         
-        int[][] dp = new int[n][k];
-        for (int i = 0; i < k; ++i) {
-            dp[0][i] = costs[0][i];
-        }
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < k; ++j) {
-                int min = Integer.MAX_VALUE;
-                for (int m = 0; m < k; ++m) {
-                    if (m == j) continue;
-                    min = Math.min(min, dp[i - 1][m]);
-                }
-                dp[i][j] = min + costs[i][j];
+        int first = -1;
+        int second = -1;
+        for (int j = 0; j < n; ++j) {
+            if (costs[0][j] < (first == -1 ? Integer.MAX_VALUE : costs[0][first])) {
+                second = first;
+                first = j;
+            } else if (costs[0][j] < (second == -1 ? Integer.MAX_VALUE : costs[0][second])) {
+                second = j;
             }
         }
-        int res = Integer.MAX_VALUE;
-        for (int i = 0; i < k; ++i) {
-            res = Math.min(res, dp[n - 1][i]);
+        int[] prev = costs[0];
+        for (int i = 1; i < m; ++i) {
+            int[] cur = costs[i];
+            int newFirst = -1;
+            int newSecond = -1;
+            for (int j = 0; j < n; ++j) {
+                int min = prev[first];
+                if (j == first) {
+                    min = prev[second];
+                }
+                cur[j] += min;
+                if (cur[j] < (newFirst == -1 ? Integer.MAX_VALUE : cur[newFirst])) {
+                    newSecond = newFirst;
+                    newFirst = j;
+                } else if (cur[j] < (newSecond == -1 ? Integer.MAX_VALUE : cur[newSecond])) {
+                    newSecond = j;
+                }
+            }
+            first = newFirst;
+            second = newSecond;
+            prev = cur;
         }
-        return res;
+        int min = Integer.MAX_VALUE;
+        for (int i : costs[m - 1]) {
+            min = Math.min(min, i);
+        }
+        return min;
     }
+    private void print(int[] grid) {
+    for (int i = 0; i < grid.length; ++i) {
+        System.out.print(grid[i] + " ");
+    }
+    System.out.println();
+}
 }
