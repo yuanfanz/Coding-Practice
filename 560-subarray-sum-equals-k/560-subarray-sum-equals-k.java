ddb3f4f05a0fc42f154f0ecbbd2aa4fc084f380b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-02 12:10:16 -0700

    Time: 33 ms (73.10%), Space: 67.6 MB (34.50%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,6 +1,7 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
+        
         map.put(0, 1);
         int sum = 0;
         int count = 0;
