3861f59e7018a5d23ef786cbab0005c2a9247952
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-02 12:32:56 -0700

    Time: 39 ms (32.07%), Space: 98 MB (28.78%) - LeetHub

--- 523-continuous-subarray-sum/523-continuous-subarray-sum.java
+++ 523-continuous-subarray-sum/523-continuous-subarray-sum.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, -1);
         
-        int presum = 0;
+        // 这里value还是放index，和325一样，和560不同
+        map.put(0, -1);
+        int sum = 0;
         for (int i = 0; i < nums.length; ++i) {
-            presum += nums[i];
-            int rem = presum % k;
-            if (map.containsKey(rem)) {
-                int index = map.get(rem);
-                if (i - index > 1) {
+            sum += nums[i];
+            int mod = sum % k;
+            if (map.containsKey(mod)) {
+                // 之前出现过同样的余数
+                // 且中间相隔的元素至少有两个
+                if (i - map.get(mod) > 1) {
                     return true;
                 }
-            } else {
-                map.put(rem, i);
+            } else { // 为了保留最长的index，只有新的余数才放进去
+                map.put(mod, i);
             }
         }
         return false;
