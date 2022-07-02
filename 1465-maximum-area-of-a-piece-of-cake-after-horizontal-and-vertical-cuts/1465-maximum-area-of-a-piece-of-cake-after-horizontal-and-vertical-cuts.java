3a646eeaa343ff33b018b7934a298a9b462949d8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-01 21:05:54 -0700

    Time: 139 ms (5.24%), Space: 63 MB (5.47%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+        int mod = (int) Math.pow(10, 9) + 7;
+        
+        List<Integer> row = new ArrayList<>();
+        List<Integer> col = new ArrayList<>();
+        
+        Arrays.sort(horizontalCuts);
+        Arrays.sort(verticalCuts);
+        
+        int prev = horizontalCuts[0];
+        row.add(horizontalCuts[0]);
+        for (int i = 1; i < horizontalCuts.length; ++i) {
+            row.add(horizontalCuts[i] - prev);
+            prev = horizontalCuts[i];
+        }
+        row.add(h - prev);
+        
+        prev = verticalCuts[0];
+        col.add(verticalCuts[0]);
+        for (int i = 1; i < verticalCuts.length; ++i) {
+            col.add(verticalCuts[i] - prev);
+            prev = verticalCuts[i];
+        }
+        col.add(w - prev);
+        Collections.sort(row, Collections.reverseOrder());
+        Collections.sort(col, Collections.reverseOrder());
+        
+        long max = 0;
+        int i = 0;
+        int j = 0;
+        while (i < row.size() && j < col.size()) {
+            long rowNum = (long) row.get(i);
+            long colNum = (long) col.get(j);
+            // System.out.println(rowNum);
+            // System.out.println(colNum);
+            // System.out.println();
+            long area = rowNum * colNum;
+            max = Math.max(max, area);
+            if (rowNum > colNum) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+        return (int) (max % mod);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
