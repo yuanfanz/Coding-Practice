db5e1696179c0a624bef2d98c44394ec50ec3822
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-02 16:42:32 -0700

    Time: 530 ms (6.69%), Space: 71.2 MB (15.45%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.java
@@ -3,44 +3,53 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
-
-        int[][] visited = new int[m][n];
-        for (int[] cur : visited) {
-            Arrays.fill(cur, -1);
-        }
         
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{0, 0, 0});
-        int step = 0;
-        while (queue.size() > 0) {
-            int size = queue.size();
-            for (int s = 0; s < size; ++s) {
-                int[] cur = queue.poll();
-                int i = cur[0];
-                int j = cur[1];
-                if (i == m - 1 && j == n - 1) {
-                    return step;
-                }
-                if (visited[i][j] != -1 && cur[2] >= visited[i][j]) continue;
-                visited[i][j] = cur[2];
-                for (int[] dir : dirs) {
-                    int row = i + dir[0];
-                    int col = j + dir[1];
-                    if (row < 0 || col < 0 || row >= m || col >= n) {
-                        continue;
-                    }
-                    int curK = cur[2];
-                    if (grid[row][col] == 1) {
-                        curK++;
-                    }
-                    if (curK > k) {
-                        continue;
-                    }
-                    queue.offer(new int[]{row, col, curK});
-                }
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+        pq.offer(new int[]{0, 0, 0, 0});
+        int[][][] visited = new int[m][n][k + 1];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                Arrays.fill(visited[i][j], Integer.MAX_VALUE);
             }
-            step++;
+        }
+        while (pq.size() > 0) {
+            int[] cur = pq.poll();
+            int i = cur[0];
+            int j = cur[1];
+            int step = cur[2];
+            int curK = cur[3];
+            if (i == m - 1 && j == n - 1) {
+                return step;
+            }
+            if (visited[i][j][curK] <= step) continue;
+            visited[i][j][curK] = step;
+            for (int[] dir : dirs) {
+                int row = i + dir[0];
+                int col = j + dir[1];
+                if (row < 0 || col < 0 || row >= m || col >= n) continue;
+                int nextK = curK;
+                if (grid[row][col] == 1) {
+                    nextK++;
+                }
+                if (nextK > k) continue;
+                pq.offer(new int[]{row, col, step + 1, nextK});
+            }
         }
         return -1;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
