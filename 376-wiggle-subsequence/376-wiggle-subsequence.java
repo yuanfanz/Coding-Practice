df70c7ebfe8d70d9c7bb53fc6448cd6fb46ab201
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-02 17:52:58 -0700

    Time: 0 ms (100.00%), Space: 42.6 MB (6.14%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.java
+++ 376-wiggle-subsequence/376-wiggle-subsequence.java
@@ -6,20 +6,16 @@ class Solution {
         
         int[] up = new int[n];
         int[] down = new int[n];
-        
         up[0] = 1;
         down[0] = 1;
         
         for (int i = 1; i < n; ++i) {
+            up[i] = up[i - 1];
+            down[i] = down[i - 1];
             if (nums[i] > nums[i - 1]) {
                 up[i] = down[i - 1] + 1;
-                down[i] = down[i - 1];
             } else if (nums[i] < nums[i - 1]) {
                 down[i] = up[i - 1] + 1;
-                up[i] = up[i - 1];
-            } else {
-                up[i] = up[i - 1];
-                down[i] = down[i - 1];
             }
         }
         return Math.max(up[n - 1], down[n - 1]);
