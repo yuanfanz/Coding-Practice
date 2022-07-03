f7daa03a53f7dd2ae273a00b59721b692efcd1e5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-03 11:09:31 -0700

    Time: 4 ms (22.78%), Space: 53.7 MB (69.99%) - LeetHub

--- 529-minesweeper/529-minesweeper.java
+++ 529-minesweeper/529-minesweeper.java
@@ -27,47 +27,19 @@ class Solution {
                 }
             }
             
-            if (i == 3 && j == 3) {
-                // System.out.println("mine: " + mine + " ");
-            }
-            
             if (mine > 0) { // this cell is not 'B', stop further BFS
                 board[i][j] = (char) ('0' + mine);
             } else {
                 board[i][j] = 'B';
-                int count = 0;
-                int index = 0;
                 for (int[] dir : dirs) {
                     int row = i + dir[0];
                     int col = j + dir[1];
-                    // if (i == 3 && j == 3) {
-                    //     System.out.println(row + " " + col + " ");
-                    // }
-                    if (row < 0 || col < 0 || row >= m || col >= n) {
-                        // if (i == 3 && j == 3) {
-                        //     System.out.println("INVALID: " + row + " " + col + " ");
-                        // }
-                        continue;
-                    }
-                    count++;
+                    if (row < 0 || col < 0 || row >= m || col >= n) continue;
                     if (board[row][col] == 'E') {
-                        if (i == 3 && j == 3) {
-                            // System.out.print(row + " " + col + " ");
-                        }
-                        index++;
                         queue.offer(new int[]{row, col});
                         board[row][col] = 'B';
-                    } else {
-                        if (i == 3 && j == 3) {
-                            // System.out.println("row: " + row + " col: " + col + " " + board[row][col] + " ");
-                        }
                     }
                 }
-                if (i == 3 && j == 3) {
-                    // System.out.println("count: " + count + " ");
-                    // System.out.println("index: " + index + " ");
-                }
-                // System.out.println();
             }
         }
         return board;
