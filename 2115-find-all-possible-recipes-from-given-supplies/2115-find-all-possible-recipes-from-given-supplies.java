cc61434135def8fa09fd0d60d164f3f1a21f9c06
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-03 20:53:05 -0700

    Time: 51 ms (82.56%), Space: 45.4 MB (88.72%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.java
@@ -1,76 +1,84 @@
 class Solution {
     public List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
-        Map<String, Integer> indegree = new HashMap<>();
         Map<String, Integer> indexMap = new HashMap<>();
+        Map<String, Integer> indegreeMap = new HashMap<>();
         Map<String, List<String>> graph = new HashMap<>();
-        int k = 0;
-        for (String r : recipes) {
-            indegree.put(r, 0);
-            indexMap.put(r, k++);
+        for (int i = 0; i < recipes.length; ++i) {
+            indexMap.put(recipes[i], i);
+            // only do topological sort to receipes
+            // ingredients does not need topo-sort
+            indegreeMap.put(recipes[i], 0);
         }
-        int n = ingredients.size();
-        for (int i = 0; i < n; ++i) {
+        // build graph of recipe and ingredients, build indegree
+        for (int i = 0; i < recipes.length; ++i) {
             String curRecipe = recipes[i];
-            List<String> list = ingredients.get(i);
-            for (String item : list) {
-                if (indegree.containsKey(item)) {
-                    indegree.put(curRecipe, indegree.get(curRecipe) + 1);
-                    List<String> nextList = graph.getOrDefault(item, new ArrayList<>());
-                    nextList.add(curRecipe);
-                    graph.put(item, nextList);
+            List<String> ingredientsList = ingredients.get(i);
+            for (String ingredient : ingredientsList) {
+                if (indegreeMap.containsKey(ingredient)) { // if this ingredient is one of recipes
+                    indegreeMap.put(curRecipe, indegreeMap.getOrDefault(curRecipe, 0) + 1);
+                    // build graph
+                    List<String> list = graph.getOrDefault(ingredient, new ArrayList<>());
+                    list.add(curRecipe);
+                    graph.put(ingredient, list);
                 }
             }
         }
+        // current supplies we already have
         Set<String> supplySet = new HashSet<>();
         for (String s : supplies) {
             supplySet.add(s);
         }
         Queue<String> queue = new LinkedList<>();
-        Set<String> result = new HashSet<>();
-        for (String r : indegree.keySet()) {
-            if (indegree.get(r) == 0) {
-                int index = indexMap.get(r);
-                List<String> list = ingredients.get(index);
-                boolean notEnough = false;
-                for (String item : list) {
-                    if (!supplySet.contains(item)) {
-                        notEnough = true;
-                        break;
-                    }
+        // only put 0 indegree recipe into queue first
+        for (String key : indegreeMap.keySet()) {
+            if (indegreeMap.get(key) == 0) {
+                // need to check if we can make this recipe
+                if (checkCanMakeRecipe(key, indexMap, supplySet, ingredients)) {
+                    queue.offer(key);
                 }
-                if (notEnough) continue;
-                queue.offer(r);
             }
         }
+        Set<String> result = new HashSet<>();
         while (queue.size() > 0) {
-            String cur = queue.poll();
-            result.add(cur);
-            supplySet.add(cur);
-            if (graph.containsKey(cur)) {
-                for (String next : graph.get(cur)) {
-                    int val = indegree.get(next);
-                    if (val < 0) continue;
-                    val--;
-                    if (val == 0) {
-                        int index = indexMap.get(next);
-                        List<String> list = ingredients.get(index);
-                        boolean notEnough = false;
-                        for (String item : list) {
-                            if (!supplySet.contains(item)) {
-                                notEnough = true;
-                                break;
-                            }
+            String curRecipe = queue.poll();
+            supplySet.add(curRecipe);
+            result.add(curRecipe);
+            // do BFS
+            if (graph.containsKey(curRecipe)) {
+                List<String> nextRecipeList = graph.get(curRecipe);
+                for (String nextRecipe : nextRecipeList) {
+                    int nextIndegree = indegreeMap.get(nextRecipe);
+                    if (nextIndegree <= 0) continue;
+                    nextIndegree--;
+                    if (nextIndegree == 0) {
+                        if (checkCanMakeRecipe(nextRecipe, indexMap, supplySet, ingredients)) {
+                            queue.offer(nextRecipe);
                         }
-                        if (notEnough) continue;
-                        queue.offer(next);
                     }
-                    indegree.put(next, val);
+                    indegreeMap.put(nextRecipe, nextIndegree);
                 }
             }
         }
         return new ArrayList<>(result);
     }
+    
+    private boolean checkCanMakeRecipe(String key, Map<String, Integer> indexMap,
+                                      Set<String> supplySet, List<List<String>> ingredients) {
+        int index = indexMap.get(key);
+        List<String> ingredientsList = ingredients.get(index);
+        for (String s : ingredientsList) {
+            if (!supplySet.contains(s)) return false;
+        }
+        return true;
+    }
 }
+
+
+
+
+
+
+
 
 
 
