83a9285028e8c6561eea38030493a521a9d06968
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-03 19:23:14 -0700

    Time: 0 ms (100.00%), Space: 42.2 MB (72.29%) - LeetHub

--- 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
+++ 366-find-leaves-of-binary-tree/366-find-leaves-of-binary-tree.java
@@ -3,21 +3,24 @@ class Solution {
     public List<List<Integer>> findLeaves(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
         
-        dfs(root, result);
+        while (root != null) {
+            List<Integer> list = new ArrayList<>();
+            root = dfs(result, root, list);
+            result.add(list);
+        }
         return result;
     }
     
-    private int dfs(TreeNode root, List<List<Integer>> result) {
-        if (root == null) return -1;
-        int left = dfs(root.left, result);
-        int right = dfs(root.right, result);
-        
-        int height = Math.max(left, right) + 1;
-        if (height == result.size()) {
-            result.add(new ArrayList<>());
+    private TreeNode dfs(List<List<Integer>> result, TreeNode root, 
+                        List<Integer> list) {
+        if (root == null) return null;
+        if (root.left == null && root.right == null) {
+            list.add(root.val);
+            return null;
         }
-        result.get(height).add(root.val);
-        return height;
+        root.left = dfs(result, root.left, list);
+        root.right = dfs(result, root.right, list);
+        return root;
     }
 }
 
