46f6a287b12f07ad1b5c90adcaadd1e1afa4cac6
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-03 21:12:44 -0700

    Time: 380 ms (5.05%), Space: 168.1 MB (26.60%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.java
@@ -1,28 +1,34 @@
 class StockPrice {
-    TreeMap<Integer, Integer> map;
+    // timeMap: key is timestamp, value is price
+    TreeMap<Integer, Integer> timeMap;
+    // valueMap: key is price, set contains timestamps
     TreeMap<Integer, Set<Integer>> valueMap;
     public StockPrice() {
-        map = new TreeMap<>();
+        timeMap = new TreeMap<>();
         valueMap = new TreeMap<>();
     }
     
     public void update(int timestamp, int price) {
-        if (map.containsKey(timestamp)) {
-            int oldPrice = map.get(timestamp);
-            Set<Integer> set = valueMap.get(oldPrice);
+        if (timeMap.containsKey(timestamp)) {
+            int prevPrice = timeMap.get(timestamp);
+            timeMap.put(timestamp, price);
+            Set<Integer> set = valueMap.get(prevPrice);
             set.remove(timestamp);
             if (set.size() == 0) {
-                valueMap.remove(oldPrice);
+                valueMap.remove(prevPrice);
+            } else {
+                valueMap.put(prevPrice, set);
             }
+        } else {
+            timeMap.put(timestamp, price);
         }
-        map.put(timestamp, price);
         Set<Integer> set = valueMap.getOrDefault(price, new HashSet<>());
         set.add(timestamp);
         valueMap.put(price, set);
     }
     
     public int current() {
-        return map.get(map.lastKey());
+        return timeMap.get(timeMap.lastKey());
     }
     
     public int maximum() {
