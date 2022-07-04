675ce23d2f990384603e5122a0cde5a77b6bd617
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-03 20:06:26 -0700

    Time: 4 ms (25.56%), Space: 64.9 MB (72.99%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.java
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.java
@@ -3,31 +3,20 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         
-        List<Integer> cols = new ArrayList<>();
-        for (int i = 0; i < n; ++i) {
-            if (grid[0][i] == 1) {
-                cols.add(i);
-            }
-        }
-        for (int i : cols) {
-            flipCol(grid, i);
-        }
-        
-        for (int i = 1; i < m; ++i) {
-            for (int j = 1; j < n; ++j) {
-                if (grid[i][0] == 0) {
-                    if (grid[i][j] == 1) return false;
-                } else {
-                    if (grid[i][j] == 0) return false;
-                }
+        for (int i = 0; i < m - 1; ++i) {
+            for (int j = 0; j < n - 1; ++j) {
+                if (check(grid, i, j)) return false;
             }
         }
         return true;
     }
     
-    private void flipCol(int[][] grid, int col) {
-        for (int i = 0; i < grid.length; ++i) {
-            grid[i][col] = 1 - grid[i][col];
-        }
+    private boolean check(int[][] grid, int i, int j) {
+        int count = 0;
+        count += grid[i][j];
+        count += grid[i][j + 1];
+        count += grid[i + 1][j];
+        count += grid[i + 1][j + 1];
+        return count % 2 == 1;
     }
 }
