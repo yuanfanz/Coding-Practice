bc389e3c1b08f35684f8e5a379348f0e5ed6beb5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-15 22:42:28 -0700

    Time: 362 ms (22.75%), Space: 101.3 MB (13.48%) - LeetHub

--- 935-knight-dialer/935-knight-dialer.java
+++ 935-knight-dialer/935-knight-dialer.java
@@ -1,43 +1,30 @@
 class Solution {
     public int knightDialer(int n) {
         int mod = (int) Math.pow(10, 9) + 7;
-        long[][] dp = new long[n + 1][10];
         
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        map.put(0, Arrays.asList(4, 6));
+        map.put(1, Arrays.asList(6, 8));
+        map.put(2, Arrays.asList(7, 9));
+        map.put(3, Arrays.asList(4, 8));
+        map.put(4, Arrays.asList(3, 9, 0));
+        map.put(5, new ArrayList<>());
+        map.put(6, Arrays.asList(1, 7, 0));
+        map.put(7, Arrays.asList(2, 6));
+        map.put(8, Arrays.asList(1, 3));
+        map.put(9, Arrays.asList(2, 4));
+        
+        long[][] dp = new long[n + 1][10];
         for (int i = 0; i <= 9; ++i){
             dp[1][i] = 1;
         }
-        // Map<Integer, List<Integer>> map = new HashMap<>();
-        // map.put(0, Arrays.asList(4, 6));
-        // map.put(1, Arrays.asList(6, 8));
-        // map.put(2, Arrays.asList(7, 9));
-        // map.put(3, Arrays.asList(4, 6));
-        // map.put(4, Arrays.asList(4, 6));
-        // map.put(5, Arrays.asList(4, 6));
-        // map.put(6, Arrays.asList(4, 6));
-        // map.put(7, Arrays.asList(4, 6));
-        // map.put(8, Arrays.asList(4, 6));
-        // map.put(9, Arrays.asList(4, 6));
-        
-        // 4, 5, 6
         for (int i = 2; i <= n; ++i) {
-            dp[i][0] = dp[i - 1][4] + dp[i - 1][6];
-            dp[i][0] %= mod;
-            dp[i][1] = dp[i - 1][6] + dp[i - 1][8];
-            dp[i][1] %= mod;
-            dp[i][2] = dp[i - 1][7] + dp[i - 1][9];
-            dp[i][2] %= mod;
-            dp[i][3] = dp[i - 1][4] + dp[i - 1][8];
-            dp[i][3] %= mod;
-            dp[i][4] = dp[i - 1][3] + dp[i - 1][9] + dp[i - 1][0];
-            dp[i][4] %= mod;
-            dp[i][6] = dp[i - 1][1] + dp[i - 1][7] + dp[i - 1][0];
-            dp[i][6] %= mod;
-            dp[i][7] = dp[i - 1][2] + dp[i - 1][6];
-            dp[i][7] %= mod;
-            dp[i][8] = dp[i - 1][1] + dp[i - 1][3];
-            dp[i][8] %= mod;
-            dp[i][9] = dp[i - 1][2] + dp[i - 1][4];
-            dp[i][9] %= mod;
+            for (int j = 0; j <= 9; ++j){
+                for (int prev : map.get(j)) {
+                    dp[i][j] += dp[i - 1][prev];
+                    dp[i][j] %= mod;
+                }
+            }
         }
         int res = 0;
         for (int i = 0; i <= 9; ++i){
