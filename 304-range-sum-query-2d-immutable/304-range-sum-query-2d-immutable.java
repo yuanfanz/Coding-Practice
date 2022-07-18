ab6ce3cbafec5f1f1f485e18acd9e8585caa8a9b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-17 17:27:23 -0700

    Time: 163 ms (76.73%), Space: 127.7 MB (58.69%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -1,11 +1,17 @@
 class NumMatrix {
-    private int[][] dp;
+    int[][] dp;
     public NumMatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
         
         dp = new int[m + 1][n + 1];
-        dp[0][0] = 0;
+        dp[0][0] = matrix[0][0];
+        for (int i = 1; i <= m; ++i) {
+            dp[i][0] = dp[i - 1][0] + matrix[i - 1][0];
+        }
+        for (int i = 1; i <= n; ++i) {
+            dp[0][i] = dp[0][i - 1] + matrix[0][i - 1];
+        }
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
@@ -14,13 +20,13 @@ class NumMatrix {
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int minRow = Math.min(row1, row2);
-        int maxRow = Math.max(row1, row2);
-        int minCol = Math.min(col1, col2);
-        int maxCol = Math.max(col1, col2);
+        int rowMin = Math.min(row1, row2);
+        int rowMax = Math.max(row1, row2);
+        int colMin = Math.min(col1, col2);
+        int colMax = Math.max(col1, col2);
         
-        return dp[maxRow + 1][maxCol + 1] - dp[minRow][maxCol + 1]
-            - dp[maxRow + 1][minCol] + dp[minRow][minCol];
+        return dp[rowMax + 1][colMax + 1] - dp[rowMin][colMax + 1] - dp[rowMax + 1][colMin]
+            + dp[rowMin][colMin];
     }
 }
 
