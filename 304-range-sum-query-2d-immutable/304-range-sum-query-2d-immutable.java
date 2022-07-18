6a232db8d0b220c189e3a9b5353764346ff6a5b9
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-17 17:58:00 -0700

    Time: 167 ms (74.53%), Space: 128.3 MB (53.76%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.java
@@ -6,12 +6,6 @@ class NumMatrix {
         
         dp = new int[m + 1][n + 1];
         for (int i = 1; i <= m; ++i) {
-            dp[i][0] = dp[i - 1][0] + matrix[i - 1][0];
-        }
-        for (int i = 1; i <= n; ++i) {
-            dp[0][i] = dp[0][i - 1] + matrix[0][i - 1];
-        }
-        for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + matrix[i - 1][j - 1];
             }
