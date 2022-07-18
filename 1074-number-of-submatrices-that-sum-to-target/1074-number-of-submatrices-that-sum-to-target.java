7fbcfd8f8401c7e139055233d0ea9d8ab45e686d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-17 18:00:21 -0700

    Time: 241 ms (59.55%), Space: 117.9 MB (11.41%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.java
@@ -5,27 +5,26 @@ class Solution {
         
         int[][] presum = new int[m + 1][n + 1];
         for (int i = 1; i <= m; ++i) {
-            presum[i][0] = presum[i - 1][0] + matrix[i - 1][0];
-        }
-        for (int i = 1; i <= n; ++i) {
-            presum[0][i] = presum[0][i - 1] + matrix[0][i - 1];
-        }
-        for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 presum[i][j] = matrix[i - 1][j - 1] + presum[i][j - 1] 
                     + presum[i - 1][j] - presum[i - 1][j - 1];
             }
         }
         int count = 0;
-        for (int i = 1; i <= m; ++i) {
-            for (int j = 1; j <= n; ++j) {
-                for (int p = 1; p <= i; ++p) {
-                    for (int q = 1; q <= j; ++q) {
-                        if (presum[i][j] - presum[p - 1][j]
-                            - presum[i][q - 1] + presum[p - 1][q - 1] == target) {
-                            count++;
-                        }
+        for (int top = 1; top <= m; ++top) {
+            for (int bottom = top; bottom <= m; ++bottom) {
+                int area = 0;
+                // key is area, value is frequency for this area
+                Map<Integer, Integer> map = new HashMap<>();
+                // map.put(0, 1);
+                // calculating presum of areas
+                for (int col = 1; col <= n; ++col) {
+                    area = presum[bottom][col] - presum[top - 1][col];
+                    if (area == target) count++;
+                    if (map.containsKey(area - target)) {
+                        count += map.get(area - target);
                     }
+                    map.put(area, map.getOrDefault(area, 0) + 1);
                 }
             }
         }
