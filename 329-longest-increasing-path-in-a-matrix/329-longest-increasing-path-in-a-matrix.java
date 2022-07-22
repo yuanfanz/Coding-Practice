ae7517d39cecf379209399d9f92c6f83e525e60b
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-22 16:14:56 -0700

    Time: 16 ms (34.96%), Space: 54.2 MB (43.77%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.java
@@ -2,69 +2,47 @@ class Solution {
     public int longestIncreasingPath(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
         
-        int[][] indegree = new int[m][n];
+        int[][] dirs = new int[][]{{0,1},{0,-1},{1,0},{-1,0}};
+        int[][] memo = new int[m][n];
+        for (int[] cur : memo) {
+            Arrays.fill(cur, -1);
+        }
+        
+        int max = 0;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                for (int[] dir : dirs) {
-                    int row = i + dir[0];
-                    int col = j + dir[1];
-                    if (row < 0 || col < 0 || row >= m || col >= n) {
-                        continue;
-                    }
-                    if (matrix[row][col] > matrix[i][j]) {
-                        indegree[row][col]++;
-                    }
-                }
+                // pick every cell as a start point
+                max = Math.max(dfs(i, j, memo, matrix, dirs), max);
             }
         }
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (indegree[i][j] == 0) {
-                    queue.offer(new int[]{i, j});
-                }
+        return max;
+    }
+    
+    private int dfs(int i, int j, int[][] memo, int[][] matrix, int[][] dirs) {
+        if (memo[i][j] != -1) return memo[i][j];
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int result = 1;
+        for (int[] dir : dirs) {
+            int row = i + dir[0];
+            int col = j + dir[1];
+            if (!checkBoundry(row, col, m, n)) continue;
+            if (matrix[row][col] > matrix[i][j]) {
+                result = Math.max(result, 1 + dfs(row, col, memo, matrix, dirs));
             }
         }
-        int step = 0;
-        while (queue.size() > 0) {
-            int size = queue.size();
-            for (int k = 0; k < size; ++k) {
-                int[] cur = queue.poll();
-                int i = cur[0];
-                int j = cur[1];
-                for (int[] dir : dirs) {
-                    int row = i + dir[0];
-                    int col = j + dir[1];
-                    if (row < 0 || col < 0 || row >= m || col >= n) {
-                        continue;
-                    }
-                    if (matrix[row][col] > matrix[i][j]) {
-                        indegree[row][col]--;
-                        if (indegree[row][col] == 0) {
-                            queue.offer(new int[]{row, col});
-                        }
-                    }
-                }
-            }
-            step++;
+        // if (result >= memo[i][j]) {
+            memo[i][j] = result;
+        // }
+        return result;
+    }
+    
+    private boolean checkBoundry(int row, int col, int m, int n) {
+        if (row < 0 || col < 0 || row >= m || col >= n) {
+            return false;
         }
-        return step;
+        return true;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
