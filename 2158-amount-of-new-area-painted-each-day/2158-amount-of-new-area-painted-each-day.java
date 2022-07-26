582a3e5ff98c2588a68edaff55c143d931515f00
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-26 10:46:43 -0700

    Time: 14 ms (87.37%), Space: 213 MB (5.01%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.java
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.java
@@ -1,71 +1,22 @@
 class Solution {
     public int[] amountPainted(int[][] paint) {
-        Node root = new Node();
-        int N = (int) Math.pow(10, 5);
-        List<Integer> list = new ArrayList<>();
-        for (int[] cur : paint) {
-            int left = cur[0];
-            int right = cur[1];
-            int len = query(root, 0, N, left, right - 1);
-            list.add(right - left - len);
-            update(root, 0, N, left, right - 1, 1);
-        }
-        return list.stream().mapToInt(i -> i).toArray();
-    }
-    
-    public int query(Node node, int start, int end, int left, int right) {
-        if (left <= start && end <= right) {
-            return node.len;
-        }
-        int mid = start + (end - start) / 2;
-        pushDown(node, mid - start + 1, end - mid);
-        int res = 0;
-        if (left <= mid) {
-            res += query(node.left, start, mid, left, right);
-        }
-        if (right > mid) {
-            res += query(node.right, mid + 1, end, left, right);
+        int n = paint.length;
+        
+        int[] res = new int[n];
+        int[] arr = new int[50001];
+        for (int i = 0; i < paint.length; ++i) {
+            int start = paint[i][0];
+            int end = paint[i][1];
+            while (start < end) {
+                // either next cell, or next non-paint cell
+                int jump = Math.max(start + 1, arr[start]);
+                // paint a new cell, add 1 to current paint day
+                res[i] += arr[start] == 0 ? 1 : 0;
+                
+                arr[start] = Math.max(arr[start], end);
+                start = jump;
+            }
         }
         return res;
-    }
-    
-    public void update(Node node, int start, int end, int left, int right, int val) {
-        if (left <= start && end <= right) {
-            node.len = end - start + 1;
-            node.add = 1;
-            return;
-        }
-        int mid = start + (end - start) / 2;
-        pushDown(node, mid - start + 1, end - mid);
-        if (left <= mid) {
-            update(node.left, start, mid, left, right, val);
-        }
-        if (right > mid) {
-            update(node.right, mid + 1, end, left, right, val);
-        }
-        pushUp(node);
-    }
-    
-    public void pushDown(Node node, int leftNum, int rightNum) {
-        if (node.left == null) node.left = new Node();
-        if (node.right == null) node.right = new Node();
-        if (node.add == 0) return;
-        
-        node.left.len = leftNum;
-        node.right.len = rightNum;
-        
-        node.left.add = node.add;
-        node.right.add = node.add;
-        node.add = 0;
-    }
-    
-    public void pushUp(Node node) {
-        node.len = node.left.len + node.right.len;
-    }
-    
-    class Node{
-        Node left, right;
-        int len;
-        int add;
     }
 }
