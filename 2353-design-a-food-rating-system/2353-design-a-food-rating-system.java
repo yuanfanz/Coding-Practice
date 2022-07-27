7c7daaf7ef9b8007909cce829df8f61c0b0a24e6
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-26 19:47:25 -0700

    Time: 1289 ms (33.33%), Space: 167.5 MB (16.67%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.java
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.java
@@ -1,47 +1,43 @@
 class FoodRatings {
-    Map<String, String> map;
-    Map<String, Integer> map2;
-    Map<String, TreeMap<Integer, TreeSet<String>>> ratingMap;
+
+    Map<String, Node> map;
+    Map<String, PriorityQueue<Node>> cuisineMap;
     public FoodRatings(String[] foods, String[] cuisines, int[] ratings) {
         map = new HashMap<>();
-        map2 = new HashMap<>();
-        ratingMap = new HashMap<>();
-        for (String cu : cuisines) {
-            ratingMap.put(cu, new TreeMap<>());
-        }
+        cuisineMap = new HashMap<>();
         for (int i = 0; i < foods.length; ++i) {
-            String cur = foods[i];
             String cu = cuisines[i];
-            map.put(cur, cu);
-            map2.put(cur, ratings[i]);
-            
-            TreeMap<Integer, TreeSet<String>> submap = ratingMap.get(cu);
-            TreeSet<String> nameSet = submap.getOrDefault(ratings[i], new TreeSet<>());
-            nameSet.add(cur);
-            submap.put(ratings[i], nameSet);
-            ratingMap.put(cu, submap);
+            PriorityQueue<Node> pq = cuisineMap.getOrDefault
+                (cu, new PriorityQueue<>((a, b) -> 
+                                a.rate == b.rate ? a.food.compareTo(b.food) : b.rate - a.rate));
+            Node node = new Node(foods[i], cu, ratings[i]);
+            pq.offer(node);
+            cuisineMap.put(cu, pq);
+            map.put(foods[i], node);
         }
     }
     
     public void changeRating(String food, int newRating) {
-        String cu = map.get(food);
-        int prevRating = map2.get(food);
-        map2.put(food, newRating);
-        TreeMap<Integer, TreeSet<String>> submap = ratingMap.get(cu);
-        TreeSet<String> nameSet = submap.get(prevRating);
-        nameSet.remove(food);
-        if (nameSet.size() == 0) {
-            submap.remove(prevRating);
-        }
-        TreeSet<String> nameSet2 = submap.getOrDefault(newRating, new TreeSet<>());
-        nameSet2.add(food);
-        submap.put(newRating, nameSet2);
-        ratingMap.put(cu, submap);
+        Node node = map.get(food);
+        PriorityQueue<Node> pq = cuisineMap.get(node.cu);
+        pq.remove(node);
+        node.rate = newRating;
+        pq.offer(node);
     }
     
     public String highestRated(String cuisine) {
-        TreeMap<Integer, TreeSet<String>> submap = ratingMap.get(cuisine);
-        return submap.get(submap.lastKey()).first();
+        return cuisineMap.get(cuisine).peek().food;
+    }
+    
+    class Node{
+        String food;
+        String cu;
+        int rate;
+        public Node(String food, String cu, int rate) {
+            this.food = food;
+            this.cu = cu;
+            this.rate = rate;
+        }
     }
 }
 
