f57ed7ed30334da043d57d495ff13275d0aacc9d
Yuanfan Zhang <yuanfanz@uci.edu>
2022-07-28 18:58:07 -0700

    Time: 2 ms (91.18%), Space: 43.4 MB (24.21%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.java
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.java
@@ -1,21 +1,21 @@
 class Solution {
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
-        // key is the index in s, value is index in indices
         Map<Integer, Integer> map = new HashMap<>();
-        
         for (int i = 0; i < indices.length; ++i) {
             if (s.startsWith(sources[i], indices[i])) {
                 map.put(indices[i], i);
             }
         }
+        
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < s.length();) {
+        int i = 0;
+        while (i < s.length()) {
             if (map.containsKey(i)) {
-                sb.append(targets[map.get(i)]);
-                i += sources[map.get(i)].length();
+                int index = map.get(i);
+                sb.append(targets[index]);
+                i += sources[index].length();
             } else {
-                sb.append(s.charAt(i));
-                i++;
+                sb.append(s.charAt(i++));
             }
         }
         return sb.toString();
