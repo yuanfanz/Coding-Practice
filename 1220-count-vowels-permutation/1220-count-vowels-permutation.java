1e22e44ebc569f67ef3df1ae41dd3503b4f672a8
Yuanfan Zhang <yuanfanz@uci.edu>
2022-08-07 09:23:11 -0700

    Time: 56 ms (29.01%), Space: 53 MB (36.51%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.java
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.java
@@ -6,15 +6,10 @@ class Solution {
             dp[1][i] = 1;
         }
         for (int i = 2; i <= n; ++i) {
-            // a 
             dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % mod;
-            // dp[i][0] %= mod;
             dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
-            // dp[i][1] %= mod;
             dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % mod;
-            // dp[i][2] %= mod;
             dp[i][3] = dp[i - 1][2] % mod;
-            // dp[i][3] %= mod;
             dp[i][4] = (dp[i - 1][2] + dp[i - 1][3]) % mod;
         }
         long res = 0;
