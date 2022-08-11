42c30579033b56843d26ace39e3b094a92b893cc
Yuanfan Zhang <yuanfanz@uci.edu>
2022-08-10 18:14:54 -0700

    Time: 0 ms (100.00%), Space: 41.8 MB (92.88%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.java
@@ -6,7 +6,11 @@ class Solution {
     
     private boolean isValid(TreeNode root, Integer min, Integer max) {
         if (root == null) return true;
-        if (min != null && root.val <= min || max != null && root.val >= max) {
+        
+        if (min != null && root.val <= min) {
+            return false;
+        }
+        if (max != null && root.val >= max) {
             return false;
         }
         return isValid(root.left, min, root.val) && isValid(root.right, root.val, max);
