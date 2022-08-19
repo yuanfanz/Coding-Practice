26585170aa74b0c98a7e68e0a9e51fb4e5cd65bb
Yuanfan Zhang <yuanfanz@uci.edu>
2022-08-19 12:29:23 -0700

    Time: 11 ms (62.34%), Space: 84.8 MB (56.48%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.java
@@ -10,27 +10,12 @@ class Solution {
                 dp[i] = dp[i] || dp[i - 2];
             }  
             if (i >= 3) {
-                
-//                 System.out.println(nums[i - 1]);
-//                 System.out.println(nums[i - 2]);
-//                 System.out.println(nums[i - 3]);
-                
-//                 System.out.println(nums[i - 1] - nums[i - 2]);
-//                 System.out.println(nums[i - 2] - nums[i - 3]);
-                
                 if ((nums[i - 1] - nums[i - 2] == 1 && nums[i - 2] - nums[i - 3] == 1)
                    || (nums[i - 1] == nums[i - 2] && nums[i - 2] == nums[i - 3])) {
                     dp[i] = dp[i] || dp[i - 3];
                 }
             }
         }
-        // print(dp);
         return dp[n];
-    }
-    private void print(boolean[] grid) {
-        for (int i = 0; i < grid.length; ++i) {
-            System.out.print(grid[i] + " ");
-        }
-        System.out.println();
     }
 }
