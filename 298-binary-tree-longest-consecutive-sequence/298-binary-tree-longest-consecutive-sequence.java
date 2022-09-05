6870b531a1ae1597d629b377b23d2858cce22fe4
Yuanfan Zhang <yuanfanz@uci.edu>
2022-09-05 14:41:52 -0700

    Time: 1 ms (100.00%), Space: 50.6 MB (70.79%) - LeetHub

--- 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.java
+++ 298-binary-tree-longest-consecutive-sequence/298-binary-tree-longest-consecutive-sequence.java
@@ -1,3 +1,25 @@
+class Solution {
+    private int max = 0;
+    public int longestConsecutive(TreeNode root) {
+        if (root == null) return 0;
+        dfs(root, root.val + 1, 0);
+        return max;
+    }
+    
+    private void dfs(TreeNode root, int target, int len) {
+        if (root == null) return;
+        
+        if (root.val == target) {
+            len++;
+        } else {
+            len = 1;
+        }
+        max = Math.max(max, len);
+        dfs(root.left, root.val + 1, len);
+        dfs(root.right, root.val + 1, len);
+    }
+}
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -13,37 +35,3 @@
  *     }
  * }
  */
-class Solution {
-    private int max = 1;
-    public int longestConsecutive(TreeNode root) {
-        dfs(root, 1, root.val);
-        return max;
-    }
-    
-    private void dfs(TreeNode root, int len, int target) {
-        if (root == null) return;
-        
-        if (root.val == target) {
-            max = Math.max(max, len);
-        } else {
-            len = 1;
-        }
-        dfs(root.left, len + 1, root.val + 1);
-        dfs(root.right, len + 1, root.val + 1);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
