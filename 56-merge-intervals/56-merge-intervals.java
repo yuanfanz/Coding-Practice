75dd8b6ff2b622c5e92d457b8792843067c7d496
Yuanfan Zhang <yuanfanz@uci.edu>
2022-09-10 18:58:28 -0700

    Time: 18 ms (25.78%), Space: 55.5 MB (34.33%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -4,14 +4,12 @@ class Solution {
         
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         
-        int[] prev = intervals[0];
-        list.add(prev);
+        list.add(intervals[0]);
         for (int i = 1; i < intervals.length; ++i) {
-            int[] cur = intervals[i];
-            if (cur[0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], cur[1]);
+            int[] last = list.get(list.size() - 1);
+            if (intervals[i][0] <= last[1]) {
+                last[1] = Math.max(last[1], intervals[i][1]);
             } else {
-                prev = cur;
                 list.add(intervals[i]);
             }
         }
