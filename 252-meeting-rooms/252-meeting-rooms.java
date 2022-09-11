9effc442e8494bb197299899fd0337bd79594db5
Yuanfan Zhang <yuanfanz@uci.edu>
2022-09-10 19:03:59 -0700

    Time: 9 ms (52.57%), Space: 44.8 MB (62.36%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,11 +1,16 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return true;
+        }
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
         
+        int prev = intervals[0][1];
         for (int i = 1; i < intervals.length; ++i) {
-            if (intervals[i][0] < intervals[i - 1][1]) {
+            if (intervals[i][0] < prev) {
                 return false;
             }
+            prev = Math.max(prev, intervals[i][1]);
         }
         return true;
     }
