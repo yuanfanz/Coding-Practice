fef8013b9670b2521a7a14ac6663abb421029795
Yuanfan Zhang <yuanfanz@uci.edu>
2022-09-11 18:59:07 -0700

    Time: 65 ms (60.99%), Space: 148.2 MB (30.50%) - LeetHub

--- 2345-finding-the-number-of-visible-mountains/2345-finding-the-number-of-visible-mountains.java
+++ 2345-finding-the-number-of-visible-mountains/2345-finding-the-number-of-visible-mountains.java
@@ -1,29 +1,32 @@
 // copied from discuss
 
 class Solution {
-    public int visibleMountains(int [][] peaks){
-        for(int i = 0; i < peaks.length; i ++){
-            int temp = peaks[i][0];
-            peaks[i][0] -= peaks[i][1];
-            peaks[i][1] += temp;
+    public int visibleMountains(int[][] peaks){
+        // convert peaks coordinates into start and end of mountains
+        for(int i = 0; i < peaks.length; ++i){
+            int middle = peaks[i][0];
+            int height = peaks[i][1];
+            peaks[i][0] -= height;
+            peaks[i][1] = middle + height;
         }
+        // sort array by start, then by end
         Arrays.sort(peaks, (a, b) -> a[0] == b[0] ? a[1] - b[1]: a[0] - b[0]);
-        int size = 0;
+        int count = 0;
         int i = 0;
-        while(i < peaks.length){
-            size++;
+        while (i < peaks.length) {
+            count++;
             if(i + 1 == peaks.length){
-                return size;
+                return count;
             } 
             int currEnd = peaks[i][1];
             if(peaks[i][0] == peaks[i + 1][0]){
-                size--;
+                count--;
             }
             while(i + 1 != peaks.length && currEnd >= peaks[i + 1][1]){
                 i++;
             }
             i++;
         }
-        return size;
+        return count;
     }
 }
