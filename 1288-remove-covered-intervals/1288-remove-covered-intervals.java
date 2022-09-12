a878ad8b067d661135acf779fa9a04ede8488756
Yuanfan Zhang <yuanfanz@uci.edu>
2022-09-11 17:28:30 -0700

    Time: 21 ms (5.88%), Space: 47.1 MB (14.79%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -2,14 +2,13 @@ class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
         
-        int prevEnd = intervals[0][1];
         int count = 0;
+        int prev = intervals[0][1];
         for (int i = 1; i < intervals.length; ++i) {
-            if (intervals[i][1] <= prevEnd) {
+            if (intervals[i][1] <= prev) {
                 count++;
-            } else {
-                prevEnd = intervals[i][1];
             }
+            prev = Math.max(prev, intervals[i][1]);
         }
         return intervals.length - count;
     }
