795dde134dff303166170139fe582ebc624768a1
Yuanfan Zhang <yuanfanz@uci.edu>
2022-10-20 18:50:29 -0700

    Time: 36 ms (77.44%), Space: 74.5 MB (80.58%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,29 +1,9 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        // for (int i = 0; i < nums.length; ++i) {
-        //     // System.out.println("i: " + i);
-        //     for (int j = i - k; j <= i + k && j < nums.length; ++j) {
-        //         // System.out.println("j: " + j);
-        //         if (i == j || j < 0) continue;
-        //         if (nums[i] == nums[j]) return true;
-        //     }
-        // }
-        // return false;
-        
-        Map<Integer, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < nums.length; ++i) {
-            int num = nums[i];
-            List<Integer> list = map.getOrDefault(num, new ArrayList<>());
-            list.add(i);
-            map.put(num, list);
-        }
-        for (int key : map.keySet()) {
-            if (map.get(key).size() < 2) continue;
-            List<Integer> list = map.get(key);
-            Collections.sort(list);
-            for (int i = 1; i < list.size(); ++i) {
-                if (list.get(i) - list.get(i - 1) <= k) return true;
-            }
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; ++i){
+            if (i > k) set.remove(nums[i - k - 1]);
+            if (!set.add(nums[i])) return true;
         }
         return false;
     }
