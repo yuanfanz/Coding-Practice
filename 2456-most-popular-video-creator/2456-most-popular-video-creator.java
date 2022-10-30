2c359d822614fbcb031a4e4d93d3d9c6bc5bd01a
Yuanfan Zhang <yuanfanz@uci.edu>
2022-10-30 13:35:59 -0700

    Time: 507 ms (16.67%), Space: 324.4 MB (16.67%) - LeetHub

--- 2456-most-popular-video-creator/2456-most-popular-video-creator.java
+++ 2456-most-popular-video-creator/2456-most-popular-video-creator.java
@@ -5,16 +5,6 @@ class Solution {
         Map<String, Integer> idView = new HashMap<>();
         
         for (int i = 0; i < ids.length; ++i) {
-            // if (ids[i].equals("l")) {
-            //     System.out.print(creators[i] + " ");
-            //     System.out.print("l" + " ");
-            //     System.out.println(views[i]);
-            // }
-            // if (ids[i].equals("dfsw")) {
-            //     System.out.print(creators[i] + " ");
-            //     System.out.print("dfsw" + " ");
-            //     System.out.println(views[i]);
-            // }
             String name_key = creators[i] + "_" + ids[i];
             idView.put(name_key, Math.max(idView.getOrDefault(name_key, 0), views[i]));
         }
@@ -31,12 +21,6 @@ class Solution {
             } else {
                 int high = idView.get(name_key);
                 int lasthigh = idView.get(last_name_key);
-                // if (name.equals("v")) {
-                //     System.out.print(high + " ");
-                //     System.out.print(lasthigh + " ");
-                //     System.out.print(id + " ");
-                //     System.out.println(lastId);
-                // }
                 if (high >= lasthigh) {
                     if (high == lasthigh) {
                         if (id.compareTo(lastId) < 0) {
@@ -53,11 +37,8 @@ class Solution {
         for (String key : nameView.keySet()) {
             max = Math.max(max, nameView.get(key));
         }
-            // System.out.println(max);
         List<String> list = new ArrayList<>();
         for (String key : nameView.keySet()) {
-            // System.out.print(key + " ");
-            // System.out.println(nameView.get(key));
             if (nameView.get(key) == max) {
                 list.add(key);
             }
@@ -72,11 +53,3 @@ class Solution {
         return result;
     }
 }
-
-
-
-
-
-
-
-
