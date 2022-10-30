9281383a71c8533822da35237e67f35920ea7eb2
Yuanfan Zhang <yuanfanz@uci.edu>
2022-10-30 14:53:36 -0700

    Time: 0 ms (100.00%), Space: 42.6 MB (95.93%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.java
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.java
@@ -1,7 +1,16 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface BinaryMatrix {
+ *     public int get(int row, int col) {}
+ *     public List<Integer> dimensions {}
+ * };
+ */
 
 class Solution {
     public int leftMostColumnWithOne(BinaryMatrix binaryMatrix) {
         List<Integer> dimensions = binaryMatrix.dimensions();
+        
         int m = dimensions.get(0);
         int n = dimensions.get(1);
         
@@ -9,7 +18,7 @@ class Solution {
         int j = n - 1;
         while (i <= j) {
             int mid = i + (j - i) / 2;
-            if (check(binaryMatrix, mid, m)) {
+            if (checkOne(binaryMatrix, mid, m)) {
                 j = mid - 1;
             } else {
                 i = mid + 1;
@@ -18,7 +27,8 @@ class Solution {
         if (i >= n) return -1;
         return i;
     }
-    private boolean check(BinaryMatrix binaryMatrix, int j, int total) {
+    
+    private boolean checkOne(BinaryMatrix binaryMatrix, int j, int total) {
         int i = 0;
         while (i < total) {
             if (binaryMatrix.get(i, j) == 1) {
@@ -29,12 +39,3 @@ class Solution {
         return false;
     }
 }
-
-/**
- * // This is the BinaryMatrix's API interface.
- * // You should not implement it, or speculate about its implementation
- * interface BinaryMatrix {
- *     public int get(int row, int col) {}
- *     public List<Integer> dimensions {}
- * };
- */
