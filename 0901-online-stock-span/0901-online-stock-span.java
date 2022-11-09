1b5ca8f14c98ec6473abba540d5b5ee5659ea2ac
Yuanfan Zhang <yuanfanz@uci.edu>
2022-11-08 20:55:07 -0800

    Time: 74 ms (62.78%), Space: 68.8 MB (81.94%) - LeetHub

---
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -0,0 +1,25 @@
+class StockSpanner {
+    Stack<int[]> stack;
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+    
+    public int next(int price) {
+        int count = 1;
+        if (stack.isEmpty()) {
+            stack.push(new int[]{price, 1});
+        } else {
+            while (stack.size() > 0 && stack.peek()[0] <= price) {
+                count += stack.pop()[1];
+            }
+            stack.push(new int[]{price, count});
+        }
+        return count;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
