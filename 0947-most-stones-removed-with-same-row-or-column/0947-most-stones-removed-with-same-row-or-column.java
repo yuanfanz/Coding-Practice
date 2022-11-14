8bcdd970f64de2d23b96e54b22341c2aab75b617
Yuanfan Zhang <yuanfanz@uci.edu>
2022-11-13 18:51:48 -0800

    Time: 56 ms (45.37%), Space: 49 MB (70.73%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int removeStones(int[][] stones) {
+        int len = stones.length;
+        
+        UnionFind uf = new UnionFind(len);
+        for (int i = 0; i < stones.length; ++i) {
+            for (int j = i + 1; j < stones.length; ++j) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        return len - uf.count;
+    }
+    
+    class UnionFind{
+        int[] parent;
+        int count;
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; ++i) {
+                parent[i] = i;
+            }
+            count = n;
+        }
+        public int find(int p) {
+            while (p != parent[p]) {
+                parent[p] = parent[parent[p]];
+                p = parent[p];
+            }
+            return p;
+        }
+        public void union(int p, int q) {
+            int rootP = find(p);
+            int rootQ = find(q);
+            if (rootP == rootQ) return;
+            if (rootP < rootQ) {
+                parent[rootQ] = rootP;
+            } else {
+                parent[rootP] = rootQ;
+            }
+            count--;
+        }
+    }
+}
