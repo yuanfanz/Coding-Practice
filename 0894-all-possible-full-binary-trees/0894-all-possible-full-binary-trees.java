4824cb304de181230b9e78154fd64039506b2769
Yuanfan Zhang <17347678+yuanfanz@users.noreply.github.com>
2023-07-22 21:00:08 -0700

    Time: 3 ms (46.87%), Space: 46.4 MB (23.66%) - LeetHub

---
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.java
@@ -0,0 +1,42 @@
+
+class Solution {
+    public List<TreeNode> allPossibleFBT(int N) {
+        if (N <= 0 || N % 2 == 0) return new ArrayList<>();
+        
+        List<TreeNode> res = new ArrayList<>();
+        
+        if (N == 1) {
+            res.add(new TreeNode(0));
+            return res;
+        }
+        for (int i = 1; i < N; i += 2) {
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(N - i - 1);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = l;
+                    root.right = r;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
